fd735754c2aabd6c12a6fba1100058d49301fa01
michelot <michelot>
1998-01-27 20:50:38 +0000

    Adding integrator files written by Weishi Wan.

---
+++ integrator/src/DriftMap.cc
@@ -0,0 +1,25 @@
+#include "DriftMap.h"
+
+/// ******************************************
+///   class DriftMap
+/// ******************************************
+
+DriftMap::DriftMap() : ODE() {
+  dimen = 2;
+}
+
+DriftMap::~DriftMap() {
+}
+
+Vector DriftMap::operator() (const Vector& state, double t ){
+  Vector ret(dimen);
+  if (dimen == 2){
+    ret(0) = state(0)+t*state(1);
+    ret(1) = state(1);
+    return ret;
+  }
+  else{
+    cerr << "DriftMap::operator(): Wrong dimension." << endl;
+    return 0;
+  }
+}
---
+++ integrator/src/Integrator.cc
@@ -0,0 +1,19 @@
+#include "Integrator.h"
+
+/// ******************************************
+///   class Integrator
+/// ******************************************
+
+Integrator::Integrator() {
+}
+
+/*
+Integrator::Integrator( int _dim, double _length, int _nStep ) {
+  dim = _dim;
+  length = _length;
+  nStep = _nStep;
+}
+*/
+
+Integrator::~Integrator() {
+}
