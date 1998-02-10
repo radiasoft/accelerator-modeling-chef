1b0c48ff76be5244c63812f76371a4e106a26e9c
abraun <abraun>
1998-02-10 20:10:58 +0000

    Removed pow function.
    Andrew Braun

--- beamline/src/physics/6d/JetThinQuadPhysics.cc
+++ beamline/src/physics/6d/JetThinQuadPhysics.cc
@@ -2,8 +2,10 @@
 
 void JetthinQuad::propagate( Particle& p ) {
  // "Strength" is B'l in Tesla
- static double xpr, ypr, p3divpbar;
- p3divpbar = sqrt( pow( 1.0 + p.state[5], 2.0 )
+ static double xpr, ypr, p3divpbar, state5p1;
+
+ state5p1 = 1.0 + p.state[5];
+ p3divpbar = sqrt( state5p1 * state5p1
                  - p.state[3]*p.state[3] 
                  - p.state[4]*p.state[4] );
  xpr = p.state[3] / p3divpbar; 
@@ -12,7 +14,7 @@ void JetthinQuad::propagate( Particle& p ) {
  xpr += - strength * p.state[0] / p.BRho();
  ypr +=   strength * p.state[1] / p.BRho();
 
- p3divpbar = ( 1.0 + p.state[5] ) / sqrt( 1.0 + xpr*xpr + ypr*ypr );
+ p3divpbar = state5p1 / sqrt( 1.0 + xpr*xpr + ypr*ypr );
 
  p.state[3] = xpr*p3divpbar;
  p.state[4] = ypr*p3divpbar;
