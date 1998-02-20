ec85256e99431d762beba5e0fde2d9d3c410905f
abraun <abraun>
1998-02-20 20:48:04 +0000

    Modified the propagate( JetParticle) method by filling it in with the JetParticle propagator for the obsolete thinSeptaPhysics class.  There is a question as to whether this is right.

--- beamline/src/physics/6d/thinSeptumPhysics.cc
+++ beamline/src/physics/6d/thinSeptumPhysics.cc
@@ -24,5 +24,29 @@ void thinSeptum::propagate( Particle& p ) {
 
 
 
-void thinSeptum::propagate( JetParticle& p ) { }
+void thinSeptum::propagate( JetParticle& p ) { 
+// This is taken from the thinSeptaPhysics.cc file. AJB 2/20/98
+
+  static int i;
+  Jet TrState[6], dummy[6];
+  Jet JetxWire;
+  Jet kick;
+
+  if( align ) align->misalign( p, geometry /* ??? */, TrState );
+  else        for( i = 0; i < 6; i++  ) TrState[i] = p.state(i);
+
+  JetxWire = xWire;
+  if (TrState[0].standardPart() > JetxWire.standardPart() ) kick = strengthPos/(1.0 + TrState[5]);
+  else kick = strengthNeg/(1.0 + TrState[5]);
+  
+ TrState[3] += kick;
+  
+  if( align ) {
+    for( i = 0; i < 6; i++  ) dummy[i] = TrState[i];
+    align->align( dummy, geometry /* ??? */, TrState );
+  }
+  
+  ( p.state ).SetComponent( 3, TrState[3] );
+  ( p.state ).SetComponent( 4, TrState[4] );
+}
 
