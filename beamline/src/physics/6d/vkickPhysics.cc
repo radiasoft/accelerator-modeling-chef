bdcdb42a00d0fff463ab9337a127714f3ef0d514
michelot <michelot>
1998-03-27 20:31:26 +0000

    Rearranged the logic a little so that "if(align)" is performed only once.
    L. Michelotti

--- beamline/src/physics/6d/vkickPhysics.cc
+++ beamline/src/physics/6d/vkickPhysics.cc
@@ -4,18 +4,15 @@ void vkick::propagate( Particle& p ) {
   static double TrState[6], dummy[6];
   if( align ) {
     align->misalign( p, geometry /* ??? */, TrState );
-  } else  {
-    memcpy((void *)TrState, (const void *)p.state, 6 * sizeof(double));
-  }
-  // p.state[4] += strength;
-  TrState[4] += strength;
-
-  if( align ) {
+    TrState[4] += strength;
     memcpy((void *)dummy, (const void *)TrState, 6 * sizeof(double));
     align->align( dummy, geometry /* ??? */, TrState );
+    p.state[3] = TrState[3];
+    p.state[4] = TrState[4];
   }
-  p.state[3] = TrState[3];
-  p.state[4] = TrState[4];
+  else {
+    p.state[4] += strength;
+  }
 }
 
 void vkick::propagate( JetParticle& p ) {
@@ -23,20 +20,13 @@ void vkick::propagate( JetParticle& p ) {
   static Jet TrState[6], dummy[6];
   if( align ) {
     align->misalign( p, geometry /* ??? */, TrState );
-  } else {
-    for( i = 0; i < 6; i++  ) TrState[i] = p.state(i);
-  }
-
-  TrState[4] += strength;
-  // Jet  dummy;
-  // dummy = p.state(4) + strength;
-  // ( p.state ).SetComponent( 4, dummy );
-
-  if( align ) {
+    TrState[4] += strength;
     for( i = 0; i < 6; i++  ) dummy[i] = TrState[i];
     align->align( dummy, geometry /* ??? */, TrState );
+    ( p.state ).SetComponent( 3, TrState[3] );
+    ( p.state ).SetComponent( 4, TrState[4] );
   }
-  ( p.state ).SetComponent( 3, TrState[3] );
-  ( p.state ).SetComponent( 4, TrState[4] );
+  else {
+    ( p.state ).SetComponent( 4, p.state(4) + strength );
+  }
 }
-
