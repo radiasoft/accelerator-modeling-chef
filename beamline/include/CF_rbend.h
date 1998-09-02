5c1fb92b47d05346e377c8b131b1118f83f79d21
michelot <michelot>
1998-09-02 16:36:06 +0000

    Adding class CF_rbend to the repository.
    Leo Michelotti

--- beamline/include/CF_rbend.h
+++ beamline/include/CF_rbend.h
@@ -1,34 +1,51 @@
 #ifndef CF_RBEND_H
 #define CF_RBEND_H
 
-#include "mxyzptlk.h"
-#include "beamline.inc"
-
-#include "combinedFunction.h"
+#include "beamline.h"
 
 class CF_rbend : public bmlnElmnt
 {
- public:
+  public:
+
+  CF_rbend( double,     // length  [ meters ]
+            double,     // field   [ tesla ]
+            double,     // Pole face angle [ radians ]
+            int = 4 );
+ 
+
+  CF_rbend( const char*,// name
+            double,     // length  [ meters ]
+            double,     // field   [ tesla ]
+            double,     // Pole face angle [ radians ]
+            int = 4 );
+
+
   CF_rbend( double,         // length     [ meter    ]
             double,         // field      [ tesla    ]
             double,         // angle      [ radians  ]
-            const Vector&   // multipoles [ meter^-n ]
+            const Vector&,  // multipoles [ meter^-n ]
             // Normal and skew multipoles alternate.
             // xmlt(0)       = b_0    xmlt(1)     = a_0
             // xmlt(2)       = b_1    xmlt(3)     = a_1
             // xmlt(4)       = b_2    xmlt(5)     = a_2
             // ...
             // xmlt(2*m)     = b_[m]  xmlt(2*m+1) = a_[m]
-          );
-  CF_rbend( char*,          // name
+            int = 4 );
+
+
+  CF_rbend( const char*,    // name
             double,         // length     [ meter    ]
             double,         // field      [ tesla    ]
             double,         // angle      [ radians  ]
-            const Vector&   // multipoles [ meter^-n ]
-          );
+            const Vector&,  // multipoles [ meter^-n ]
+            int = 4 );
+
+
   CF_rbend( const CF_rbend& );
 
   ~CF_rbend();
+  void eliminate();
+
 
   void localPropagate( Particle& );
   void localPropagate( JetParticle& );
@@ -42,10 +59,29 @@ class CF_rbend : public bmlnElmnt
     { return "CF_rbend"; }
   bmlnElmnt* Clone() const 
     { return new CF_rbend( *this ); }
+  double OrbitLength( const Particle& );
+  void Split( double, bmlnElmnt**, bmlnElmnt** );
 
+  double PoleFaceAngle()    
+    { return _poleFaceAngle; }
+  double getPoleFaceAngle() 
+    { return _poleFaceAngle; }
+
+  int setSextupole( double );  
+  // Returns 0 if sextupole is set correctly.
+  //         1 if there are no sextupoles.
 
  private:
-  bmlnElmnt* u[52];
+
+  bmlnElmnt** _u;
+  bmlnElmnt** _v;
+  // bmlnElmnt* u[52];
+
+  double _poleFaceAngle;
+
+  ostream& writeTo(ostream&);
+  istream& readFrom(istream&);
 };
+
 
 #endif // CF_RBEND
--- beamline/src/CF_rbend.cc
+++ beamline/src/CF_rbend.cc
@@ -3,87 +3,157 @@
 CF_rbend::CF_rbend( double        lng,  // length     [ meter    ]
                     double        fld,  // field      [ tesla    ]
                     double        ang,  // angle      [ radians  ]
-                    const Vector& xmlt  // multipoles [ meter^-n ]
-                    // Normal and skew multipoles alternate.
-                    // xmlt(0)       = b_0    xmlt(1)     = a_0
-                    // xmlt(2)       = b_1    xmlt(3)     = a_1
-                    // xmlt(4)       = b_2    xmlt(5)     = a_2
-                    // ...
-                    // xmlt(2*m)     = b_[m]  xmlt(2*m+1) = a_[m]
-                  )
-: bmlnElmnt()
+                    int )
+: bmlnElmnt( lng, fld ), _poleFaceAngle( ang  )
 {
   #include "CF_rbendConstructor.ins"
 }
 
 
-CF_rbend::CF_rbend( char*  nm,         // name
+CF_rbend::CF_rbend( const char*   nm,   // name
+                    double        lng,  // length     [ meter    ]
+                    double        fld,  // field      [ tesla    ]
+                    double        ang,  // angle      [ radians  ]
+                    int )
+: bmlnElmnt( nm, lng, fld ), _poleFaceAngle( ang  )
+{
+  #include "CF_rbendConstructor.ins"
+}
+
+
+CF_rbend::CF_rbend( double lng,
+                    double fld,
+                    double ang,
+                    const Vector& xmlt,
+                    int  )
+: bmlnElmnt( lng, fld ), _poleFaceAngle( ang  )
+{
+  #include "CF_rbendConstructor.ins"
+}
+
+
+CF_rbend::CF_rbend( const char*  nm,
                     double lng,
                     double fld,
                     double ang,
-                    const Vector& xmlt )
-: bmlnElmnt( nm )
+                    const Vector& xmlt,
+                    int  )
+: bmlnElmnt( nm, lng, fld ), _poleFaceAngle( ang  )
 {
   #include "CF_rbendConstructor.ins"
 }
 
 
 CF_rbend::CF_rbend( const CF_rbend& x )
-: bmlnElmnt( (const bmlnElmnt&) x )
+: bmlnElmnt( (const bmlnElmnt&) x ), _poleFaceAngle( x._poleFaceAngle )
 {
-  int i, j, k, l;
-  for( i = 0; i < 4; i++ ) {
-    u[i] = x.u[i]->Clone();
+  int m = 1 + ( ( int(x._v) - int(x._u) )/sizeof( bmlnElmnt* ) );
+  _u = new bmlnElmnt* [ m ];
+  _v = &( _u[m-1] );
+  
+  for( int k = 0; k < m; k++ ) {
+    _u[k] = ( x._u[k] )->Clone();
   }
-  j = 4;
-  for( i = 0; i < 3; i++ ) {
-    u[j++] = u[1];
-    u[j++] = u[2];
-    u[j++] = u[3];
-  }
-  --j;
-  u[j++] = u[0];
-
-  for( k = 0; k < 3; k++ ) {
-    i = 0;
-    for( l = 0; l < 13; l++ ) {
-      u[j++] = u[i++];
-    }
-  }  
-
-  if( j != 52 ) {
-    cerr << "*** ERROR ***                                 \n"
-            "*** ERROR *** CF_rbend::CF_rbend( CF_rbend )  \n"
-            "*** ERROR *** A horrible, inexplicable error  \n"
-            "*** ERROR *** has occurred.                   \n"
-            "*** ERROR ***                                 \n"
-         << endl;
-    exit(1);
-  }
-
 }
 
 
 CF_rbend::~CF_rbend()
 {
-  delete u[0];
-  delete u[1];
-  delete u[2];
-  delete u[3];
+  while( _v >= _u ) {
+    delete (*(_v--));
+  }
+  delete [] _u;
 }
 
 
 void CF_rbend::localPropagate( Particle& p )
 {
-  for( int i = 0; i < 52; i++ ) {
-    u[i]->localPropagate( p );
+  bmlnElmnt** x = _u;
+  while( x <= _v ) {
+    (*(x++))->localPropagate( p );
   }
 }
 
 
 void CF_rbend::localPropagate( JetParticle& p )
 {
-  for( int i = 0; i < 52; i++ ) {
-    u[i]->localPropagate( p );
+  bmlnElmnt** x = _u;
+  while( x <= _v ) {
+    (*(x++))->localPropagate( p );
   }
+}
+
+
+int CF_rbend::setSextupole( double arg_x )
+{
+  int m = 1 + ( ( int(_v) - int(_u) )/sizeof( bmlnElmnt* ) );
+  thinSextupole** w = new thinSextupole* [ m ];
+  int counter = -1;
+  bmlnElmnt** x = _u;
+
+  while( x <= _v ) {
+    if( 0 == strcmp( (*x)->Type(), "thinSextupole" ) ) {
+      // w[++counter] = dynamic_cast<thinSextupole*>(*x);
+      w[++counter] = (thinSextupole*)(*x);
+    }
+    x++;
+  }
+
+  if( counter < 0 ) {
+    delete [] w;
+    return 1;
+  }
+  
+  double s = arg_x/((double) ( counter + 1 ));
+  for( int i = 0; i <= counter; i++ ) {
+    w[i]->setStrength( s );
+  }
+  
+  delete [] w;
+  return 0;
+}
+
+void CF_rbend::Split( double, bmlnElmnt**, bmlnElmnt** )
+{
+  cerr << "*** ERROR ***                                 \n" 
+          "*** ERROR *** CF_rbend::Split                 \n" 
+          "*** ERROR *** This function is not written.   \n" 
+          "*** ERROR ***                                 \n" 
+       << endl;
+  exit(1);
+}
+
+ostream& CF_rbend::writeTo( ostream& os )
+{
+  cerr << "*** ERROR ***                                 \n" 
+          "*** ERROR *** CF_rbend::writeTo               \n" 
+          "*** ERROR *** This function is not written.   \n" 
+          "*** ERROR ***                                 \n" 
+       << endl;
+  exit(1);
+  return os;
+}
+
+istream& CF_rbend::readFrom( istream& is )
+{
+  cerr << "*** ERROR ***                                 \n" 
+          "*** ERROR *** CF_rbend::readFrom              \n" 
+          "*** ERROR *** This function is not written.   \n" 
+          "*** ERROR ***                                 \n" 
+       << endl;
+  exit(1);
+  return is;
+}
+
+void CF_rbend::eliminate( void )
+{
+  delete this;
+}
+
+
+double CF_rbend::OrbitLength( const Particle& x )
+{
+  static double tworho;
+  tworho  = 2.0 * ( x.Momentum() / PH_CNV_brho_to_p ) / strength;
+  return tworho * asin( length / tworho );
 }
