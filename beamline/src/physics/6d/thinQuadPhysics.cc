a5cb724d9af18d5e0ef0bb29207e6a5c198debef
michelot <michelot>
1998-09-17 17:21:17 +0000

    *** empty log message ***

--- beamline/src/physics/6d/thinQuadPhysics.cc
+++ beamline/src/physics/6d/thinQuadPhysics.cc
@@ -5,15 +5,18 @@ void thinQuad::localPropagate( Particle& p ) {
  static double TrState[6], dummy[6];
  static double k;
 
- memcpy((void *)TrState, (const void *)p.state, 6 * sizeof(double));
+ if( strength != 0.0 ) 
+ {
+   memcpy((void *)TrState, (const void *)p.state, 6 * sizeof(double));
 
- k = strength / p.ReferenceBRho();
- TrState[3] += - k * TrState[0];
- TrState[4] +=   k * TrState[1];
+   k = strength / p.ReferenceBRho();
+   TrState[3] += - k * TrState[0];
+   TrState[4] +=   k * TrState[1];
 
- p.state[3] = TrState[3];
- p.state[4] = TrState[4];
+   p.state[3] = TrState[3];
+   p.state[4] = TrState[4];
 
+ }
 }
 
 void thinQuad::localPropagate( JetParticle& p ) {
@@ -21,15 +24,17 @@ void thinQuad::localPropagate( JetParticle& p ) {
  static int i;
  static double k;
 
- for( i = 0; i < 6; i++  ) {
-   TrState[i] = p.state(i);
+ if( strength != 0.0 ) 
+ {
+   for( i = 0; i < 6; i++  ) {
+     TrState[i] = p.state(i);
+   }
+
+   k = strength / p.ReferenceBRho();
+   TrState[3] += - k * TrState[0];
+   TrState[4] +=   k * TrState[1];
+
+   ( p.state ).SetComponent( 3, TrState[3] );
+   ( p.state ).SetComponent( 4, TrState[4] );
  }
-
- k = strength / p.ReferenceBRho();
- TrState[3] += - k * TrState[0];
- TrState[4] +=   k * TrState[1];
-
- ( p.state ).SetComponent( 3, TrState[3] );
- ( p.state ).SetComponent( 4, TrState[4] );
-
 }
--- beamline/src/physics/6d/thinSextupolePhysics.cc
+++ beamline/src/physics/6d/thinSextupolePhysics.cc
@@ -5,19 +5,22 @@ void thinSextupole::localPropagate( Particle& p ) {
  double TrState[6], dummy[6];
  int    i;
 
- for( i = 0; i < 6; i++  ) {
-   TrState[i] = p.state[i];
- }
+ if( strength != 0.0 ) 
+ {
+   for( i = 0; i < 6; i++  ) {
+     TrState[i] = p.state[i];
+   }
 
- k = strength / p.ReferenceBRho();
- x = TrState[0];
- y = TrState[1];
+   k = strength / p.ReferenceBRho();
+   x = TrState[0];
+   y = TrState[1];
  
- TrState[3] -= k * ( x*x - y*y );
- TrState[4] += 2.0 * k * x*y;
+   TrState[3] -= k * ( x*x - y*y );
+   TrState[4] += 2.0 * k * x*y;
 
- p.state[3] = TrState[3];
- p.state[4] = TrState[4];
+   p.state[3] = TrState[3];
+   p.state[4] = TrState[4];
+ }
 }
 
 
@@ -27,18 +30,20 @@ void thinSextupole::localPropagate( JetParticle& p ) {
  static Jet x, y;
  static int i;
  
- for( i = 0; i < 6; i++  ) {
-   TrState[i] = p.state(i);
- }
+ if( strength != 0.0 ) 
+ {
+   for( i = 0; i < 6; i++  ) {
+     TrState[i] = p.state(i);
+   }
 
- k = strength / p.ReferenceBRho();
- x = TrState[0];
- y = TrState[1];
+   k = strength / p.ReferenceBRho();
+   x = TrState[0];
+   y = TrState[1];
  
- TrState[3] -= k * ( x*x - y*y );
- TrState[4] += 2.0 * k * x*y ;
+   TrState[3] -= k * ( x*x - y*y );
+   TrState[4] += 2.0 * k * x*y ;
 
- ( p.state ).SetComponent( 3, TrState[3] );
- ( p.state ).SetComponent( 4, TrState[4] );
-
+   ( p.state ).SetComponent( 3, TrState[3] );
+   ( p.state ).SetComponent( 4, TrState[4] );
+ }
 }
