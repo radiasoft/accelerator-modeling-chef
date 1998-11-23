ab31e6aff440d6d975100ed7b82ac194c4a2b292
mccrory <mccrory>
1998-11-23 21:11:11 +0000

    Allow a monitor to have a length.  Also, add a private attirbute to class monitor to determine where within the drift to put the actual pickup of the monitor.

--- beamline/src/physics/6d/monitorPhysics.cc
+++ beamline/src/physics/6d/monitorPhysics.cc
@@ -1,27 +1,72 @@
 #include "beamline.inc"
 
 void monitor::localPropagate( Particle& p ) {
- int i;
- 
- for ( i = 0; i < BMLN_dynDim; i++ ) rgr[i] = p.state[i];
- 
- if ( onOff ) {
-   fprintf( outputFile, "BPM: %s ..........\n", ident );
-   for ( i = 0; i < BMLN_dynDim; i++ ) 
-     fprintf( outputFile, "%10.6lf  ", rgr[i] );
-   fprintf( outputFile, "\n" );
- }
+  int i;
+  static double realLength;
+
+  if( length > 0.0 ) {
+    // ---------------------------------------------------------------------
+    realLength = length;
+    length *= getDriftFraction();
+    
+    bmlnElmnt::localPropagate( p );   // Drift through half the length.
+    
+    for ( i = 0; i < BMLN_dynDim; i++ ) rgr[i] = p.state[i];
+    
+    if ( onOff ) {
+      fprintf( outputFile, "BPM: %s ..........\n", ident );
+      for ( i = 0; i < BMLN_dynDim; i++ ) 
+	fprintf( outputFile, "%10.6lf  ", rgr[i] );
+      fprintf( outputFile, "\n" );
+    }
+    length = realLength - length;
+    bmlnElmnt::localPropagate( p ); // Drift through the other half.
+    
+    length = realLength;
+  } // ---------------------------------------------------------------------
+  else {
+    for ( i = 0; i < BMLN_dynDim; i++ ) rgr[i] = p.state[i];
+    
+    if ( onOff ) {
+      fprintf( outputFile, "BPM: %s ..........\n", ident );
+      for ( i = 0; i < BMLN_dynDim; i++ ) 
+	fprintf( outputFile, "%10.6lf  ", rgr[i] );
+      fprintf( outputFile, "\n" );
+    }
+  }
 }
 
 void monitor::localPropagate( JetParticle& p ) {
- int i;
-
- for ( i = 0; i < BMLN_dynDim; i++ ) rgr[i] = ( p.state(i) ).standardPart();
- 
- if ( onOff ) {
-   fprintf( outputFile, "BPM: %s ..........\n", ident );
-   for ( i = 0; i < BMLN_dynDim; i++ ) 
-     fprintf( outputFile, "%10.6lf  ", rgr[i] );
-   fprintf( outputFile, "\n" );
- }
+  int i;
+  static double realLength;
+    if( length > 0.0 ) {
+      // ---------------------------------------------------------------------
+      realLength = length;
+      length *= getDriftFraction();
+      
+      bmlnElmnt::localPropagate( p );   // Drift through half the length.
+      
+      for ( i = 0; i < BMLN_dynDim; i++ ) rgr[i] = ( p.state(i) ).standardPart();
+      
+      if ( onOff ) {
+	fprintf( outputFile, "BPM: %s ..........\n", ident );
+	for ( i = 0; i < BMLN_dynDim; i++ ) 
+	  fprintf( outputFile, "%10.6lf  ", rgr[i] );
+	fprintf( outputFile, "\n" );
+      }
+      length = realLength - length;
+      bmlnElmnt::localPropagate( p ); // Drift through the other half.
+      
+      length = realLength;
+    } // ---------------------------------------------------------------------
+    else {
+      for ( i = 0; i < BMLN_dynDim; i++ ) rgr[i] = ( p.state(i) ).standardPart();
+      
+      if ( onOff ) {
+	fprintf( outputFile, "BPM: %s ..........\n", ident );
+	for ( i = 0; i < BMLN_dynDim; i++ ) 
+	  fprintf( outputFile, "%10.6lf  ", rgr[i] );
+	fprintf( outputFile, "\n" );
+      }
+    }
 }
