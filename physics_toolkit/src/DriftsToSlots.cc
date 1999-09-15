0e07094296b89346ede0fdba1b13c932c2c9fefe
michelot <michelot>
1999-09-15 21:17:42 +0000

    I've modified the algorithm so that upstream and downstream searches bypass all elements of non-zero length until reaching an rbend or CF_rbend.
    Leo Michelotti

--- physics_toolkit/src/DriftsToSlots.cc
+++ physics_toolkit/src/DriftsToSlots.cc
@@ -8,6 +8,14 @@
  *  Version 1.0  (untested)
  *  October 30, 1998
  *  
+ *
+ *  Changed the algorithm to be more general.
+ *  It now searches upstream and downstream for
+ *  non-zero longth elements or rbends.
+ *
+ *  Version 2.0
+ *  September 14, 1999
+ *
  *  Leo Michelotti
  *  
  */
@@ -18,61 +26,194 @@
 #include "Slot.h"
 
 
+char d2S_LookUpStream ( bmlnElmnt*&      el2Ptr, 
+                        char&            rb, 
+                        char&            CF, 
+                        BeamlineIterator bi
+                       )
+{
+  rb = 0;
+  CF = 0;
+
+  bi.goBack();
+  bi.goBack();
+  el2Ptr = bi++;
+
+  // cout << "DGN> " 
+  //      << el2Ptr->Type() << "  " 
+  //      << el2Ptr->Name()
+  //      << endl;
+
+  while( 0 != strcmp( el2Ptr->Type(), "rbend"    )  &&
+         0 != strcmp( el2Ptr->Type(), "CF_rbend" )     ) 
+  {
+    if( 0.0 != el2Ptr->Length() ) {
+      return 0;
+    }
+    bi.goBack();
+    bi.goBack();
+    el2Ptr = bi++;
+    // cout << "DGN> " 
+    //      << el2Ptr->Type() << "  " 
+    //      << el2Ptr->Name()
+    //      << endl;
+
+  }
+
+  rb = ( 0 == strcmp( el2Ptr->Type(), "rbend"    ) );
+  CF = ( 0 == strcmp( el2Ptr->Type(), "CF_rbend" ) );
+
+  if( !(rb || CF) ) 
+  {
+    cerr << "*** ERROR ***                                        \n"
+            "*** ERROR *** ConvertDriftsToSlots                   \n"
+            "*** ERROR *** A horrible, inexplicable error has     \n"
+            "*** ERROR *** occurred while looking upstream.       \n"
+            "*** ERROR ***                                        \n"
+         << endl;
+    return 0;
+  }
+
+  return 1;
+}
+
+
+
+
+char d2S_LookDownStream ( bmlnElmnt*&      el2Ptr, 
+                          char&            rb, 
+                          char&            CF, 
+                          BeamlineIterator bi
+                       )
+{
+  rb = 0;
+  CF = 0;
+
+  el2Ptr = bi++;
+  if( el2Ptr == 0 ) {
+    bi.reset();
+    el2Ptr = bi++;
+  }
+
+  // cout << "DGN> " 
+  //      << el2Ptr->Type() << "  " 
+  //      << el2Ptr->Name()
+  //      << endl;
+
+  while( 0 != strcmp( el2Ptr->Type(), "rbend"    )  &&
+         0 != strcmp( el2Ptr->Type(), "CF_rbend" )     ) 
+  {
+    if( 0.0 != el2Ptr->Length() ) {
+      return 0;
+    }
+    el2Ptr = bi++;
+    if( el2Ptr == 0 ) {
+      bi.reset();
+      el2Ptr = bi++;
+    }
+    // cout << "DGN> " 
+    //      << el2Ptr->Type() << "  " 
+    //      << el2Ptr->Name()
+    //      << endl;
+
+  }
+
+  rb = ( 0 == strcmp( el2Ptr->Type(), "rbend"    ) );
+  CF = ( 0 == strcmp( el2Ptr->Type(), "CF_rbend" ) );
+
+  if( !(rb || CF) ) 
+  {
+    cerr << "*** ERROR ***                                        \n"
+            "*** ERROR *** ConvertDriftsToSlots                   \n"
+            "*** ERROR *** A horrible, inexplicable error has     \n"
+            "*** ERROR *** occurred while looking downstream.     \n"
+            "*** ERROR ***                                        \n"
+         << endl;
+    return 0;
+  }
+
+  return 1;
+}
+
+
+
+
 beamline* DriftsToSlots( /* const */ beamline& original )
 {
+  bmlnElmnt* elPtr;
+  bmlnElmnt* el2Ptr;
+  dlink*     linkPtr;
+  dlink*     link2Ptr;
+
+
+  // Check if Slots are already present ...
+  DeepBeamlineIterator dbi( original );
+  while((  elPtr = dbi++  )) 
+  {
+    if( 0 == strcmp( elPtr->Type(), "Slot" ) ) {
+      cerr << "*** ERROR ***                                        \n"
+              "*** ERROR *** DriftsToSlots                          \n"
+              "*** ERROR *** Slots already exist in this beamline.  \n"
+              "*** ERROR ***                                        \n"
+           << endl;
+      return &original;
+    }
+  }
+
+
+  // Create new beamlines ...
+  beamline* ret = new beamline;
+  beamline* flatRing = original.flatten();
+
+
+  // Some variables ...
   Frame arcFrame;
   Vector fd(3);
   enum { x = 0, y, z };
   arcFrame.reset();  
 
-  beamline* ret = new beamline;
-  beamline* flatRing = original.flatten();
-
-  drift dummyDrift( 0.0 );
-  flatRing->insert( dummyDrift );
-  flatRing->append( dummyDrift );
-
-  bmlnElmnt* a;
-  bmlnElmnt* b;
-  bmlnElmnt* c;
-  
-  dlist_iterator getNext((  *(dlist*) flatRing  ));
-  
   char isUpStream, isDownStream;
   char a_rb, a_CF, c_rb, c_CF;
+  bmlnElmnt* a;
+  bmlnElmnt* c;
   
-  a = (bmlnElmnt*) getNext();
-  b = (bmlnElmnt*) getNext();
-  
-  // ret->append( a->Clone() );
-  // Should not be necessary, since I've put a dummy drift
-  // at the beginning of the original beamline
-  
-  while((  c = (bmlnElmnt*) getNext()  )) 
+
+  // Process the flattened beamline ...
+  BeamlineIterator bi( flatRing );
+  while((  elPtr = bi++  )) 
   {
-    // Bypass markers.
-    if( 0 == strcmp( c->Type(), "marker" ) ) continue;
-  
-    // If the trial element is not a drift, just copy it.
-    if( 0 !=  strcmp( b->Type(), "drift" ) ) 
+    // If the trial element is not a drift, just copy it ...
+    if( 0 !=  strcmp( elPtr->Type(), "drift" ) ) 
     {
-      ret->append( b->Clone() );
+      ret->append( elPtr->Clone() );
     }
   
-    // If the second element is a drift, then begin processing ...
+
+    // ... otherwise, begin processing ...
     else 
     {
       // There are four possibilities
-      isUpStream   = ( ( c_rb = ( 0 == strcmp( c->Type(), "rbend"    ) ) ) || 
-		       ( c_CF = ( 0 == strcmp( c->Type(), "CF_rbend" ) ) )
-		     );
-      isDownStream = ( ( a_rb = ( 0 == strcmp( a->Type(), "rbend"    ) ) ) || 
-		       ( a_CF = ( 0 == strcmp( a->Type(), "CF_rbend" ) ) )
-		     );
-  
+      // cout << "DGN> Looking upstream for "
+      //      << elPtr->Type() << "  "
+      //      << elPtr->Name() << "  "
+      //      << endl;
+      isDownStream  = d2S_LookUpStream   ( a, a_rb, a_CF, bi );
+      // cout << "DGN> Looking downstream for "
+      //      << elPtr->Type() << "  "
+      //      << elPtr->Name() << "  "
+      //      << endl;
+      isUpStream    = d2S_LookDownStream ( c, c_rb, c_CF, bi );
+
+      // isUpStream   = ( ( c_rb = ( 0 == strcmp( c->Type(), "rbend"    ) ) ) || 
+      //                  ( c_CF = ( 0 == strcmp( c->Type(), "CF_rbend" ) ) )
+      //                );
+      // isDownStream = ( ( a_rb = ( 0 == strcmp( a->Type(), "rbend"    ) ) ) || 
+      //                  ( a_CF = ( 0 == strcmp( a->Type(), "CF_rbend" ) ) )
+      //                );
+      // 
       if( !isUpStream && !isDownStream )
       {
-	ret->append( b->Clone() );
+	ret->append( elPtr->Clone() );
       }
   
       else if( isUpStream && !isDownStream )
@@ -89,9 +230,9 @@ beamline* DriftsToSlots( /* const */ beamline& original )
 	}
 	fd(x) = 0.0;
 	fd(y) = 0.0;
-	fd(z) = b->Length();
+	fd(z) = elPtr->Length();
 	arcFrame.translate(fd);
-	ret->append( new Slot(b->Name(), arcFrame) );
+	ret->append( new Slot(elPtr->Name(), arcFrame) );
       }
   
       else if( !isUpStream && isDownStream )
@@ -99,7 +240,7 @@ beamline* DriftsToSlots( /* const */ beamline& original )
 	arcFrame.reset(); 
 	fd(x) = 0.0;
 	fd(y) = 0.0;
-	fd(z) = b->Length();
+	fd(z) = elPtr->Length();
 	arcFrame.translate(fd);
 	if( a_rb ) {
 	  arcFrame.rotate( - ((rbend*) a)->getPoleFaceAngle(), arcFrame.getyAxis() );
@@ -110,7 +251,7 @@ beamline* DriftsToSlots( /* const */ beamline& original )
 	else {
 	  exit(22);
 	}
-	ret->append( new Slot(b->Name(), arcFrame) );
+	ret->append( new Slot(elPtr->Name(), arcFrame) );
       }
   
       else // isUpstream and isDownStream
@@ -127,7 +268,7 @@ beamline* DriftsToSlots( /* const */ beamline& original )
 	}
 	fd(x) = 0.0;
 	fd(y) = 0.0;
-	fd(z) = b->Length();
+	fd(z) = elPtr->Length();
 	arcFrame.translate(fd);
 	if( a_rb ) {
 	  arcFrame.rotate( - ((rbend*) a)->getPoleFaceAngle(), arcFrame.getyAxis() );
@@ -138,21 +279,12 @@ beamline* DriftsToSlots( /* const */ beamline& original )
 	else {
 	  exit(22);
 	}
-	ret->append( new Slot(b->Name(), arcFrame) );
+	ret->append( new Slot(elPtr->Name(), arcFrame) );
       }
   
     } // End processing 
   
-    // Get ready for next step
-    a = b;
-    b = c;
-  
   } // End while loop
-  
-  
-  // ret->append( b->Clone() );
-  // Should not be necessary, since I've put a dummy drift
-  // at the end of the original beamline.
   
   
   // Get rid of flatRing
@@ -164,3 +296,88 @@ beamline* DriftsToSlots( /* const */ beamline& original )
 
   return ret;
 }
+
+
+
+// beamline* ConvertDriftsToSlots( /* const */ beamline& original, 
+//                                             slist&    removedElements )
+// {
+//   Frame arcFrame;
+//   Vector fd(3);
+//   enum { x = 0, y, z };
+//   arcFrame.reset();  
+// 
+//   beamline* ret = original.flatten();
+// 
+//   bmlnElmnt* elPtr;
+//   bmlnElmnt* el2Ptr;
+//   dlink*     linkPtr;
+//   dlink*     link2Ptr;
+//   dlist_traversor getNext((  *(dlist*) ret  ));
+//   
+//   bool       isNotDrift;
+//   bool       isNotSlot;
+//   while((  linkPtr = getNext()  )) 
+//   {
+//     // Process rbend and CF_rbend ...
+//     elPtr = (bmlnElmnt*) linkPtr->e;
+//     if( strcmp( elPtr->Type(), "rbend" )    == 0  ||
+//         strcmp( elPtr->Type(), "CF_rbend" ) == 0
+//       ) 
+//     {
+//       // Search for previous drift or Slot ...
+//       link2Ptr = linkPtr->prev;
+//       el2Ptr   = (bmlnElmnt*) link2ptr->e;
+//       if( link2Ptr == linkPtr ) {
+//         return ret;
+//       }
+//       while((isNotDrift = (strcmp( el2Ptr->Type, "drift" ) != 0))  &&
+//             (isNotSlot  = (strcmp( el2Ptr->Type, "Slot" )  != 0))  )
+//       {
+//         if( strcmp( elPtr->Type(), "rbend" )    == 0  ||
+//             strcmp( elPtr->Type(), "CF_rbend" ) == 0
+// 	    ) 
+//         {
+//           cerr << "*** ERROR ***                                        \n"
+//                   "*** ERROR *** ConvertDriftsToSlots                   \n"
+//                   "*** ERROR *** Two rbend-like elements not separated  \n"
+//                   "*** ERROR *** by a drift or Slot.                    \n"
+//                   "*** ERROR ***                                        \n"
+//                << endl;
+//           return 0;
+// 	}
+//         link2Ptr = link2Ptr->prev;
+//         el2Ptr   = (bmlnElmnt*) link2ptr->e;
+//         if( link2Ptr == linkPtr ) {
+//           return ret;
+//         }
+//       }
+// 
+//       // Process previous drift ...
+//       if( !isNotDrift ) {
+//         
+//       }
+// 
+//       // Process previous Slot ...
+//       else if( !isNotSlot ) {
+//       }
+// 
+//       // Error
+//       else {
+//         cerr << "*** ERROR ***                                        \n"
+//                 "*** ERROR *** ConvertDriftsToSlots                   \n"
+//                 "*** ERROR *** A horrible, inexplicable error         \n"
+//                 "*** ERROR *** has occurred. Call an exorcist now!    \n"
+//                 "*** ERROR ***                                        \n"
+//              << endl;
+//         return 0;
+//       }
+// 
+//     }
+// 
+// 
+//   link2Ptr = linkPtr;  // For use outside while loop
+//   }                    // End while loop
+//   
+//   return ret;
+// }
