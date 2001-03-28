52b1780002baa871f9e17d5ec2e0d9e16a9e30c4
cvs2svn <admin@example.com>
2001-03-28 16:13:28 +0000

    This commit was manufactured by cvs2svn to create tag 'BMLFACTORY_1_0'.

--- beamline/src/physics/6d/markerPhysics.cc
+++
@@ -1,9 +0,0 @@
-#include "marker.h"
-
-void marker::localPropagate( Particle& ) {
- // Markers don't do anything.
-}
-
-void marker::localPropagate( JetParticle& ) {
- // Markers don't do anything.
-}
