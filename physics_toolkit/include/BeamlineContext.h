ef158525de8ca3cc3f15fca271e6433464b33de3
michelot <michelot>
2001-08-31 21:22:50 +0000

    Continued development.         - Leo Michelotti

--- physics_toolkit/include/BeamlineContext.h
+++ physics_toolkit/include/BeamlineContext.h
@@ -1,20 +1,75 @@
 #ifndef BEAMLINECONTEXT_H
 #define BEAMLINECONTEXT_H
 
+#ifndef _IOSTREAM_H
+#include <iostream.h>
+#endif
+
 class beamline;
 class LattFuncSage;
 class ClosedOrbitSage;
 class ChromaticityAdjuster;
 class TuneAdjuster;
+class ConstBmlVisitor;
+class Particle;
+class bmlnElmnt;
+class BeamlineIterator;
+class DeepBeamlineIterator;
+class ReverseBeamlineIterator;
+class DeepReverseBeamlineIterator;
 
 class BeamlineContext
 {
   public:
-    BeamlineContext( beamline*, bool=false );
-    // If second argument is true, the
+    BeamlineContext( bool=false, beamline* = 0 );
+    BeamlineContext( const BeamlineContext& );
+    // If first argument is true, the
     // beamline is cloned, and therefore
     // owned by the context.
     ~BeamlineContext();
+
+    int assign( /* const */ beamline* );
+
+    void accept( ConstBmlVisitor& ) const;
+    void setClonedFlag( bool );
+    bool getClonedFlag();
+
+    void writeTree();
+
+    // Beamline functions ...
+    const char* name() const;
+    void peekAt( double& s, Particle* = 0 ) const;
+    double sumLengths() const;
+    int setLength( bmlnElmnt*, double );
+
+    // Iterator functions
+    int beginIterator();
+    int beginDeepIterator();
+    int beginReverseIterator();
+    int beginDeepReverseIterator();
+
+    void endIterator();
+    void endDeepIterator();
+    void endReverseIterator();
+    void endDeepReverseIterator();
+
+    const bmlnElmnt* i_next();
+    const bmlnElmnt* di_next();
+    const bmlnElmnt* ri_next();
+    const bmlnElmnt* dri_next();
+
+    void i_reset();
+    void di_reset();
+    void ri_reset();
+    void dri_reset();
+
+    const beamline* cheatBmlPtr() const;
+
+    // Stream functions
+    friend ostream& operator<<( ostream&, const BeamlineContext& );
+    friend istream& operator>>( istream&,       BeamlineContext& );
+
+
   private:
     beamline*             _p_bml;
     LattFuncSage*         _p_lfs;
@@ -23,6 +78,11 @@ class BeamlineContext
     TuneAdjuster*         _p_ta;
 
     bool                  _isCloned;
+
+    BeamlineIterator*            _p_bi;
+    DeepBeamlineIterator*        _p_dbi;
+    ReverseBeamlineIterator*     _p_rbi;
+    DeepReverseBeamlineIterator* _p_drbi;
 };
 
 #endif // BEAMLINECONTEXT_H
