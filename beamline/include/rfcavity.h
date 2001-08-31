ddeabe647287ea125caf27e8a2a3623ae0fbe688
michelot <michelot>
2001-08-31 21:03:17 +0000

    Added class rfcavity.              - Leo Michelotti

--- beamline/include/rfcavity.h
+++ beamline/include/rfcavity.h
@@ -4,6 +4,57 @@
 #include "bmlnElmnt.h"
 #include "MathConstants.h"
 
+class rfcavity : public bmlnElmnt
+{
+private:
+  double w_rf;                  // RF frequency [MHz]  // ??? What??
+  double phi_s;                 // synchronous phase
+  double sin_phi_s;             // sine of synchronous phase
+  // The max energy gain per turn [GeV] is represented by bmlnELmnt::strength
+  double Q;                     // quality factor
+  double R;                     // shunt impedance
+  ostream& writeTo(ostream&);
+  istream& readFrom(istream&);
+
+public:
+  rfcavity( const char* = "NONAME" ); // Name
+  rfcavity( double,   // length [m]
+            double,   // RF frequency [MHz]
+            double,   // max energy gain per turn [eV] (strength)
+            double,   // synchronous phase [radians]
+            double,   // Q
+            double    // R shunt impedance
+          );
+  rfcavity( char*,    // Name
+            double,   // length [m]
+            double,   // RF frequency [MHz]
+            double,   // max energy gain per turn [eV] (strength)
+            double,   // synchronous phase [radians]
+            double,   // Q
+            double    // R shunt impedance
+          );
+  rfcavity( const rfcavity& );
+  ~rfcavity();
+
+  void localPropagate( ParticleBunch& x ) { bmlnElmnt::localPropagate( x ); }
+  void localPropagate( Particle& );
+  void localPropagate( JetParticle& );
+
+  void accept( BmlVisitor& v ) { v.visitRfcavity( this ); }
+  void accept( ConstBmlVisitor& v ) const { v.visitRfcavity( this ); }
+
+  void eliminate();
+
+  const char* Type() const;
+  virtual int isType(char* c) { if ( strcmp(c, "rfcavity") != 0 ) return bmlnElmnt::isType(c); else return 1; }
+  bmlnElmnt* Clone() const { return new rfcavity( *this ); }
+  inline double getPhi() const { return phi_s; }
+  inline double getFrequency() const { return w_rf; }
+  inline double getQ() const { return Q; }
+  inline double getR() const { return R; }
+};
+
+
 //
 // *** class thinRFCavity written by Jian Ping Shan
 // *** April 4, 1994
@@ -44,6 +95,7 @@ public:
   void localPropagate( JetParticle& );
 
   void accept( BmlVisitor& v ) { v.visitThinrfcavity( this ); }
+  void accept( ConstBmlVisitor& v ) const { v.visitThinrfcavity( this ); }
 
   void eliminate();
 
