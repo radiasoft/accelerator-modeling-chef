1af3f6d56e619a1dd493f5e56973c78df472e347
michelot <michelot>
2002-11-21 21:29:33 +0000

    Added multipole and electrostatic separator instantiation. -lpjm

--- bmlfactory/src/bel_inst_fns.c
+++ bmlfactory/src/bel_inst_fns.c
@@ -48,11 +48,6 @@ make_solenoid( char* name, double length ) {
 }
 
 static inline bmlnElmnt*
-make_elseparator( char* name, double length ) {
-  return new drift( name, length );
-}
-
-static inline bmlnElmnt*
 make_instrument( char* name, double length ) {
   return new drift( name, length );
 }
