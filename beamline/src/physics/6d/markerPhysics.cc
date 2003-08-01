6de8e5e035765d89d7ae26c91f7c063b27cb01b4
ostiguy <ostiguy>
2003-08-01 18:20:55 +0000

    include config.h required for auto-configuration scripts.

--- beamline/src/physics/6d/markerPhysics.cc
+++ beamline/src/physics/6d/markerPhysics.cc
@@ -1,9 +1,38 @@
 #if HAVE_CONFIG_H
 #include <config.h>
 #endif
-#if HAVE_CONFIG_H
-#include <config.h>
-#endif
+/*************************************************************************
+**************************************************************************
+**************************************************************************
+******                                                                
+******  BEAMLINE:  C++ objects for design and analysis
+******             of beamlines, storage rings, and   
+******             synchrotrons.                      
+******  Version:   2.0                    
+******                                    
+******  File:      markerPhysics.cc
+******                                                                
+******  Copyright (c) 1991 Universities Research Association, Inc.    
+******                All Rights Reserved                             
+******                                                                
+******  Author:    Leo Michelotti                                     
+******                                                                
+******             Fermilab                                           
+******             P.O.Box 500                                        
+******             Mail Stop 220                                      
+******             Batavia, IL   60510                                
+******                                                                
+******             Phone: (630) 840 4956                              
+******             Email: michelotti@fnal.gov                         
+******                                                                
+******  Usage, modification, and redistribution are subject to terms          
+******  of the License and the GNU General Public License, both of
+******  which are supplied with this software.
+******                                                                
+**************************************************************************
+*************************************************************************/
+
+
 #include "marker.h"
 
 void marker::localPropagate( Particle& ) {
