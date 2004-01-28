f79392dee4af29e855be8b9be23d199556a7abbb
michelot <michelot>
2004-01-28 16:54:50 +0000

    Added exceptions; removed exit statements.   - lpjm

--- mxyzptlk/src/JetVector.cc
+++ mxyzptlk/src/JetVector.cc
@@ -106,16 +106,11 @@ JetVector::JetVector( const JetVector& x )
                             // of lazy evaluation.
     #ifndef NOCHECKS
     if( comp[i].Env() != myEnv ) {
-      cerr << "\n" 
-           << "*** ERROR ***                                           \n" 
-              "*** ERROR *** JetVector::JetVector( JetVector& )        \n"          
-              "*** ERROR *** Inconsistent environment at component "
-           << i << "\n"
-              "*** ERROR *** "
-           << (int) comp[i].Env() << " != " << (int) myEnv << "\n"
-              "*** ERROR ***                                           \n" 
-           << endl;
-      exit(0);
+      throw( JL::BadEnvironment( 
+               i, (int) comp[i].Env(), (int) myEnv, 
+               __FILE__, __LINE__, 
+               "JetVector::JetVector( const JetVector& )",
+               "Inconsistent environment at a component." ));
     }
     #endif
   }
