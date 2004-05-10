f3279e1494012bf62fcca6fee2b1533bf792ac4f
michelot <michelot>
2004-05-10 15:43:10 +0000

    These were written over a year ago for use with a longitudinal phase space tomography project. They perform simple interpolation and one- dimensional integration. Included are a linear spline, a simple cubic spline, and an Akima spline.
    Leo Michelotti

---
+++ integrator/src/Linterpoland.cc
@@ -0,0 +1,88 @@
+#include "Linterpoland.h"
+
+Linterpoland::Linterpoland()
+{
+}
+
+
+Linterpoland::~Linterpoland()
+{
+}
+
+
+double Linterpoland::operator()( const double& x )
+{
+  static int n;
+  static double p;
+
+  n = _nodes.size();
+
+  if( n < 2  )               { return 0.0;            }
+  if( x <= _nodes[0]._x )    { return _nodes[0]._y;   }
+  if( x >= _nodes[n-1]._x  ) { return _nodes[n-1]._y; }
+
+  int i = 0;
+  int im;
+  while( x > _nodes[i]._x ) { im = i; i++; }
+
+  
+  p = ( x - _nodes[im]._x ) / ( _nodes[i]._x - _nodes[im]._x );
+
+  return (  (1.0-p) * _nodes[im]._y  + p * _nodes[i]._y  );
+}
+
+
+double Linterpoland::derivative( const double& x )
+{
+  static int n;
+  static double p;
+
+  n = _nodes.size();
+
+  if( n < 2  ||  x <= _nodes[0]._x  ||  x >= _nodes[n-1]._x  ) 
+  { return 0; }
+
+  int i = 0;
+  int im;
+  while( x > _nodes[i]._x ) { im = i; i++; }
+
+  return ( ( _nodes[i]._y - _nodes[im]._y ) / 
+           ( _nodes[i]._x - _nodes[im]._x )   );
+}
+
+
+int Linterpoland::acceptNode( double x, double y )
+{
+  Node w;
+  w._x = x;
+  w._y = y;
+
+  _nodes.push_back(w);
+  _sort();
+  
+  return 0;
+}
+
+
+
+bool Linterpoland::Node::operator<( const Node& n ) const
+{
+  return ( _x < n._x );
+}
+
+
+bool Linterpoland::Node::cmp::operator()( const Node& x, const Node& y ) const
+{
+  return ( x < y );
+}
+
+
+void Linterpoland::_sort()
+{
+  std::vector<Node>::iterator first = _nodes.begin();
+  std::vector<Node>::iterator last  = _nodes.end();
+
+  Node::cmp lessThan;
+
+  std::sort( first, last, lessThan );
+}
---
+++ integrator/src/Simpson.cc
@@ -0,0 +1,32 @@
+#include "Simpson.h"
+
+Simpson::Simpson()
+{
+}
+
+
+double Simpson::integrate( double t_i, double t_f ) const
+{
+  if( t_f == t_i ) { return 0.0; }
+  if( t_f < t_i  ) { return -(this->integrate( t_f, t_i )); }
+
+  int i;
+  int n = _integrator->getNStep();
+  double dt = ( t_f - t_i ) / ((double) n);
+  double f[n+1];
+  double t = t_i;
+  for( i = 0; i <=  n; i++ ) {
+    f[i] = _integrator->getValue( t );
+    t += dt;
+  }
+
+  double ret = 0.0;
+  for( i = 1; i < n; i++ ) {
+    ret += f[i];
+  }
+  ret += ( f[0] + f[n] )/2.0;
+  ret *= dt;
+
+  return ret;
+}
+
