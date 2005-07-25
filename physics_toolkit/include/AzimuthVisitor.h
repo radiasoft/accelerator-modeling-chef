3373d24ff7fd33a5abd39a90b354135aa95d8f4b
ostiguy <ostiguy>
2005-07-25 16:30:42 +0000

    Added AzimuthVisitor and ElementTagVisitor classes. These classes are used to support database tag association based on azimuth coordinates.
    -jfo

---
+++ physics_toolkit/include/AzimuthVisitor.h
@@ -0,0 +1,76 @@
+/*************************************************************************
+**************************************************************************
+**************************************************************************
+******
+******  BEAMLINE : C++ objects for design and analysis
+******             of beamlines, storage rings, and   
+******             synchrotrons
+******
+******  File:      AzimuthVisitor.h
+******
+******  Copyright (c) Universities Research Association, Inc.
+******                All Rights Reserved
+******
+******  Author:    Jean-Francois Ostiguy
+******             ostiguy@fnal.gov
+****** 
+******  Usage, modification, and redistribution are subject to terms          
+******  of the License supplied with this software.
+******  
+******  Software and documentation created under 
+******* U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******* The U.S. Government retains a world-wide non-exclusive, 
+******* royalty-free license to publish or reproduce documentation 
+******* and software for U.S. Government purposes. This software 
+******* is protected under the U.S. and Foreign Copyright Laws. 
+******* URA/FNAL reserves all rights.
+*******                                                                
+*******
+**************************************************************************
+**************************************************************************
+**************************************************************************
+*************************************************************************/
+
+
+#ifndef AZIMUTHVISITOR_H
+#define AZIMUTHVISITOR_H
+
+#include <BmlVisitor.h>
+#include <string>
+
+class Particle;
+class beamline;
+class bmlnElmnt;
+
+
+class AzimuthVisitor: public BmlVisitor {
+
+public: 
+
+  AzimuthVisitor(Particle* p, 
+                 std::string origin_tag       = "ORIGIN",
+                 std::string azimuth_up_tag   = "azimuth_up", 
+                 std::string azimuth_down_tag = "azimuth_down");
+ 
+  virtual ~AzimuthVisitor();
+
+
+  void setAzimuthUpTag(std::string s)   {  _azimuth_up_tag      = s; }
+  void setAzimuthDownTag(std::string s) {  _azimuth_down_tag    = s; }
+  void setOriginTag(std::string s)      {  _origin_tag          = s; }
+
+  virtual void visitBeamline( beamline* e);   
+  virtual void visitBmlnElmnt( bmlnElmnt* e); 
+
+
+private:
+
+ Particle*          _p;
+ std::string        _azimuth_up_tag;
+ std::string        _azimuth_down_tag;
+ std::string        _origin_tag;
+ double             _azimuth;
+
+};
+
+#endif
