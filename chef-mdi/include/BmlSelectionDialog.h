10f73df18b40062a5169c536404ff76cee88d9bb
ostiguy <ostiguy>
2005-08-10 18:58:48 +0000

    Improved parser error recovery. -jfo

--- chef-mdi/include/BmlSelectionDialog.h
+++ chef-mdi/include/BmlSelectionDialog.h
@@ -1,7 +1,36 @@
+/*************************************************************************
+**************************************************************************
+**************************************************************************
+******
+******  CHEF:      An application layered on the Beamline/mxyzptlk
+******             class libraries.
+******
+******  File:      BmlSelectionDialog.h
+******
+******  Copyright (c) Universities Research Association, Inc.
+******                All Rights Reserved
+******
+******  Author:    Jean-Francois Ostiguy
+******             Fermilab
+******             ostiguy@fnal.gov
+******
+******  Usage, modification, and redistribution are subject to terms          
+******  of the License supplied with this software.
+******  
+******  Software and documentation created under 
+******* U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******* The U.S. Government retains a world-wide non-exclusive, 
+******* royalty-free license to publish or reproduce documentation 
+******* and software for U.S. Government purposes. This software 
+******* is protected under the U.S. and Foreign Copyright Laws. 
+******* URA/FNAL reserves all rights.
+*******                                                                
+**************************************************************************
+
 #ifndef BMLSELECTIONDIALOG_H
 #define BMLSELECTIONDIALOG_H
 
-#include "bmlselectiondialogbase.h"
+#include <bmlselectiondialogbase.h>
 
 #include <list>
 #include <string>
@@ -17,7 +46,7 @@ class BmlSelectionDialog: public BmlSelectionDialogBase {
  public:
 
   BmlSelectionDialog(QWidget* parent=0, const char* name=0, WFlags f=0);
-  void setList(     std::list<std::string>& bmllist);
+  void setList( std::list<std::string>& bmllist, const char* use_name=0);
   void getSelected();
   void setBeamParameters(const bmlfactory& bf); 
   double getBRHO() {return _brho;}
