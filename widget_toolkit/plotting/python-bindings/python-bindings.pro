ca7343503f80e9cc912459e83e73d7578b867034
ostiguy <ostiguy>
2005-08-25 18:14:20 +0000

    Python bindings for the plot widget have been moved to the plot widget hierarchy.
    -jfo

---
+++ widget_toolkit/plotting/python-bindings/python-bindings.pro
@@ -0,0 +1,44 @@
+###################################################################
+#
+# python-bindings.pro
+#
+# python bindings for the chefplot widget
+#
+# Author: Jean-Francois Ostiguy
+#         Accelerator Division
+#         Fermi National Accelerator Laboratory 
+#         ostiguy@fnal.gov
+#
+########################################################################
+
+TEMPLATE	= lib
+LANGUAGE	= C++
+TARGET          = pychefplot
+
+unix:CONFIG	+= qt warn_on debug thread rtti exceptions dll
+
+#
+# Note: the relative path is broken under UNIX. Forward slashes do not seem to work, but win32 style backslashes are OK.
+#
+!include ( "..\..\..\chef-config\config.pri" ) {
+        message( "Error: failed to find global configuration file config.pri" ) 
+}
+
+
+SOURCES	+= ./src/py-chefplot-module.cpp \
+           ./src/py-chefplotmain.cpp \
+           ./src/py-chefcurve.cpp \
+           ./src/py-chefplotdata.cpp
+
+
+INCLUDEPATH += ../chefplot/include
+INCLUDEPATH += ../chefplot/src/ui
+INCLUDEPATH +=  $$FNAL_INSTALL_TOP_DIR/include/python-bindings
+
+INCLUDEPATH += $$PYTHON_INC
+
+LIBS	+= -L$$FNAL_LIBDIR -lbmlfactory -lphysics_toolkit -lbeamline -lmxyzptlk -lbasic_toolkit -L$$GLIB_LIBDIR -lglib-2.0
+LIBS    += -lqwt
+
+unix:LIBS  +=  $$QMAKE_RPATH$$FNAL_LIBDIR $$QMAKE_RPATH$$GLIB_LIBDIR
+
---
+++ widget_toolkit/plotting/python-bindings/src/py-chefplotmain.cpp
@@ -0,0 +1,71 @@
+/***************************************************************************                                                               
+******  Boost.python Python bindings for mxyzpltk/beamline libraries 
+******  
+******                                    
+******  File:      py-chefplotmain.cpp
+******                                                                
+******  Copyright (c) Universities Research Association, Inc./ Fermilab    
+******                All Rights Reserved                             
+******
+******  Software and documentation created under 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  The U.S. Government retains a world-wide non-exclusive, 
+******  royalty-free license to publish or reproduce documentation 
+******  and software for U.S. Government purposes. This software 
+******  is protected under the U.S.and Foreign Copyright Laws. 
+******                                                                
+******  Author:    Jean-Francois Ostiguy                                     
+******                                                                
+******             Fermi National Laboratory, Batavia, IL   60510                                
+******             ostiguy@fnal.gov                         
+******
+****************************************************************************/
+#include <boost/python.hpp>
+#include <chefplotmain.h>
+
+using namespace boost::python;
+ 
+class CHEFPlotMainWrapper: public CHEFPlotMain {
+
+public:
+
+   CHEFPlotMainWrapper(PyObject * self): _self(self) {}
+
+   virtual ~CHEFPlotMainWrapper( )
+   {  
+   
+
+#if 0
+   boost::python::handle<> hdl (borrowed( _self) );
+   object o(hdl);
+   boost::python::extract<boost::shared_ptr<CHEFPlotMainWrapper> > x (o);    
+
+   if ( x.check() ) {
+       std::auto_ptr<CHEFPlotMainWrapper> ap = x();
+       ap.release();
+       }
+#endif
+
+   }
+
+   private:
+  
+   PyObject* _self;
+
+
+};
+
+void wrap_chefplot_CHEFPlotMain( )
+{ 
+
+   void  (CHEFPlotMain::*resize_int_int)(int, int)    = &CHEFPlotMain::resize;
+  
+   class_ <CHEFPlotMain, boost::shared_ptr<CHEFPlotMainWrapper>, boost::noncopyable> ("CHEFPlotMain", init<>( ) )
+     .def("addData",  &CHEFPlotMain::addData)
+     .def("show"   ,  &CHEFPlotMain::show)
+     .def("hide"   ,  &CHEFPlotMain::hide)
+     .def("resize" ,  resize_int_int)
+ ;
+
+}
+
