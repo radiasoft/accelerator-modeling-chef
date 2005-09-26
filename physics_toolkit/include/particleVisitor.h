25288108bc9405cfb71b340a64d1f30ae24e1f05
ostiguy <ostiguy>
2005-09-26 19:26:55 +0000

    Eliminated forward class declarations based on 2 template arguments. Use convenience (typdefs) headers instead. -jfo

--- physics_toolkit/include/particleVisitor.h
+++ physics_toolkit/include/particleVisitor.h
@@ -9,9 +9,19 @@
 ******                                    
 ******  File:      particleVisitor.h
 ******                                                                
-******  Copyright (c) 2001  Universities Research Association, Inc.   
+******  Copyright (c) Universities Research Association, Inc. / Fermilab     
 ******                All Rights Reserved                             
-******                                                                
+******
+******  Usage, modification, and redistribution are subject to terms          
+******  of the License supplied with this software.
+******  
+******  Software and documentation created under 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  The U.S. Government retains a world-wide non-exclusive, 
+******  royalty-free license to publish or reproduce documentation 
+******  and software for U.S. Government purposes. This software 
+******  is protected under the U.S. and Foreign Copyright Laws. 
+******                                                                 
 ******  Author:    Leo Michelotti                                     
 ******                                                                
 ******             Fermilab                                           
@@ -22,9 +32,6 @@
 ******             Phone: (630) 840 4956                              
 ******             Email: michelotti@fnal.gov                         
 ******                                                                
-******  Usage, modification, and redistribution are subject to terms          
-******  of the License and the GNU General Public License, both of
-******  which are supplied with this software.
 ******                                                                
 **************************************************************************
 *************************************************************************/
@@ -44,17 +51,13 @@
 #ifndef PARTICLEVISITOR_H
 #define PARTICLEVISITOR_H
 
-#include "BmlVisitor.h"
+#include <BmlVisitor.h>
+#include <Jet.h>
 
 class Particle;
 class JetParticle;
 
-template<typename T1, typename T2> 
-class TJet;
-typedef TJet<double,FNAL::Complex> Jet;
-// class Jet;
 
-// ---------------------------
 class particleVisitor : public BmlVisitor {
  protected:
   Particle* particle;
