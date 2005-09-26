e15571eb6c691143cbb66c3344bef93e49d2f93c
ostiguy <ostiguy>
2005-09-26 19:26:55 +0000

    Eliminated forward class declarations based on 2 template arguments. Use convenience (typdefs) headers instead. -jfo

--- physics_toolkit/include/JacobianVisitor.h
+++ physics_toolkit/include/JacobianVisitor.h
@@ -9,9 +9,19 @@
 ******                                    
 ******  File:      JacobianVisitor.h
 ******                                                                
-******  Copyright (c) 2001  Universities Research Association, Inc.   
+******  Copyright (c) Universities Research Association, Inc. / Fermilab     
 ******                All Rights Reserved                             
-******                                                                
+******
+******  Usage, modification, and redistribution are subject to terms          
+******  of the License supplied with this software.
+******  
+******  Software and documentation created under 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  The U.S. Government retains a world-wide non-exclusive, 
+******  royalty-free license to publish or reproduce documentation 
+******  and software for U.S. Government purposes. This software 
+******  is protected under the U.S. and Foreign Copyright Laws. 
+******                                       
 ******  Author:    Leo Michelotti                                     
 ******                                                                
 ******             Fermilab                                           
@@ -22,9 +32,6 @@
 ******             Phone: (630) 840 4956                              
 ******             Email: michelotti@fnal.gov                         
 ******                                                                
-******  Usage, modification, and redistribution are subject to terms          
-******  of the License and the GNU General Public License, both of
-******  which are supplied with this software.
 ******                                                                
 **************************************************************************
 *************************************************************************/
@@ -33,7 +40,7 @@
 #ifndef JACOBIANVISITOR_H
 #define JACOBIANVISITOR_H
 
-#include "particleVisitor.h"
+#include <particleVisitor.h>
 
 class beamline;
 
