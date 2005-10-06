998aa1a36ec662478bc09aacf747e0ff3e795d2e
ostiguy <ostiguy>
2005-10-06 14:45:39 +0000

    Eliminated use of the obsolete strstream class.
    -jfo

--- widget_toolkit/interpreter/src/ioredirector.cpp
+++ widget_toolkit/interpreter/src/ioredirector.cpp
@@ -33,7 +33,8 @@
 *************************************************************************/
 #include <ioredirector.h>
 #include <iostream>
-#include <strstream>
+#include <fstream>
+#include <sstream>
 #include <qstring.h>
 
 
@@ -42,17 +43,21 @@ IORedirector::IORedirector()  {
   
 }
 
+
+
 void IORedirector::read( const char* s) {
 
   char line[256];
-  std::strstream inout;
+  std::stringstream inout;
   inout.getline (line, sizeof(line) );
   s = line;
+
   
 }
 
 
 void IORedirector::write( const char* s) {
+
   QString qs= QString( s );
   emit textReady( qs );
   
