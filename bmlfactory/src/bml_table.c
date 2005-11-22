36703c157bfbd4bbb912b6affcc5439101539953
ostiguy <ostiguy>
2005-11-22 22:56:50 +0000

    copyright notice update.
    -jfo

--- bmlfactory/src/bml_table.c
+++ bmlfactory/src/bml_table.c
@@ -1,6 +1,3 @@
-#if HAVE_CONFIG_H
-#include <config.h>
-#endif
 /*************************************************************************
 **************************************************************************
 **************************************************************************
@@ -12,9 +9,19 @@
 ******                                    
 ******  File:      bml_table.c
 ******                                                                
-******  Copyright (c) 1999  Universities Research Association, Inc.   
+******  Copyright (c) Universities Research Association, Inc.   
 ******                All Rights Reserved                             
-******                                                                
+******
+******  Usage, modification, and redistribution are subject to terms          
+******  of the License supplied with this software.
+******  
+******  Software and documentation created under 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  The U.S. Government retains a world-wide non-exclusive, 
+******  royalty-free license to publish or reproduce documentation 
+******  and software for U.S. Government purposes. This software 
+******  is protected under the U.S. and Foreign Copyright Laws. 
+******                                                        
 ******  Author:    Dmitri Mokhov and Oleg Krivosheev                  
 ******                                                                
 ******  Contact:   Leo Michelotti or Jean-Francois Ostiguy            
@@ -29,12 +36,12 @@
 ******             Email: michelotti@fnal.gov                         
 ******                    ostiguy@fnal.gov                            
 ******                                                                
-******  Usage, modification, and redistribution are subject to terms          
-******  of the License and the GNU General Public License, both of
-******  which are supplied with this software.
 ******                                                                
 **************************************************************************
 *************************************************************************/
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 
    /* -*- C -*- */
