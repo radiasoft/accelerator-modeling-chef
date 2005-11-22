7c304a81b3946abf2efa6afc986cd6b7e93fadca
ostiguy <ostiguy>
2005-11-22 21:20:05 +0000

    Initial commit to CVS. headers for misc standalone utility functions.
    -jfo

---
+++ basic_toolkit/include/utils.h
@@ -0,0 +1,47 @@
+/*************************************************************************
+**************************************************************************
+**************************************************************************
+******                                                                
+******  BASIC TOOLKIT:  Low level utility C++ classes.
+******                                    
+******  File:      utils.h
+******                                                                
+******  Copyright  Universities Research Association, Inc./Fermilab    
+******             All Rights Reserved                             
+******                                                                
+******  Authors:    Leo Michelotti        michelotti@fnal.gov                                    
+******              Jean-Francois Ostiguy ostiguy@fnal.gov                                                    
+****** 
+****** Copyright Universities Research Association, Inc./ Fermilab    
+******            All Rights Reserved                             
+*******
+******  Usage, modification, and redistribution are subject to terms          
+******  of the License supplied with this software.
+******  
+******  Software and documentation created under 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  The U.S. Government retains a world-wide non-exclusive, 
+******  royalty-free license to publish or reproduce documentation 
+******  and software for U.S. Government purposes. This software 
+******  is protected under the U.S. and Foreign Copyright Laws. 
+******                                                                
+**************************************************************************
+*************************************************************************/
+
+/* A collection of misc utility functions */ 
+
+#ifndef UTILS_H
+#define UTILS_H
+
+bool nexcom( int, int, int* );  // Computes the next composition
+                                //  of an integer into a number of parts.
+                                //  Algorithm devised by Herbert Wilf.
+
+extern "C" {
+ int bcfRec( int, int );        // Recursive evaluation of binomial
+                                //  coefficient.
+ int nearestInteger( double );  // Returns the integer nearest to its
+                                //  double argument.
+}
+
+#endif // UTILS_H 
