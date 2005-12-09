1e9f9c5b1b7ddf14013b84a917cc5425371ed8dd
ostiguy <ostiguy>
2005-12-09 18:43:08 +0000

    Updated copyright header.
    -jfo

--- basic_toolkit/include/MathConstants.h
+++ basic_toolkit/include/MathConstants.h
@@ -21,8 +21,14 @@
 ******             Email: michelotti@fnal.gov                         
 ******                                                                
 ******  Usage, modification, and redistribution are subject to terms          
-******  of the License and the GNU General Public License, both of
-******  which are supplied with this software.
+******  of the License supplied with this software.
+******  
+******  Software and documentation created under 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  The U.S. Government retains a world-wide non-exclusive, 
+******  royalty-free license to publish or reproduce documentation 
+******  and software for U.S. Government purposes. This software 
+******  is protected under the U.S. and Foreign Copyright Laws.
 ******                                                                
 **************************************************************************
 *************************************************************************/
