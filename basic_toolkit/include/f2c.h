dc47cc675bf63e0f8c7dc1a503890ca990f13bbb
ostiguy <ostiguy>
2005-12-09 18:43:08 +0000

    Updated copyright header.
    -jfo

--- basic_toolkit/include/f2c.h
+++ basic_toolkit/include/f2c.h
@@ -1,28 +1,30 @@
 /*************************************************************************
 **************************************************************************
 **************************************************************************
+******
+******  File: f2.h
 ******                                                                
-******  BASIC TOOLKIT:  Low level utility C++ classes.
-******  Version:   4.0                    
-******                                    
-******  File:      f2c.h
+****** Copyright 1990, 1993, 1994, 2000 by AT&T, 
+****** Lucent Technologies and Bellcore.
+****** Permission to use, copy, modify, and distribute this software
+****** and its documentation for any purpose and without fee is hereby
+****** granted, provided that the above copyright notice appear in all
+****** copies and that both that the copyright notice and this
+****** permission notice and warranty disclaimer appear in supporting
+****** documentation, and that the names of AT&T, Bell Laboratories,
+****** Lucent or Bellcore or any of their entities not be used in
+****** advertising or publicity pertaining to distribution of the
+****** software without specific, written prior permission.
+******
+****** AT&T, Lucent and Bellcore disclaim all warranties with regard to
+****** this software, including all implied warranties of
+****** merchantability and fitness.  In no event shall AT&T, Lucent or
+****** Bellcore be liable for any special, indirect or consequential
+****** damages or any damages whatsoever resulting from loss of use,
+****** data or profits, whether in an action of contract, negligence or
+****** other tortious action, arising out of or in connection with the
+****** use or performance of this software.
 ******                                                                
-******  Copyright (c) 1990 Universities Research Association, Inc.    
-******                All Rights Reserved                             
-******                                                                
-******  Author:    Leo Michelotti                                     
-******                                                                
-******             Fermilab                                           
-******             P.O.Box 500                                        
-******             Mail Stop 220                                      
-******             Batavia, IL   60510                                
-******                                                                
-******             Phone: (630) 840 4956                              
-******             Email: michelotti@fnal.gov                         
-******                                                                
-******  Usage, modification, and redistribution are subject to terms          
-******  of the License and the GNU General Public License, both of
-******  which are supplied with this software.
 ******                                                                
 **************************************************************************
 *************************************************************************/
