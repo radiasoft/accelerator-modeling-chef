46b66940b87ec141b296372036c9efcde474791d
ostiguy <ostiguy>
2005-12-12 19:08:37 +0000

    initial import -jfo

---
+++ chef-mdi/input_files/jetsqrt.py
@@ -0,0 +1,16 @@
+#
+mxyzptlk.BeginEnvironment(12)
+x =  mxyzptlk.coord(1.0)
+y =  mxyzptlk.coord(1.0)
+z =  mxyzptlk.coord(1.0)
+px = mxyzptlk.coord(0.0)
+py = mxyzptlk.coord(0.0)
+pz = mxyzptlk.coord(0.0)
+mxyzptlk.EndEnvironment()
+#
+
+print mxyzptlk.sqrt(x)
+print mxyzptlk.sqrt(x)*mxyzptlk.sqrt(x)
+
+
+
