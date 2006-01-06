6d8a36cdaa5b30f940dddd70449360abab7cfc10
ostiguy <ostiguy>
2006-01-06 17:03:22 +0000

    Eliminated obsolete code, mostly workarounds for old non-ANSI compliant compilers.
    -jfo

--- basic_toolkit/include/complexAddon.h
+++ basic_toolkit/include/complexAddon.h
@@ -3,26 +3,37 @@
 **************************************************************************
 ******                                                                
 ******  BASIC TOOLKIT:  Low level utility C++ classes.
-******  Version:   4.0                    
 ******                                    
 ******  File:      complexAddon.h
 ******                                                                
-******  Copyright (c) 1990 Universities Research Association, Inc.    
-******                All Rights Reserved                             
+******  Copyright Universities Research Association, Inc./ Fermilab    
+******            All Rights Reserved                             
+*******
+******  Usage, modification, and redistribution are subject to terms          
+******  of the License supplied with this software.
+******  
+******  Software and documentation created under 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  The U.S. Government retains a world-wide non-exclusive, 
+******  royalty-free license to publish or reproduce documentation 
+******  and software for U.S. Government purposes. This software 
+******  is protected under the U.S. and Foreign Copyright Laws. 
 ******                                                                
 ******  Author:    Leo Michelotti                                     
 ******                                                                
 ******             Fermilab                                           
-******             P.O.Box 500                                        
-******             Mail Stop 220                                      
 ******             Batavia, IL   60510                                
+******             michelotti@fnal.gov
+******
 ******                                                                
-******             Phone: (630) 840 4956                              
-******             Email: michelotti@fnal.gov                         
-******                                                                
-******  Usage, modification, and redistribution are subject to terms          
-******  of the License and the GNU General Public License, both of
-******  which are supplied with this software.
+****** Revision History:
+******
+******  Dec 2005  
+******
+******  Jean-Francois Ostiguy, ostiguy@fnal.gov
+****** 
+******  - eliminated support for obsolete compilers. 
+******    Now assuming ANSI std C++.  
 ******                                                                
 **************************************************************************
 *************************************************************************/
@@ -31,135 +42,35 @@
 #ifndef COMPLEXADDON_H
 #define COMPLEXADDON_H
 
-// This is written only to avoid constant warning messages
-// about ambiguous operators.  It arises because of constructors
-// which can change double and complex to matrices or jets.
-// 
-// Leo Michelotti
-// Dec 16, 1994    (Battle of the Bulge + 50 years)
-
 #include <complex>
-#include <cmath>
-#if !(defined(__SUNPRO_CC) && defined(IOSTREAMH))
-#include <iostream>
-#endif
-
-namespace FNAL {
-typedef std::complex<double> Complex;
-}
-using std::real;
-using std::imag;
-using std::exp;
-using std::conj;
-using std::cos;
-using std::sin;
-using std::pow;
-using std::sqrt;
-using std::ostream;
-using std::cerr;
-using std::endl;
-
-//#if defined(__SUNPRO_CC) 
-//#ifdef __BORLANDC__
-//#ifdef _MSC_VER
 
 
-// -----------------------------------------------------------
+const std::complex<double> complex_1( 1.0, 0.0 );
+const std::complex<double> complex_0( 0.0, 0.0 );
+const std::complex<double> complex_i( 0.0, 1.0 );
+
+
+// ----------------------------------------------------------------------
 // The following operators are used by the mxyzptlk library
 // They define a partial ordering that is used to sort 
 // DA variables. DO NOT modify this unless you understand
 // what you are doing !
+//-----------------------------------------------------------------------
 
-inline char operator>= ( const FNAL::Complex& x, const FNAL::Complex& y )
+inline bool operator>= ( const std::complex<double>& x, const std::complex<double>& y )
 {
- return real(x) >= real(y)  &&  imag(x) >= imag(y);
+ return std::real(x) >= std::real(y)  &&  std::imag(x) >= std::imag(y);
 }
 
-inline char operator< ( const FNAL::Complex& x, const FNAL::Complex& y )
+inline bool operator< ( const std::complex<double>& x, const std::complex<double>& y )
 {
- return real(x) < real(y)  &&  imag(x) < imag(y);
+ return std::real(x) < std::real(y)  &&  std::imag(x) < std::imag(y);
 }
 
-inline char operator> ( const FNAL::Complex& x, const FNAL::Complex& y )
+inline bool operator> ( const std::complex<double>& x, const std::complex<double>& y )
 {
- return real(x) > real(y)  &&  imag(x) > imag(y);
+ return std::real(x) > std::real(y)  &&  std::imag(x) > std::imag(y);
 }
 
-// -----------------------------------------------------------
-
-#if defined(__SUNPRO_CC) && defined(IOSTREAMH)
-#include <iostream.h>
-// this is a kludge to allow the std::complex type to be used in conjunction 
-// with the old style AT&T iostream library
- 
-
-inline ostream& operator<<(ostream& os, const FNAL::Complex& x) {
-	os << "( " << std::real(x) << "," << std::imag(x) << " )";
-	return os;
-}
-
-inline istream& operator>>(istream& is, FNAL::Complex& x) {
- double u,v;
-	is  >> u; 
-        is  >> v;
-        std::complex<double> z(u,v);
-        x = z;
-	return is;
-}
-#endif 
-
-
-#ifdef __BORLANDC__
-
-inline FNAL::Complex pow ( const FNAL::Complex& x, int n )
-{
-  return pow( x, (double) n );
-}
-
-inline double pow( const double& x, int n )
-{
-  return pow( x, (double) n );
-}
-
-inline bool operator==( const FNAL::Complex& x, const double& y )
-{
-  if( ( imag(x) != 0.0 ) || ( real(x) != y ) )   return 0;
-  else                                           return 1;
-}
-
-inline bool operator==( const double& y, const FNAL::Complex& x )
-{
-  if( ( imag(x) != 0.0 ) || ( real(x) != y ) )   return 0;
-  else                                           return 1;
-}
-
-inline bool operator!=( const FNAL::Complex& x, const double& y )
-{
-  return !( operator==( x, y ) );
-}
-
-inline bool operator!=( const double& y, const FNAL::Complex& x )
-{
-  return !( operator==( x, y ) );
-}
-
-#endif // __BORLANDC__
-
-#ifdef _MSC_VER
-
-inline double abs(const FNAL::Complex& x) {
-	return sqrt(real(x)*real(x) + imag(x)*imag(x));
-}
-
-//ostream& operator<<(ostream& os, const FNAL::Complex& x) {
-//	os << "( " << real(x) << "," << imag(x) << " )" << std::endl;
-//	return os;
-//}
-
-#endif // _MSC_VER
-
-const FNAL::Complex complex_1( 1.0, 0.0 );
-const FNAL::Complex complex_0( 0.0, 0.0 );
-const FNAL::Complex complex_i( 0.0, 1.0 );
 
 #endif // COMPLEXADDON_H
