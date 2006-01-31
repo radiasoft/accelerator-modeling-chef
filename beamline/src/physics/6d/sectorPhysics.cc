7e764944faf165fb59f2f1ed76ba6188d9886350
ostiguy <ostiguy>
2006-01-31 18:49:40 +0000

    Updated copyright headers.
    -jfo

--- beamline/src/physics/6d/sectorPhysics.cc
+++ beamline/src/physics/6d/sectorPhysics.cc
@@ -11,7 +11,7 @@
 ******                                                                
 ******  Copyright Universities Research Association, Inc./ Fermilab    
 ******            All Rights Reserved                             
-******
+*****
 ******  Usage, modification, and redistribution are subject to terms          
 ******  of the License supplied with this software.
 ******  
@@ -20,7 +20,7 @@
 ******  The U.S. Government retains a world-wide non-exclusive, 
 ******  royalty-free license to publish or reproduce documentation 
 ******  and software for U.S. Government purposes. This software 
-******  is protected under the U.S. and Foreign Copyright Laws. 
+******  is protected under the U.S. and Foreign Copyright Laws.
 ******                                                                
 ******  Author:    Leo Michelotti                                     
 ******                                                                
@@ -30,8 +30,7 @@
 ******             Batavia, IL   60510                                
 ******                                                                
 ******             Phone: (630) 840 4956                              
-******             Email: michelotti@fnal.gov                         
-******                                                                
+******             Email: michelotti@fnal.gov  
 ******                                                                
 **************************************************************************
 *************************************************************************/
