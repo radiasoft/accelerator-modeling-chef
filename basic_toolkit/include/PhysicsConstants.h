e3dd8e8c8ce1a9ac8b9577203d3e2c19927f954b
michelot <michelot>
2006-04-17 13:07:15 +0000

    Upgraded value of charge on electron, according to NIST.    - lpjm

--- basic_toolkit/include/PhysicsConstants.h
+++ basic_toolkit/include/PhysicsConstants.h
@@ -52,10 +52,12 @@
 #define PH_NORM_me         0.5110034e-3       /* Mass of electron [GeV/c^2]          */
 #define PH_NORM_mmu        0.105658387        /* mass of muon [GeV/c^2]              */
 
-/* #define PH_MKS_e           1.6021892e-19 */   /* Proton charge   [Coulombs]          */
-/* #define PH_ESU_e           4.803242e-10  */   /* Proton charge   [esu]               */ 
-
-#define PH_MKS_e           1.602176462e-19    /* Proton charge   [Coulombs]          */
+/*  Old values
+#define PH_ESU_e           4.803242e-10          Proton charge   [esu]
+#define PH_MKS_e           1.6021892e-19         Proton charge   [Coulombs]
+#define PH_MKS_e           1.602176462e-19    Proton charge   [Coulombs]
+*/
+#define PH_MKS_e           1.60217653e-19     /* Proton charge   [Coulombs]          */
 #define PH_MKS_c           2.99792458e8       /* Speed of light  [m/sec]             */
 #define PH_MKS_h           6.62606876e-34     /* Planck's constant [Joule-sec]       */
 #define PH_MKS_omega_p     9.578756e7         /* Proton cyclotron frequence          */
