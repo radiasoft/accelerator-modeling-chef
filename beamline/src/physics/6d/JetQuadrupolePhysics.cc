886c77b38ed5992990514e833415ecc4ba98078a
michelot <michelot>
2006-05-10 15:50:26 +0000

    *** empty log message ***

--- beamline/src/physics/6d/JetQuadrupolePhysics.cc
+++ beamline/src/physics/6d/JetQuadrupolePhysics.cc
@@ -121,42 +121,39 @@ JetQuadrupole::TPOT_Prop  JetQuadrupole::LikeTPOT(4);
 
 void JetthinQuad::localPropagate( Particle& p ) {
  // "Strength" is B'l in Tesla
- static double xpr, ypr, p3divpbar, state5p1;
+ double TrState[6];
+ double k = 0;
 
- state5p1 = 1.0 + p.state[5];
- p3divpbar = sqrt( state5p1 * state5p1
-                 - p.state[3]*p.state[3] 
-                 - p.state[4]*p.state[4] );
- xpr = p.state[3] / p3divpbar; 
- ypr = p.state[4] / p3divpbar; 
+ if( strength != 0.0 ) 
+ {
+   memcpy((void *)TrState, (const void *)p.state, 6 * sizeof(double));
 
- xpr += - strength * p.state[0] / p.BRho();
- ypr +=   strength * p.state[1] / p.BRho();
+   k = strength / p.ReferenceBRho();
+   TrState[3] += - k * TrState[0];
+   TrState[4] +=   k * TrState[1];
 
- p3divpbar = state5p1 / sqrt( 1.0 + xpr*xpr + ypr*ypr );
+   p.state[3] = TrState[3];
+   p.state[4] = TrState[4];
 
- p.state[3] = xpr*p3divpbar;
- p.state[4] = ypr*p3divpbar;
+ }
 }
 
 void JetthinQuad::localPropagate( JetParticle& p ) {
- Jet dummy;
- Jet xpr, ypr, p3divpbar;
+ Jet TrState[6];
 
- p3divpbar = sqrt( pow( 1.0 + p.state(5), 2.0 )
-                 - p.state(3)*p.state(3) 
-                 - p.state(4)*p.state(4) );
- xpr = p.state(3) / p3divpbar; 
- ypr = p.state(4) / p3divpbar; 
+ int i = 0;
+ if( strength != 0.0 ) 
+ {
+   for( i = 0; i < 6; i++  ) {
+     TrState[i] = p.state(i);
+   }
 
- xpr += - KL * p.state(0) / p.BRho();
- ypr +=   KL * p.state(1) / p.BRho();
+   Jet k( (1.0/ p.ReferenceBRho()) * JetStrength  );
+   TrState[3] += - k * TrState[0];
+   TrState[4] +=   k * TrState[1];
 
- p3divpbar = ( 1.0 + p.state(5) ) / sqrt( 1.0 + xpr*xpr + ypr*ypr );
-
- dummy = xpr*p3divpbar;
- ( p.state ).SetComponent( 3, dummy );
- dummy = ypr*p3divpbar;
- ( p.state ).SetComponent( 4, dummy );
+   ( p.state ).SetComponent( 3, TrState[3] );
+   ( p.state ).SetComponent( 4, TrState[4] );
+ }
 }
 
