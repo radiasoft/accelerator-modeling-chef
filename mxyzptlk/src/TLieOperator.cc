a5154e81315480108a00d4a8d31b52435d99a725
michelot <michelot>
2006-08-08 14:57:46 +0000

    I believe this file is a necessary complement to TMapping.cc, but I could be wrong.                                              - lpjm

---
+++ mxyzptlk/src/TLieOperator.cc
@@ -0,0 +1,57 @@
+/*************************************************************************
+**************************************************************************
+**************************************************************************
+******                                                                
+******  Mxyzptlk:  A C++ implementation of differential algebra.      
+******                                    
+******  File:      TLieOperator.cc
+******                                                                
+******  Copyright Universities Research Association, Inc. / Fermilab   
+******                All Rights Reserved
+******
+******  Usage, modification, and redistribution are subject to terms          
+******  of the License supplied with this software.
+******  
+******  Software and documentation created under 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  The U.S. Government retains a world-wide non-exclusive, 
+******  royalty-free license to publish or reproduce documentation 
+*****   and software for U.S. Government purposes. This software 
+******  is protected under the U.S. and Foreign Copyright Laws. 
+******
+******  Author:    Leo Michelotti                                     
+******             Fermilab                                           
+******             Email: michelotti@fnal.gov                         
+******
+******  Revision History
+******   
+******  Feb 2005       Jean-Francois Ostiguy
+******                 ostiguy@fnal.gov 
+****** 
+****** - new memory management scheme 
+******                                                            
+******  Sep-Dec 2005  ostiguy@fnal.gov
+******  
+****** - refactored code to usea single class template parameter
+******   instead of two. Mixed mode operations now handled using 
+******   implicit conversion operators.
+****** - reference counting now based on using boost::intrusive pointer
+****** - reference counted TJetEnvironment
+****** - implementation details completely moved to TJL   
+****** - redesigned coordinate class Tcoord. New class Tparams for parameters
+****** - header files support for both explicit and implicit template instantiations
+******   (default for mxyzptlk = explicit)
+******   for implicit instantiations, define MXYZPTLK_IMPLICIT_TEMPLATES
+******
+**************************************************************************
+*************************************************************************/
+
+#include <GenericException.h>
+#include <iosetup.h>
+#include <TLieOperator.h>
+#include <complex>
+
+using FNAL::pcerr;
+using FNAL::pcout;
+
+using std::complex;
