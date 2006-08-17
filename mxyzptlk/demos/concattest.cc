12747a94a431e3d71e5d7d9dbc3d8c7aca86b019
michelot <michelot>
2006-08-17 15:47:46 +0000

    Minor differences: aesthetics only.             - lpjm

--- mxyzptlk/demos/concattest.cc
+++ mxyzptlk/demos/concattest.cc
@@ -6,7 +6,7 @@
 using namespace std;
 
 main() {
-static int index[] = { 3, 2 };
+int index[] = { 3, 2 };
 double   answer[2];
 
 Jet__environment::BeginEnvironment( 7 );
@@ -15,8 +15,7 @@ coord y( 0.0 );
 Jet__environment::EndEnvironment();
 
 Jet u, v, w, z;
-// Mapping a( 2, 0, x.Env() ), c( 2, 0, x.Env() );
-Mapping a( 2, 0, x.Env() );
+Mapping a( 2, 0 );
 
 u = x*y*y + exp( x + y );  v = cos( y*x*x ) / ( x + 2.0 );  
 w = sin(u) * cos(v);       z = exp( u*u*u ) / ( u*v );
@@ -28,28 +27,33 @@ coord xx( u.standardPart() );
 coord yy( v.standardPart() );
 Jet__environment::EndEnvironment();
 
-Mapping b( 2, 0, xx.Env() );
+Mapping b( 2, 0 );
 
 u = sin(xx) * cos(yy);  v = exp( xx*xx*xx ) / ( xx*yy );
 
 b.SetComponent( 0, u ); b.SetComponent( 1, v );
 
-// c = b(a);
-// c.derivative( index, answer );
+#if 0
+Mapping c( 2, 0 );
+c = b(a);
+c.derivative( index, answer );
+#endif
 
+#if 1
 b(a).derivative( index, answer );
+#endif
 
 cout.setf( ios::fixed,ios::floatfield);
 cout.setf( ios::right,ios::adjustfield);
 cout << "                Using: expansion     composition\n"
      << "Component zero:  " 
-     << setw(15) << answer[0] 
-     << " " 
      << setw(15) << w.derivative( index ) 
+     << " " 
+     << setw(15) << answer[0] 
      << "\n"
      << "Component one :  " 
-     << setw(15) << answer[1] 
-     << " " 
      << setw(15) << z.derivative( index )
+     << " " 
+     << setw(15) << answer[1] 
      << endl;
 }
