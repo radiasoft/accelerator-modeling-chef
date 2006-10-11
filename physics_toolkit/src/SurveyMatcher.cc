cfc153125eecba9b76db1770f16ced92bf2d87ef
michelot <michelot>
2006-10-11 14:40:16 +0000

    *** empty log message ***

--- physics_toolkit/src/SurveyMatcher.cc
+++ physics_toolkit/src/SurveyMatcher.cc
@@ -45,7 +45,7 @@
 #include <physics_toolkit/SurveyMatcher.h>
 #include <basic_toolkit/GenericException.h>
 #include <beamline/beamline.h>
-#include <physics_toolkit/FramePusher.h>
+#include <beamline/FramePusher.h>
 
 using namespace std;
 
