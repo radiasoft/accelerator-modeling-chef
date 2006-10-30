08afb144933b03ddfa3e05f705253d1cb4fc4ef8
ostiguy <ostiguy>
2006-10-30 16:11:32 +0000

    Added various sample files. -jfo

---
+++ chef-mdi/input_files/jet-basic-ops.py
@@ -0,0 +1,23 @@
+#
+mxyzptlk.BeginEnvironment(12)
+x =  mxyzptlk.coord(1.0)
+y =  mxyzptlk.coord(1.0)
+z =  mxyzptlk.coord(1.0)
+px = mxyzptlk.coord(0.0)
+py = mxyzptlk.coord(0.0)
+pz = mxyzptlk.coord(0.0)
+mxyzptlk.EndEnvironment()
+#
+
+print "x = "
+print  x
+print "y = "
+print  y
+print "x+y =" 
+print  x+y
+print "x*y ="
+print  x*y
+
+print  "x*y + 2.0 = " 
+print x*y + 2.0 
+
---
+++ chef-mdi/input_files/jet-sin.py
@@ -0,0 +1,18 @@
+#
+mxyzptlk.BeginEnvironment(12)
+x =  mxyzptlk.coord(1.0)
+y =  mxyzptlk.coord(1.0)
+z =  mxyzptlk.coord(1.0)
+px = mxyzptlk.coord(0.0)
+py = mxyzptlk.coord(0.0)
+pz = mxyzptlk.coord(0.0)
+mxyzptlk.EndEnvironment()
+#
+
+print x
+
+print mxyzptlk.sin(x)
+print mxyzptlk.asin(sin(x))
+
+
+
