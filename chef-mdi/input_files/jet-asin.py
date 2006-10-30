d59b61db9a995bcd277e031197933737b1720955
ostiguy <ostiguy>
2006-10-30 16:11:32 +0000

    Added various sample files. -jfo

---
+++ chef-mdi/input_files/jet-asin.py
@@ -0,0 +1,18 @@
+#
+mxyzptlk.BeginEnvironment(12)
+x =  mxyzptlk.coord(1.0)
+y =  mxyzptlk.coord(1.0)
+z =  mxyzptlk.coord(1.0)
+px = mxyzptlk.coord(0.0)
+py = mxyzptlk.coord(0.0)
+pz = mxyzptlk.coord(0.0)
+mxyzptlk.EndEnvironment()
+#
+
+print x
+w = mxyzptlk.sin(x)
+print w
+print mxyzptlk.asin(w)
+
+
+
