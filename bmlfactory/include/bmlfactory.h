4863156e4b1544eabcedcf9ffb400f04f4c8eca5
ostiguy <ostiguy>
2006-12-14 14:49:48 +0000

    bmlfactory is now an abstract interface class. MAD8Factory is the concrete classfor the MAD8 parser. -jfo

--- bmlfactory/include/bmlfactory.h
+++ bmlfactory/include/bmlfactory.h
@@ -5,7 +5,6 @@
 ******  BEAMLINE FACTORY:  Interprets MAD input files and             
 ******             creates instances of class beamline.                       
 ******                                                
-******  Version:   1.2
 ******                                    
 ******  File:      bmlfactory.h
 ******                                                                
@@ -23,134 +22,55 @@
 ******  is protected under the U.S. and Foreign Copyright Laws. 
 ******                                   
 ******                                                                
-******  Authors:   Dmitri Mokhov, Oleg Krivosheev
-******             Jean-Francois Ostiguy      
-******                                                                
-******  Contact:   Leo Michelotti or Jean-Francois Ostiguy            
-******                                                                
-******             Fermilab                                           
-******             P.O.Box 500                                        
-******             Mail Stop 220                                      
-******             Batavia, IL   60510                                
-******                                                                
-******             Phone: (630) 840 4956                              
-******                    (630) 840 2231                              
-******             Email: michelotti@fnal.gov                         
-******                    ostiguy@fnal.gov                            
-******                                                                
+******  Authors:   Jean-Francois Ostiguy      
+******             ostiguy@fnal.gov                                                    
+******
+******  NOTE: bmlfactory is an abstract interface class. The old bmlfactory
+******        class is now called MAD8BmlFactory.  
+******                                                        
 **************************************************************************
 *************************************************************************/
 #ifndef BMLFACTORY_H
 #define BMLFACTORY_H
 
-#include <functional>
-#include <glib.h>
 #include <list>
 #include <string>
 
-#include <beamline/beamline.h>
-#include <bmlfactory/beam_element.h>
-#include <bmlfactory/beam_line.h>
-#include <bmlfactory/madparser.h>
-
-
-typedef char* char_ptr;
-
-typedef struct {
-    beam_element* bel_ptr_;  /* beam element from MAD parser */
-    bmlnElmnt*    lbel_ptr_; /* beam element from Leo */
-} bel_pair;
-
-typedef std::list<bel_pair> bel_pair_list__t;
-
-typedef struct {
-    beam_line* bml_ptr_;  /* beam line from MAD parser */
-    beamline*  lbml_ptr_; /* beam line from Leo */
-} bml_pair;
-
-typedef std::list<bml_pair> bml_pair_list__t;
-
-struct bel_predicate : public std::binary_function<bel_pair,char_ptr,bool> {
-    bool operator()( const bel_pair& x, const char_ptr& ptr ) const {
-      char label[BEL_NAME_LENGTH];
-      beam_element_get_label( x.bel_ptr_, label );
-      if ( strcasecmp( label, ptr ) == 0 )
-        return true;
-      else
-        return false;
-    }
-};
-
-struct bml_predicate : public std::binary_function<bml_pair,char_ptr,bool> {
-    bool operator()( const bml_pair& x, const char_ptr& ptr ) const {
-      if ( strcasecmp( (x.bml_ptr_)->name_, ptr ) == 0 )
-        return true;
-      else
-        return false;
-    }
-};
-
+class beamline;
 
 class bmlfactory {
 
-  private:
-
-    char*                   fname_;
-    double                  BRHO_;
-    madparser*              mp_;
-    GHashTable*             var_table_;
-    GHashTable*             bel_table_;
-    beam_element**          bel_arr_;
-    int                     bel_arr_size_;
-    beam_line**             bml_arr_;
-    int                     bml_arr_size_;
-    bel_pair_list__t*       bel_list_;
-    bml_pair_list__t*       bml_list_;
-
-    std::list<std::string> _beamline_identifiers_list;
-    std::list<beamline*>   _beamline_objects_list;
-    std::list<bmlnElmnt*>  _bmlnElmnt_objects_list;
- 
-
-    bmlnElmnt* beam_element_instantiate( beam_element* );
-    bmlnElmnt* find_beam_element( char* );
-
-    void create_bel_list();
-    void delete_bel_list();
-    
-    beamline* beam_line_instantiate( beam_line* );
-    beamline* find_beam_line( char* );
-
-    void append_bml_element( char*, beamline* );
-    void create_bml_list();
-    void delete_bml_list();
-
-    beamline* create_beamline_private( const char*  beamline, double brho);    
-    beamline* create_beamline_private( const char*  beamline);    
-
-    void bmlfactory_init(const char* fname, const char* stringbuffer);
-
-    std::string strip_final_colon( const char* );
-
   public:
 
-    bmlfactory( const char* filename, double brho, const char* stringbuffer=0); // deprecated
-    bmlfactory( const char* filename, const char* stringbuffer=0);
+    bmlfactory(  std::string fname,              const char* stringbuffer );
+    bmlfactory(  std::string fname, double brho, const char* stringbuffer );
 
-    ~bmlfactory();
+    virtual ~bmlfactory() = 0;
 
-    beamline* create_beamline( const char* beamline,  double brho);
-    beamline* create_beamline( const char* beamline); // deprecated 
+    virtual beamline* create_beamline( const char* beamline,  double brho) = 0;
+    virtual beamline* create_beamline( const char* beamline)               = 0;
         
-    std::list<std::string>&  getBeamlineList();       // list of all instantiated beamlines  
-    const char* getUseStatementBeamlineName();        // get beamline name from USE statement, 0 if none found 
-    const char* getParticleType() const;
+    virtual std::list<std::string>&  getBeamlineList() =0;          // list of all available beamlines  
 
-    bool    variableIsDefined(const char* varname) const;
-    double  getVariableValue(const char* varname) const;   
+    virtual const char* getUseStatementBeamlineName()=0;           // get beamline name from USE statement, 0 if none found 
+    virtual const char* getParticleType()               const = 0;
 
-    double getEnergy() const;
-    double getBrho() const;
+    virtual bool        variableIsDefined(const char* varname) const =0;
+    virtual double      getVariableValue(const char* varname)  const =0;   
+
+    virtual double      getEnergy() const =0;
+    virtual double      getBrho()   const =0;
+
+  protected:
+
+    std::string             fname_;
+    double                  BRHO_;
+
+    std::list<std::string>  beamline_identifiers_list_;
+
+
 };
+
+ inline bmlfactory::~bmlfactory() {}
 
 #endif // BMLFACTORY_H
