0ecbc30c3f4f5fa5e5c05599fc707df2a4cd3c59
ostiguy <ostiguy>
2007-01-08 19:42:22 +0000

    Eliminated BeamlineIterator bindings. Replaced with iterator for beamline container ... -jfo

--- python-bindings/src/py-beamlineiterator.cpp
+++ python-bindings/src/py-beamlineiterator.cpp
@@ -28,36 +28,7 @@
 
 using namespace boost::python;
 
-static object pass_through(object const& o) { return o; }
 
 
-static bmlnElmnt* next(DeepBeamlineIterator& o) 
-    {
-
-        bmlnElmnt* result = o++;
-        if (!result) { 
-             //PyErr_SetString(PyExc_StopIteration, "No more data."); 
-             //boost::python::throw_error_already_set();
-          return 0;
-          
-        }
-     
-        return result;
-    }
-
-
-
-bmlnElmnt* (DeepBeamlineIterator::* local_iterator)(int) = &DeepBeamlineIterator::operator++;
-
-void wrap_beamlineiterator() {
-
-
- class_<DeepBeamlineIterator,boost::noncopyable>("DeepBeamlineIterator", init<beamline&>()) 
-        .def("next",      &next, return_value_policy<reference_existing_object>())
-        .def("__iter__",  &pass_through )
-        .def("reset",     &DeepBeamlineIterator::reset);
-
-}
-
-
+// obsolete 
 
