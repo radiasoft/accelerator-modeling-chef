a727f91bc112c264ee227b7d13a1c5347610a07b
ostiguy <ostiguy>
2007-02-09 17:00:07 +0000

    Added functionality to iterate through a BarnacleList and modify it . -jfo

--- basic_toolkit/include/Barnacle.h
+++ basic_toolkit/include/Barnacle.h
@@ -3,14 +3,15 @@
 **************************************************************************
 ******                                                                
 ******  BASIC TOOLKIT:  Low level utility C++ classes.
-******  Version:   4.0                    
+******
 ******                                    
 ******  File:      Barnacle.h
 ******                                                                
 ******  Copyright (c) Universities Research Association, Inc./Fermilab    
 ******                All Rights Reserved                             
 ******                                                                
-******  Author:    Leo Michelotti                                     
+******  Authors:   Leo Michelotti (Original version)
+******             Jean-Francois Ostiguy                                     
 ******                                                                
 ******             Fermilab                                           
 ******             P.O.Box 500                                        
@@ -36,16 +37,20 @@
 ****** Dec 2006   Jean-Francois Ostiguy
 ******            ostiguy@fnal.gov
 ******
-****** - eliminated class BarnacleData 
+****** - eliminated abstract class BarnacleData    
 ****** - use std::list instead of dlist
 ****** - use refs in member function signatures instead of ptrs. 
 ****** - use boost::any with value semantics intead of void* objs to 
-******   store arbitrary user data 
+******   store arbitrary user data. No inheritance involved. 
 ******
-******                                                                
+****** Feb 2007  ostiguy@fnal.gov
+******
+****** - added iterators, erase member functions, empty() and size()                                                      
+******             
 ******                                                                
 **************************************************************************
 *************************************************************************/
+
 #ifndef BARNACLE_H
 #define BARNACLE_H
 
@@ -54,8 +59,8 @@
 #include <string>
 #include <list>
 
-struct Barnacle 
-{
+struct Barnacle {
+
   Barnacle(): id(), info() {} 
   Barnacle( std::string s,  boost::any  a): id(s), info(a) {} 
 
@@ -66,6 +71,8 @@ struct Barnacle
 
   std::string    id;
   boost::any     info;
+
+
 };
 
 
@@ -73,29 +80,51 @@ struct Barnacle
 
 
 
-class DLLEXPORT BarnacleList 
-{
- public:
-  typedef  std::list<Barnacle>::iterator iterator;
+class DLLEXPORT BarnacleList {
+
+
+public:
+ 
+  typedef  std::list<Barnacle>::iterator                     iterator;
+  typedef  std::list<Barnacle>::const_iterator const_iterator;
+
+  typedef  std::list<Barnacle>::reverse_iterator             reverse_iterator;
+  typedef  std::list<Barnacle>::const_reverse_iterator const_reverse_iterator;
 
   void             append    ( Barnacle const& );
   void             insert    ( Barnacle const& );
 
   void             eraseFirst( std::string   identifier );
-  void             eraseAll  ( std::string   = "" );
+  void              eraseLast( std::string   identifier );  
+  void               eraseAll( std::string   identifier );  
+  void                  erase( iterator&           pos );
+  void                  erase( reverse_iterator&   pos );
+  void                  clear();
 
 
-  iterator find  ( std::string   identifier,  int instance = 1 );
+  iterator          find  ( std::string   identifier,  int instance = 1 ); // forward find
+  reverse_iterator rfind  ( std::string   identifier,  int instance = 1 ); // reverse find
 
-  void             remove( iterator pos );
    
-  iterator begin()       { return theList_.begin(); }
-  iterator end()         { return theList_.end();   }
+  iterator begin()                      { return theList_.begin();  }
+  iterator end()                        { return theList_.end();    }
 
-  bool empty()    const  { return theList_.empty(); }   
+  const_iterator begin()      const     { return theList_.begin();  }
+  const_iterator end()        const     { return theList_.end();    }
+
+  reverse_iterator rbegin()             { return theList_.rbegin(); }
+  reverse_iterator rend()               { return theList_.rend();   }
+
+  const_reverse_iterator rbegin() const { return theList_.rbegin(); }
+  const_reverse_iterator rend()   const { return theList_.rend();   }
+
+  bool empty()    const                 { return theList_.empty();  }   
+  int  size()     const                 { return theList_.size();   }   
 
  private:
+
   std::list<Barnacle> theList_;
+
 };
 
 #endif // BARNACLE_H
