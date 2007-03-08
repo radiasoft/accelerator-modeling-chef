756d123b65c47fb8c2a39a76a6161277213fd01f
michelot <michelot>
2007-03-08 19:11:23 +0000

    The beginnings of a shell-script based testing procedure to be used to after building chef's libraries.
    - lpjm

---
+++ beamline/demos/test-build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+######################################################
+#
+# File: test-build.sh
+#
+# Tests the beamline/demos files
+#
+# Original: 
+#  version: March 8, 2007
+# 
+#   Author: Leo Michelotti
+#         : michelotti@fnal.gov
+#
+#  COMMENT: I intend that this script develop organically.
+#
+#  WARNING: Though this script is very simple, I am more
+#           comfortable using C shell than bash, so
+#           things may go badly wrong.
+#
+######################################################
+
+echo This testing script is, so far, just a temporary place holder.
