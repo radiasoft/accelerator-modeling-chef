67a8e7d9e14bdfa562a3bb5c5212adf9b6c5a840
michelot <michelot>
2007-03-08 20:40:23 +0000

    Minor refinement: comparison with correct answer.   - lpjm

--- mxyzptlk/demos/Lie_A_Test.cc
+++ mxyzptlk/demos/Lie_A_Test.cc
@@ -7,7 +7,11 @@
 ** field of a damped harmonic oscillator.
 ** 
 ** --- Leo Michelotti
+** 
 ** --- August 10, 1993
+** --- March 7, 2007
+**     : minor refinement
+**       : comparison to correct answer.
 **
 */
 
@@ -23,9 +27,11 @@ main( int argc, char** argv )
     return -1;
   }
 
+  double a = atof( argv[1] ), b = atof( argv[2] );
+
   Jet__environment::BeginEnvironment( 1 );
-    coord x( atof( argv[1] ) );   
-    coord p( atof( argv[2] ) );
+    coord x(a);
+    coord p(b);
   Jet__environment::EndEnvironment();
 
   LieOperator v( x.Env() );
@@ -33,8 +39,12 @@ main( int argc, char** argv )
   v.setVariable(      p, 0 );
   v.setVariable( -x - p, 1 );
 
-  cout << "Answer is: "
+  cout << "Answer is        : "
        << ( v.operator^( x*x + p*p ) ).standardPart()
+       << endl;
+
+  cout << "Correct answer is: "
+       << (-2*b*b)
        << endl;
 
   return 0;
