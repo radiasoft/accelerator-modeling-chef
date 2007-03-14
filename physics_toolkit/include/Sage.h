bb1d4d861b26c6d3fc91026dca1267937a11bc43
ostiguy <ostiguy>
2007-03-14 20:42:29 +0000

    - support for reference counted elements -jfo

--- physics_toolkit/include/Sage.h
+++ physics_toolkit/include/Sage.h
@@ -33,10 +33,15 @@
 ******             Phone: (630) 840 4956                              
 ******             Email: michelotti@fnal.gov                         
 ******                                                                
+****** REVISION HISTORY
+******
+****** Mar 2007     ostiguy@fnal.gov
+******
+****** - support for reference counted elements
+****** - Pass particles by reference 
 ******                                                                
 **************************************************************************
 *************************************************************************/
-
 
 /*
  *  File: Sage.h
@@ -62,14 +67,9 @@ class Sage
 {
 
 public:
-  Sage( const beamline*, bool = false );
-  // Second argument controls cloning.
-  // If true, then the Sage will clone
-  //   its own copy of the beamline from
-  //   the first argument, completely
-  //   disconnecting it from the calling
-  //   routine. 
-  // By default, it just stores the pointer.
+
+ Sage( BmlPtr          );
+ Sage( beamline const& );
 
  virtual ~Sage();
   
@@ -78,13 +78,15 @@ public:
 
  virtual void eraseAll() = 0;
 
- typedef bool (*CRITFUNC)( bmlnElmnt* );
+ typedef bool (*CRITFUNC)( ConstElmPtr );
 
- static bool no ( bmlnElmnt* );
- static bool yes( bmlnElmnt* );
- static bool isRing( const beamline*, double = _defGapTol, double = _defAngleTol );
- static bool isRing( const beamline&, double = _defGapTol, double = _defAngleTol );
- // The second argument is the maximum gap allowed 
+ static bool no ( ConstElmPtr );
+ static bool yes( ConstElmPtr );
+
+ static bool isRing( BmlPtr,          double = defGapTol_, double = defAngleTol_ );
+ static bool isRing( beamline const&, double = defGapTol_, double = defAngleTol_ );
+ 
+// The second argument is the maximum gap allowed 
  //   for a return of "true." 
  // These tests will return true for pathologically short lines
  //   like a beamline consisting of a single 1 mm drift.
@@ -103,83 +105,23 @@ public:
 
 protected:
 
- beamline*           _myBeamlinePtr;
+ BmlPtr              myBeamlinePtr_;
  int                 nelms_;   
- bool                _verbose;
- bool                _cloned;
- bool                _isRing;
- std::ostream*       _errorStreamPtr;
- std::ostream*       _outputStreamPtr;
- double              _ringGapTolerance;
- double              _ringAngleTolerance;
+ bool                verbose_;
+ bool                isRing_;
+ std::ostream*       errorStreamPtr_;
+ std::ostream*       outputStreamPtr_;
+ double              ringGapTolerance_;
+ double              ringAngleTolerance_;
 
 private:
 
- static double _defGapTol;
- static double _defAngleTol;
+ static double defGapTol_;
+ static double defAngleTol_;
  
  Sage( Sage const&); // copy forbidden 
 
 };
 
-
-inline void Sage::set_verbose()
-{
-  _verbose = true;
-}
-
-inline void Sage::unset_verbose()
-{
-  _verbose = false;
-}
-
-inline void Sage::setErrorStream( std::ostream* x )
-{
-  _errorStreamPtr = x;
-}
-
-inline void Sage::setOutputStream( std::ostream* x )
-{
-  _outputStreamPtr = x;
-}
-
-inline void Sage::treatAsRing( bool x )
-{
-  if(x) {
-    _myBeamlinePtr->setLineMode( beamline::ring );
-  }
-  else {
-    _myBeamlinePtr->setLineMode( beamline::line );
-  }
-}
-
-inline bool Sage::isTreatedAsRing() const
-{
-  return ( beamline::ring == _myBeamlinePtr->getLineMode() );
-}
-
-
-inline void Sage::setGapTolerance( double x )
-{
-  _ringGapTolerance = std::abs(x);
-}
-
-
-inline double Sage::getGapTolerance() const
-{
-  return _ringGapTolerance;
-}
-
-
-inline void Sage::setAngleTolerance( double x )
-{
-  _ringAngleTolerance = std::abs(x);
-}
-
-
-inline double Sage::getAngleTolerance() const
-{
-  return _ringAngleTolerance;
-}
 
 #endif // SAGE_H
