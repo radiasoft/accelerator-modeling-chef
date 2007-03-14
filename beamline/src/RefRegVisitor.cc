15ce0882a9d6b5762146be6c9bf7731aedac4914
ostiguy <ostiguy>
2007-03-14 18:00:30 +0000

    -use new style STL-compatible iterators -use locally defined particles defined on the stack -visitor public interface taking advantage of dynamic type
    -jfo

--- beamline/src/RefRegVisitor.cc
+++ beamline/src/RefRegVisitor.cc
@@ -31,6 +31,14 @@
 ******  royalty-free license to publish or reproduce documentation 
 ******  and software for U.S. Government purposes. This software 
 ******  is protected under the U.S. and Foreign Copyright Laws. 
+******
+****** REVISION HISTORY
+******
+******  Mar 2007        ostiguy@fnal.gov
+******
+****** -use new style STL-compatible iterators
+****** -use locally defined particles defined on the stack
+****** -visitor public interface taking advantage of dynamic type
 ******                                                                
 **************************************************************************
 *************************************************************************/
@@ -64,7 +72,6 @@
 #include <beamline/Slot.h>
 #include <beamline/rfcavity.h>
 #include <beamline/Particle.h>
-#include <beamline/BeamlineIterator.h>
 
 // Static error codes
 
@@ -74,190 +81,175 @@ using namespace std;
 
 // Constructors
 
-RefRegVisitor::RefRegVisitor( const Particle& p )
-:   _prtnPtr(0)
-  , _errorCode(OKAY)
-  , _revolutionFrequency(-1.0)
-  , _initialMomentum( p.ReferenceMomentum() )
-{
-  _prtnPtr = p.Clone();
-}
+RefRegVisitor::RefRegVisitor( Particle const& p )
+  :    particle_(p)
+  ,    errorCode_(OKAY)
+  ,    revolutionFrequency_(-1.0)
+  ,    initialMomentum_( p.ReferenceMomentum() )
+{}
 
 
-RefRegVisitor::RefRegVisitor( const RefRegVisitor& x )
-:   _prtnPtr(0)
-  , _errorCode(OKAY)
-  , _revolutionFrequency(x._revolutionFrequency)
-  , _initialMomentum(x._initialMomentum)
-{
-  _prtnPtr = x._prtnPtr->Clone();
-}
+RefRegVisitor::RefRegVisitor( RefRegVisitor const& x )
+:   particle_(x.particle_)
+  , errorCode_(OKAY)
+  , revolutionFrequency_(x.revolutionFrequency_)
+  , initialMomentum_(x.initialMomentum_)
+{}
 
 
 RefRegVisitor::~RefRegVisitor()
-{
-  delete _prtnPtr;
-}
+{}
 
 
 int RefRegVisitor::getErrorCode() const
 {
-  return _errorCode;
+  return errorCode_;
 }
 
 
 double RefRegVisitor::getCdt()
 {
-  return _prtnPtr->get_cdt();
+  return particle_.get_cdt();
 }
 
 
 void RefRegVisitor::setCdt( double x )
 {
-  _prtnPtr->set_cdt(x);
+   particle_.set_cdt(x);
 }
 
 
 // Visiting functions
 
-void RefRegVisitor::visitBeamline( beamline* x )
+void RefRegVisitor::visit( beamline& x )
 {
   double momentum;
-  // REMOVE: x->setEnergy( _prtnPtr->Energy() );  // not just reference energy
-  // REMOVE:                                      // ??? why not ???
-  x->setEnergy( _prtnPtr->ReferenceEnergy() );
+
+  x.setEnergy( particle_.ReferenceEnergy() );
 
   // Preliminary traversal in case there are
   // RF cavities in the line.
-  if( _revolutionFrequency < 0 ) 
+  if( revolutionFrequency_ < 0 ) 
   {
     double cumulativeCdt = 0.0;
-    Particle* copy = _prtnPtr->Clone();
-    bmlnElmnt* q;
-    DeepBeamlineIterator dbi( *x );
+    Particle copy(particle_);
+
 
     // Adjust the strength of all magnetic elements
     //   if the reference particle has been accelerated.
-    while((  q = dbi++  )) {
-      if(    (_initialMomentum != (momentum = copy->ReferenceMomentum()))
-          && (q->isMagnet())                                                ) 
-      {
-        q->setStrength( (q->Strength())*(momentum/_initialMomentum) );
-      }
-      if( typeid(*q) == typeid(rfcavity) ) {
-        double xxx = copy->ReferenceBeta();
-        rfcavity* rfcPtr = dynamic_cast<rfcavity*>(q);
-        copy->SetReferenceEnergy( copy->ReferenceEnergy() + rfcPtr->Strength()*sin( rfcPtr->getPhi() ) );
-        xxx = ( xxx + copy->ReferenceBeta() ) / 2.0;
-        cumulativeCdt += ( rfcPtr->Length() / xxx );
+
+    for (beamline::deep_iterator it  = x.deep_begin(); 
+                                 it != x.deep_end(); ++it) {
+    
+      momentum = copy.ReferenceMomentum();
+
+      if( (initialMomentum_ == momentum) || (! (*it)->isMagnet() ) ) continue; 
+     
+      (*it)->setStrength( ((*it)->Strength())*(momentum/initialMomentum_) );
+
+      rfcavity* rfcPtr = 0;     
+      if( (rfcPtr = dynamic_cast<rfcavity*>( (*it).get() ) ) ) {
+
+        double refbeta = copy.ReferenceBeta();
+        copy.SetReferenceEnergy( copy.ReferenceEnergy() + rfcPtr->Strength()*sin( rfcPtr->getPhi() ) );
+        refbeta = ( refbeta + copy.ReferenceBeta() ) / 2.0;
+        cumulativeCdt += ( rfcPtr->Length() / refbeta );
       }
       else {
-        q->setReferenceTime(0);
-        copy->set_cdt(0);
-        q->propagate( *copy );
-        cumulativeCdt += copy->get_cdt();
+        (*it)->setReferenceTime(0);
+        copy.set_cdt(0);
+        (*it)->propagate( copy );
+        cumulativeCdt += copy.get_cdt();
       }
     }
 
     // Store the total time of traversal as a frequency
-    // REMOVE: _revolutionFrequency = PH_MKS_c /( copy->get_cdt() );
-    _revolutionFrequency = PH_MKS_c /cumulativeCdt;
+    revolutionFrequency_ = PH_MKS_c /cumulativeCdt;
 
-    delete copy;
   }
 
-  this->BmlVisitor::visitBeamline( x );
+  BmlVisitor::visit( x ); // WHY ?????
 }
 
 
-void RefRegVisitor::visitBmlnElmnt( bmlnElmnt* x )
+void RefRegVisitor::visit( bmlnElmnt& x )
 {
-  x->setReferenceTime(0.0);
-  _prtnPtr->set_cdt(0.0);
-  x->propagate( *_prtnPtr );
-  x->setReferenceTime( _prtnPtr->get_cdt() );
-  // x->propagate( *_prtnPtr );
+  x.setReferenceTime(0.0);
+  particle_.set_cdt(0.0);
+  x.propagate( particle_ );
+  x.setReferenceTime( particle_.get_cdt() );
+
 }
 
 
-void RefRegVisitor::visitCF_rbend( CF_rbend* x ) 
-{
-  // This does not nullify the edge focussing from 
-  // the inner propagators before setting the entry
-  // and exit angles. It may be that these attributes
-  // are not even used.
-  x->setEntryAngle( *_prtnPtr );
-  x->acceptInner( *this );
-  x->setExitAngle( *_prtnPtr );
-}
-
-
-void RefRegVisitor::visitCF_sbend( CF_sbend* x ) 
+void RefRegVisitor::visit( CF_rbend& x ) 
 {
   // This does not nullify the edge focussing from 
   // the inner propagators before setting the entry
   // and exit angles. It may be that these attributes
   // are not even used.
-  x->setEntryAngle( *_prtnPtr );
-  x->acceptInner( *this );
-  x->setExitAngle( *_prtnPtr );
+  x.setEntryAngle( particle_ );
+  x.acceptInner( *this );
+  x.setExitAngle( particle_ );
 }
 
 
-// REMOVE: void RefRegVisitor::visitSbend( sbend* x )
-// REMOVE: {
-// REMOVE:   x->setEntryAngle( *_prtnPtr );
-// REMOVE:   this->visitBmlnElmnt( ((bmlnElmnt*) x) );
-// REMOVE:   x->setExitAngle( *_prtnPtr );
-// REMOVE: }
-
-
-void RefRegVisitor::visitSbend( sbend* x )
+void RefRegVisitor::visit( CF_sbend& x ) 
 {
-  const bmlnElmnt::PropFunc* propPtr = x->getPropFunction();
-  x->setPropFunction( &sbend::NoEdge );
-  x->setEntryAngle( *_prtnPtr );
-  this->visitBmlnElmnt( ((bmlnElmnt*) x) );
-  x->setExitAngle( *_prtnPtr );
-  x->setPropFunction( propPtr );
+  // This does not nullify the edge focussing from 
+  // the inner propagators before setting the entry
+  // and exit angles. It may be that these attributes
+  // are not even used.
+  x.setEntryAngle( particle_ );
+  x.acceptInner( *this );
+  x.setExitAngle( particle_ );
 }
 
 
-void RefRegVisitor::visitRbend( rbend* x )
+void RefRegVisitor::visit( sbend& x )
 {
-  const bmlnElmnt::PropFunc* propPtr = x->getPropFunction();
-  x->setPropFunction( &rbend::NoEdge );
-  x->setEntryAngle( *_prtnPtr );
-  this->visitBmlnElmnt( ((bmlnElmnt*) x) );
-  x->setExitAngle( *_prtnPtr );
-  x->setPropFunction( propPtr );
+  const bmlnElmnt::PropFunc* propPtr = x.getPropFunction();
+
+  x.setPropFunction( &sbend::NoEdge );
+  x.setEntryAngle( particle_ );
+  visit( ((bmlnElmnt&) x) );
+  x.setExitAngle( particle_ );
+  x.setPropFunction( propPtr );
+
 }
 
 
-void RefRegVisitor::visitRfcavity( rfcavity* x ) 
+void RefRegVisitor::visit( rbend& x )
 {
-  x->acceptInner( *this );
+  const bmlnElmnt::PropFunc* propPtr = x.getPropFunction();
+
+  x.setPropFunction( &rbend::NoEdge );
+  x.setEntryAngle( particle_ );
+  visit( ((bmlnElmnt&) x) );
+  x.setExitAngle( particle_ );
+  x.setPropFunction( propPtr );
 }
 
 
-void RefRegVisitor::visitThinrfcavity( thinrfcavity* x ) 
+void RefRegVisitor::visit( rfcavity& x ) 
 {
-  if( x->getFrequency() < 1.0e-9 ) {
-    if( 0 < x->getHarmonicNumber() ) {
-      x->setFrequencyRelativeTo( _revolutionFrequency );
+ 
+  x.acceptInner( *this );
+
+
+}
+
+
+void RefRegVisitor::visit( thinrfcavity& x ) 
+{
+
+  if( x.getFrequency() < 1.0e-9 ) {
+    if( x.getHarmonicNumber() > 0 ) {
+      x.setFrequencyRelativeTo( revolutionFrequency_ );
     }
   }
-  this->visitBmlnElmnt(x);
-}
+
+  visit( (bmlnElmnt&) x);
+} 
 
 
-// REMOVE: void RefRegVisitor::visitSlot( Slot* x )
-// REMOVE: {
-// REMOVE:   x->setReferenceTime(0.0);
-// REMOVE:   _prtnPtr->set_cdt(0.0);
-// REMOVE:   x->propagate( *_prtnPtr );
-// REMOVE:   x->setReferenceTime( _prtnPtr->get_cdt() );
-// REMOVE: 
-// REMOVE:   // x->setReferenceTime( *_prtnPtr );
-// REMOVE:   // x->propagate( *_prtnPtr );
-// REMOVE: }
