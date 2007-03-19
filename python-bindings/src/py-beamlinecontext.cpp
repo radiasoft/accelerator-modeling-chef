744f96f40c2405cefcd7bf3604510a488b0bc8e9
ostiguy <ostiguy>
2007-03-19 16:41:32 +0000

    Support for reference counted beamlines. Eliminated members refering to beamline cloning. -jfo

--- python-bindings/src/py-beamlinecontext.cpp
+++ python-bindings/src/py-beamlinecontext.cpp
@@ -22,14 +22,15 @@
 ****************************************************************************/
 #include <boost/python.hpp>
 
+#include <iostream>
 #include <beamline/bmlnElmnt.h>
 #include <beamline/beamline.h>
 #include <beamline/Particle.h>
 #include <beamline/JetParticle.h>
 #include <basic_toolkit/Matrix.h>
-#include <physics_toolkit/BeamlineContext.h>
+#include <boost/shared_ptr.hpp>
 
-#include <iostream>
+#include <physics_toolkit/BeamlineContext.h>
 
 #define BOOST_PYTHON_STATIC_MODULE
 
@@ -43,13 +44,11 @@ void wrap_beamlinecontext() {
 
 using namespace boost::python;
 
-class_<BeamlineContext, boost::noncopyable>( "BeamlineContext", init<const Particle&, beamline*, bool>() )
+class_<BeamlineContext, boost::noncopyable>( "BeamlineContext", init<const Particle&, BmlPtr>() )
       
 .def( "assign",                         &BeamlineContext::assign )                    //
 //.def( "accept",                       &BeamlineContext::accept )                    //  ( ConstBmlVisitor& ) const;
 //.def  "accept",                       &BeamlineContext::accept )                    //  ( BmlVisitor& ) 
-.def( "setClonedFlag",                  &BeamlineContext::setClonedFlag ) 
-.def( "getClonedFlag",                  &BeamlineContext::getClonedFlag )
 .def( "writeTree",                      &BeamlineContext::writeTree )
 .def( "name",                           &BeamlineContext::name)                             // const char* name() const;
 .def( "peekAt",                         &BeamlineContext::peekAt)                           // void peekAt( double& s, const Particle& ) const;
@@ -64,12 +63,12 @@ class_<BeamlineContext, boost::noncopyable>( "BeamlineContext", init<const Parti
 .def( "getEnergy",                      &BeamlineContext::getEnergy)                        // double getEnergy() const;
 .def( "countHowManyDeeply",             &BeamlineContext::countHowManyDeeply)               // int countHowManyDeeply() const;
 //.def("getAlignmentData",              &BeamlineContext::getAlignmentData)                 // alignmentData getAlignmentData( const bmlnElmnt* ) const;
-//.def("cloneBeamline",                 &BeamlineContext::cloneBeamline)                    // beamline* cloneBeamline() const;
 //.def("getOneTurnMap",                 &BeamlineContext::getOneTurnMap )                   // Mapping getOneTurnMap();
 .def( "getHorizontalFracTune",          &BeamlineContext::getHorizontalFracTune)            // double getHorizontalFracTune();
 .def( "getVerticalFracTune",            &BeamlineContext::getVerticalFracTune)
 .def( "getHorizontalEigenTune",         &BeamlineContext::getHorizontalEigenTune)             //double getHorizontalEigenTune()
 .def( "getVerticalEigenTune",           &BeamlineContext::getVerticalEigenTune)               // double getVerticalEigenTune()
+
 .def( "getTwissArray",                    &BeamlineContext::getTwissArray, 
                                         return_value_policy<reference_existing_object>() )    // const LattFuncSage::lattFunc* getLattFuncPtr( int )
 .def("getETArray",                      &BeamlineContext::getETArray,  
@@ -84,38 +83,14 @@ class_<BeamlineContext, boost::noncopyable>( "BeamlineContext", init<const Parti
 //                                          return_value_policy<reference_existing_object>() )   // MatrixD equilibriumCovariance();
 //.def("equilibriumCovariance",         &BeamlineContext::equilibriumCovariance, 
 //                                          return_value_policy<reference_existing_object>() )   // MatrixD equilibriumCovariance( double, double );
-.def("addHTuneCorrector",               &BeamlineContext::addHTuneCorrector)                   // int addHTuneCorrector( const bmlnElmnt* );
-.def("addVTuneCorrector",               &BeamlineContext::addVTuneCorrector)                   // int addVTuneCorrector( const bmlnElmnt* );
-.def("changeTunesBy",                   &BeamlineContext::changeTunesBy)                       // int changeTunesBy( double, double );
+// .def("addHTuneCorrector",               &BeamlineContext::addHTuneCorrector)                   // int addHTuneCorrector( const bmlnElmnt* );
+// .def("addVTuneCorrector",               &BeamlineContext::addVTuneCorrector)                   // int addVTuneCorrector( const bmlnElmnt* );
+ .def("changeTunesBy",                   &BeamlineContext::changeTunesBy)                       // int changeTunesBy( double, double );
 
-.def("beginIterator",                   &BeamlineContext::beginIterator)                       // int beginIterator();
-.def("beginDeepIterator",               &BeamlineContext::beginDeepIterator)                   // int beginDeepIterator();
-.def("beginReverseIterator",            &BeamlineContext::beginReverseIterator)                // int beginReverseIterator();
-.def("beginDeepReverseIterator",        &BeamlineContext::beginDeepReverseIterator)            // int beginDeepReverseIterator();
-
-.def("endIterator",                     &BeamlineContext::endIterator)                         // void endIterator();
-.def("endDeepIterator",                 &BeamlineContext::endDeepIterator)                     // void endDeepIterator();
-.def("endReverseIterator",              &BeamlineContext::endReverseIterator)                  // void endReverseIterator();
-.def("endDeepReverseIterator",          &BeamlineContext::endDeepReverseIterator)              // void endDeepReverseIterator();
-
-.def("i_next",                          &BeamlineContext::i_next,    
-                                           return_value_policy<reference_existing_object>() )     // const bmlnElmnt* i_next();
-.def("di_next",                         &BeamlineContext::di_next,   
-                                           return_value_policy<reference_existing_object>() )     // const bmlnElmnt* di_next();
-.def("ri_next",                         &BeamlineContext::ri_next,   
-                                           return_value_policy<reference_existing_object>() )     // const bmlnElmnt* ri_next();
-.def("dri_next",                        &BeamlineContext::dri_next,  
-                                           return_value_policy<reference_existing_object>() )     // const bmlnElmnt* dri_next();
-
-.def("i_reset",                         &BeamlineContext::i_reset)                               // void i_reset();
-.def("di_reset",                        &BeamlineContext::di_reset)                              // void di_reset();
-.def("ri_reset",                        &BeamlineContext::ri_reset)                              // void ri_reset();
-.def("dri_reset",                       &BeamlineContext::dri_reset)                             // void dri_reset();
-
-  .def("isRing", &BeamlineContext::isRing)
-  .def("isTreatedAsRing", &BeamlineContext::isTreatedAsRing)
-  .def("handleAsRing", &BeamlineContext::handleAsRing)
-  .def("handleAsLine", &BeamlineContext::handleAsLine)
+ .def("isRing", &BeamlineContext::isRing)
+ .def("isTreatedAsRing", &BeamlineContext::isTreatedAsRing)
+ .def("handleAsRing", &BeamlineContext::handleAsRing)
+ .def("handleAsLine", &BeamlineContext::handleAsLine)
   ;
 
 }
