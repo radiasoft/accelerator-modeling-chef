970685c3b1b91ef78aa1d273baa1247747afc729
ostiguy <ostiguy>
2007-03-19 16:59:59 +0000

    Respect Particle type inheritance relationships. Support for all defined c++ particle type classes. -jfo

--- python-bindings/src/py-particle.cpp
+++ python-bindings/src/py-particle.cpp
@@ -1,5 +1,8 @@
-/***************************************************************************                                                               
-******  Boost.python Python bindings for mxyzpltk/beamline libraries 
+/*******************************************************************************
+********************************************************************************
+********************************************************************************
+******
+******  Python bindings for mxyzpltk/beamline libraries 
 ******  
 ******                                    
 ******  File:      py-particle.cpp
@@ -19,9 +22,11 @@
 ******                                                                
 ******    Fermi National Laboratory, Batavia, IL   60510
 ******
-****************************************************************************/
-#include <boost/python.hpp>
+********************************************************************************
+********************************************************************************
+*******************************************************************************/
 
+#include <boost/python.hpp>
 #include <beamline/Particle.h>
 #include <beamline/JetParticle.h>
 #include <basic_toolkit/VectorD.h>
@@ -29,12 +34,23 @@
 
 using namespace boost::python;
 
-Vector  (Particle::*State_void)() const            = &Particle::State;
-Mapping (JetParticle::*JetState_void)() const      = &JetParticle::State;
+//------------------------------------------------------------------------------
+// local code and definitions
+//------------------------------------------------------------------------------
+
+namespace {
+
+Vector  (Particle::*State_void)()       const      = &Particle::State;
+
+} // anonymous namespace
+
+//------------------------------------------------------------------------------
+// wrapper code
+//------------------------------------------------------------------------------
 
 void wrap_particle () {
 
-  class_<Particle>("Particle", no_init)
+  class_<Particle, boost::noncopyable> ("Particle", no_init )
   .def("xIndex",      &Particle::xIndex)
   .staticmethod("xIndex")
   .def("yIndex",      &Particle::yIndex)
@@ -53,33 +69,33 @@ void wrap_particle () {
   .def("SetReferenceMomentum", &Particle::SetReferenceMomentum)
   .def("setStateToZero",       &Particle::setStateToZero)
   
-  .def ("get_x",    &Particle::get_x)
-  .def ("get_y",    &Particle::get_y)
-  .def ("get_cdt",  &Particle::get_cdt)
-  .def ("get_npx",  &Particle::get_npx)
-  .def ("get_npy",  &Particle::get_npy)
-  .def ("get_ndp",  &Particle::get_ndp)
-  .def ("set_x",    &Particle::set_x)
-  .def ("set_y",    &Particle::set_y )
-  .def ("set_cdt",  &Particle::set_cdt)
-  .def ("set_npx",  &Particle::set_npx)
-  .def ("set_npy",  &Particle::set_npy)
-  .def ("set_ndp",  &Particle::set_ndp)
-  .def("State",     State_void, return_value_policy<return_by_value>() )
-  .def("Energy",              &Particle::Energy)
-  .def("Momentum",            &Particle::Momentum)
-  .def("NormalizedMomentum",  &Particle::NormalizedMomentum)
-  .def("Gamma",               &Particle::Gamma)
-  .def("Beta",                &Particle::Beta)
-  .def("ReferenceBRho",       &Particle::ReferenceBRho)
-  .def("ReferenceBeta",       &Particle::ReferenceBeta)
-  .def("ReferenceMomentum",   &Particle::ReferenceMomentum)
+  .def ("get_x",               &Particle::get_x)
+  .def ("get_y",               &Particle::get_y)
+  .def ("get_cdt",             &Particle::get_cdt)
+  .def ("get_npx",             &Particle::get_npx)
+  .def ("get_npy",             &Particle::get_npy)
+  .def ("get_ndp",             &Particle::get_ndp)
+  .def ("set_x",               &Particle::set_x)
+  .def ("set_y",               &Particle::set_y )
+  .def ("set_cdt",             &Particle::set_cdt)
+  .def ("set_npx",             &Particle::set_npx)
+  .def ("set_npy",             &Particle::set_npy)
+  .def ("set_ndp",             &Particle::set_ndp)
+  .def("State",                State_void, return_value_policy<return_by_value>() )
+  .def("Energy",               &Particle::Energy)
+  .def("Momentum",             &Particle::Momentum)
+  .def("NormalizedMomentum",   &Particle::NormalizedMomentum)
+  .def("Gamma",                &Particle::Gamma)
+  .def("Beta",                 &Particle::Beta)
+  .def("ReferenceBRho",        &Particle::ReferenceBRho)
+  .def("ReferenceBeta",        &Particle::ReferenceBeta)
+  .def("ReferenceMomentum",    &Particle::ReferenceMomentum)
 
-  .def("PNI2",                &Particle::PNI2)
+  .def("PNI2",                 &Particle::PNI2)
 
-  .def("ReferenceEnergy",     &Particle::ReferenceEnergy)
-  .def("Charge",              &Particle::Charge)
-  .def("BRho",                &Particle::BRho);
+  .def("ReferenceEnergy",      &Particle::ReferenceEnergy)
+  .def("Charge",               &Particle::Charge)
+  .def("BRho",                 &Particle::BRho);
 
 
 
@@ -87,89 +103,27 @@ void wrap_particle () {
    .def( init<double const&>())
    .def( init<double const&, Vector const&>());
 
+  class_<AntiProton, bases<Particle> >("AntiProton", init<>() )
+   .def( init<double const&>())
+   .def( init<double const&, Vector const&>());
+
+
+  class_<Electron, bases<Particle> >("Electron", init<>() )
+   .def( init<double const&>())
+   .def( init<double const&, Vector const&>());
 
   class_<Positron, bases<Particle> >("Positron", init<>() )
    .def( init<double const&>())
    .def( init<double const&, Vector const&>());
 
-
-  def("createStandardEnvironments", 
-      &JetParticle::createStandardEnvironments);
-
-   class_<JetParticle>("JetParticle", no_init)
-       //.def( init<double>())
-       //.def( init<double,double>())
-       //.def( init<double,double, double*>())
-       //.def( init<const Particle&>())
-   .def( "xIndex",   &JetParticle::xIndex)
-   .staticmethod("xIndex")  
-   .def( "yIndex",   &JetParticle::yIndex)
-   .staticmethod("yIndex")  
-   .def( "cdtIndex", &JetParticle::cdtIndex)
-   .staticmethod("cdtIndex")  
-   .def( "npxIndex", &JetParticle::npxIndex)
-   .staticmethod("npxIndex")  
-   .def( "npyIndex", &JetParticle::npyIndex)
-   .staticmethod("npyIndex")  
-   .def( "ndpIndex", &JetParticle::ndpIndex)
-   .staticmethod("ndpIndex")  
-     //.def( "psd",  &JetParticle::psd)
-     //.staticmethod("psd")  
-   .def("SetReferenceEnergy", &JetParticle::SetReferenceEnergy)
-       //void setState( double* );
-       //void setState( const Vector& );
-       //void setState( const Mapping& );
-       //void getState( Mapping& );
-       //void getState( Jet* );
-    .def("State",    JetState_void, return_value_policy<return_by_value>() )
-
-    .def("get_x",    &JetParticle::get_x)
-    .def("get_y",    &JetParticle::get_y)
-    .def("get_cdt",  &JetParticle::get_cdt)
-    .def("get_npx",  &JetParticle::get_npx)
-    .def("get_npy",  &JetParticle::get_npy)
-    .def("get_ndp",  &JetParticle::get_ndp)
-
-    .def("set_x",   &JetParticle::set_x)
-    .def("set_y",   &JetParticle::set_y)
-    .def("set_cdt", &JetParticle::set_cdt)
-    .def("set_npx", &JetParticle::set_npx)
-    .def("set_npy", &JetParticle::set_npy)
-    .def("set_ndp", &JetParticle::set_ndp)
-   //MatrixD SymplecticTest();  // Tests the state for the
-                             // symplectic condition, 1 = - MJM^t; 
-                             // returns unit matrix if all is well.
-                             // Note: this assumes a 6x6 state: 
-                             // ( x, y, cdt; px/p, py/p, dp/p )
-
-    .def("Energy",              &JetParticle::Energy)
-    .def("Momentum",            &JetParticle::Momentum)
-    .def("NormalizedMomentum",  &JetParticle::NormalizedMomentum)
-    .def("Mass",                &JetParticle::Mass)
-    .def("ReferenceBRho",       &JetParticle::ReferenceBRho)
-    .def("ReferenceBeta",       &JetParticle::ReferenceBeta)
-    .def("ReferenceGamma",      &JetParticle::ReferenceGamma)
-    .def("Gamma",               &JetParticle::Gamma)
-    .def("ReferenceMomentum",   &JetParticle::ReferenceMomentum)
-    .def("PNI2",                &JetParticle::PNI2)
-    .def("ReferenceEnergy",     &JetParticle::ReferenceEnergy)
-    .def("Charge",              &JetParticle::Charge);
-  
-           // JetVector VectorBeta() const;
-           //JetVector VectorMomentum() const;
-           //JetVector NormalizedVectorMomentum() const;
-           //inline Jet Beta() const {
-           //Jet BRho() const {
-           //JetParticle& operator=(const JetParticle&);
-
-
-  class_<JetProton, bases<JetParticle> >("JetProton") 
+  class_<Muon, bases<Particle> >("Muon", init<>() )
    .def( init<double const&>())
-   .def( init<const Proton&>());
+   .def( init<double const&, Vector const&>());
 
-  class_<JetPositron, bases<JetParticle> >("JetPositron") 
+  class_<AntiMuon, bases<Particle> >("AntiMuon", init<>() )
    .def( init<double const&>())
-   .def( init<const Positron&>());
+   .def( init<double const&, Vector const&>());
+
 
 }
 
