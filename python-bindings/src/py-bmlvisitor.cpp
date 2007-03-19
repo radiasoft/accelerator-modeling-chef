cc882ddb6be9d76262a93846bc2a46bb634770c1
ostiguy <ostiguy>
2007-03-19 16:46:02 +0000

    Added copyright header. -jfo

--- python-bindings/src/py-bmlvisitor.cpp
+++ python-bindings/src/py-bmlvisitor.cpp
@@ -1,7 +1,38 @@
+/*******************************************************************************
+********************************************************************************
+********************************************************************************
+******
+******  Python bindings for mxyzpltk/beamline libraries 
+******  
+******                                    
+******  File:      py-bmlvisitor.h
+******                                                                
+******  Copyright (c) Universities Research Association, Inc./ Fermilab    
+******                All Rights Reserved                             
+******
+******  Software and documentation created under 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  The U.S. Government retains a world-wide non-exclusive, 
+******  royalty-free license to publish or reproduce documentation 
+******  and software for U.S. Government purposes. This software 
+******  is protected under the U.S.and Foreign Copyright Laws. 
+******                                                                
+******  Author:    Jean-Francois Ostiguy                                     
+******                                                                
+******             Fermi National Laboratory, Batavia, IL   60510                                
+******             ostiguy@fnal.gov                         
+******
+********************************************************************************
+********************************************************************************
+*******************************************************************************/
+
 #include <boost/python.hpp>
 #include <beamline/BmlVisitor.h>
+#include <iostream>
+
+using namespace boost::python;
 
 void wrap_bmlvisitor () {
-  using namespace boost::python;
-  class_<BmlVisitor, boost::noncopyable >("BmlVisitor",no_init);
+
+  class_<BmlVisitor, boost::noncopyable >("BmlVisitor", no_init);
 }
