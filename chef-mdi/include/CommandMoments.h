aed5e2f341a312ffca542158a44fc17ea7cc36a3
ostiguy <ostiguy>
2007-03-19 21:50:43 +0000

    New files. These classes are used to separate functionality from the GUI logic. -jfo

---
+++ chef-mdi/include/CommandMoments.h
@@ -0,0 +1,49 @@
+/**************************************************************************
+***************************************************************************
+***************************************************************************
+******                                                               ******   
+******  CHEF:      An application layered on the Beamline/mxyzptlk   ****** 
+******             class libraries.                                  ****** 
+******                                                               ****** 
+******  File:      CommandMoments.h                                  ****** 
+******                                                               ******
+******  Copyright (c) Universities Research Association, Inc.        ****** 
+******                All Rights Reserved                            ****** 
+******                                                               ****** 
+******  Authors:    Jean-Francois Ostiguy                            ******
+******              Fermilab                                         ****** 
+******              ostiguy@fnal.gov                                 ****** 
+******                                                               ******  
+******  Usage, modification, and redistribution are subject to terms ******
+******  of the License supplied with this software.                  ****** 
+******                                                               ******
+******  Software and documentation created under                     ****** 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000.    ****** 
+******  The U.S. Government retains a world-wide non-exclusive,      ****** 
+******  royalty-free license to publish or reproduce documentation   ****** 
+******  and software for U.S. Government purposes. This software     ****** 
+******  is protected under the U.S. and Foreign Copyright Laws.      ****** 
+******  URA/FNAL reserves all rights.                                ****** 
+******                                                               ******
+***************************************************************************
+***************************************************************************
+**************************************************************************/
+#ifndef COMMANDMOMENTS_H
+#define COMMANDMOMENTS_H
+
+#include <physics_toolkit/BeamlineContext.h>
+
+class QWidget;
+
+
+class CommandMoments {
+
+ public:
+    
+  QWidget*  operator()(QWidget* parent, BmlContextPtr const& bml );  
+
+};
+
+#endif //COMMANDMOMENTS_H
+
+
