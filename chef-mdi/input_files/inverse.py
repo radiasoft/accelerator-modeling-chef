6fd5aadad68148a8b53a1b3266b6e97b2f4a77c3
ostiguy <ostiguy>
2007-03-20 21:52:08 +0000

    Initial import. new demo input files -jfo

---
+++ chef-mdi/input_files/inverse.py
@@ -0,0 +1,23 @@
+#
+mxyzptlk.BeginEnvironment(12)
+x =  mxyzptlk.coord(1.0)
+y =  mxyzptlk.coord(1.0)
+z =  mxyzptlk.coord(1.0)
+px = mxyzptlk.coord(0.0)
+py = mxyzptlk.coord(0.0)
+pz = mxyzptlk.coord(0.0)
+mxyzptlk.EndEnvironment()
+#
+
+#print mxyzptlk.sqrt(x+y+z)
+print (mxyzptlk.sqrt(x+y+z) / (x+y+z) ) * mxyzptlk.sqrt(x+y+z)
+print (mxyzptlk.sqrt(x+y+z) * mxyzptlk.sqrt(x+y+z) )/ (x+y+z)
+
+
+
+
+
+
+
+
+
