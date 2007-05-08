f1b20410e9c68403fb38f690ec300c8f239c5785
ostiguy <ostiguy>
2007-05-08 18:42:25 +0000

    New ParticleBunch class implementation and interface. Still a work in progress. -jfo

---
+++ beamline/include/WakeFunctions.h
@@ -0,0 +1,50 @@
+/*************************************************************************
+**************************************************************************
+**************************************************************************
+******                                                                
+******  BEAMLINE:  C++ objects for design and analysis
+******             of beamlines, storage rings, and   
+******             synchrotrons.                      
+******                                    
+******  File:      WakeFunctions.h
+******                                                                
+******  Copyright (c) Fermi Research Alliance LLC
+******                All Rights Reserved
+******
+******  Usage, modification, and redistribution are subject to terms          
+******  of the License supplied with this software.
+******  
+******  Software and documentation created under 
+******  U.S. Department of Energy Contract No. DE-AC02-07CH11359.
+******  The U.S. Government retains a world-wide non-exclusive, 
+******  royalty-free license to publish or reproduce documentation 
+******  and software for U.S. Government purposes. This software 
+******  is protected under the U.S. and Foreign Copyright Laws. 
+******
+******                               
+******  Author:    Jean-Francois Ostiguy                                     
+******             ostiguy@fnal.gov                         
+******                                                                
+******
+**************************************************************************
+*************************************************************************/
+
+#ifndef WAKEFUNCTIONS_H
+#define WAKEFUNCTIONS_H
+
+class ShortRangeLWakeFunction {
+
+  public:
+   double operator()( double const& s) const;
+
+} ;
+
+class ShortRangeTWakeFunction {
+
+  public:
+   double operator()( double const& s) const;
+
+};
+
+
+#endif // WAKEFUNCTIONS_H
