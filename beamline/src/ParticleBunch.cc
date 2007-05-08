33d990986dd957297b05bf5365fb04d710f579f3
ostiguy <ostiguy>
2007-05-08 18:46:30 +0000

    Added definitions for smart ptrs holding various types of Particle ptrs.
    -jfo

--- beamline/src/ParticleBunch.cc
+++ beamline/src/ParticleBunch.cc
@@ -5,37 +5,28 @@
 ******  Beamline:  C++ objects for design and analysis
 ******             of beamlines, storage rings, and   
 ******             synchrotrons.                      
-******  Version:   2.0                    
 ******                                    
 ******  File:      ParticleBunch.cc
 ******                                                                
-******  Copyright Universities Research Association, Inc./ Fermilab    
+******  Copyright Fermi Research Alliance / Fermilab    
 ******            All Rights Reserved                             
 *****
 ******  Usage, modification, and redistribution are subject to terms          
 ******  of the License supplied with this software.
 ******  
 ******  Software and documentation created under 
-******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  U.S. Department of Energy Contract No. DE-AC02-07CH11359 
 ******  The U.S. Government retains a world-wide non-exclusive, 
 ******  royalty-free license to publish or reproduce documentation 
 ******  and software for U.S. Government purposes. This software 
 ******  is protected under the U.S. and Foreign Copyright Laws.
 ******                                                                
 ******                                                                
-******  Author:    Leo Michelotti                                     
-******                                                                
-******             Fermilab                                           
-******             P.O.Box 500                                        
-******             Mail Stop 220                                      
-******             Batavia, IL   60510                                
-******                                                                
-******             Phone: (630) 840 4956                              
-******             Email: michelotti@fnal.gov                         
+******  Author:    Jean-Francois Ostiguy                                     
+******             ostiguy@fnal.gov                                                   
 ******                                                                
 **************************************************************************
 *************************************************************************/
-
 #if HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -44,9 +35,12 @@
 
 #include <basic_toolkit/iosetup.h>
 #include <basic_toolkit/MathConstants.h>
+
 #include <cmath>
 #include <ostream>
 #include <iomanip>
+
+#include<boost/random.hpp>
 
 using namespace std;
 
@@ -54,141 +48,20 @@ using FNAL::pcout;
 using FNAL::pcerr;
 
 
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-//
-//  Class ParticleBunch
-//
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
+namespace {
 
-ParticleBunch::Discriminator::Discriminator( const ParticleBunch* x )
-: _within(x)
-{}
+typedef  boost::rand48         base_generator_type;
 
+base_generator_type generator1((int32_t) 117u);
+base_generator_type generator2((int32_t) 137u);
 
-ParticleBunch::Discriminator::Discriminator( const Discriminator& x )
-: _within( x._within )
-{}
-
-
-ParticleBunch::ParticleBunch()
-{}
-
-
-ParticleBunch::~ParticleBunch() 
-{
- 
- // No need to explicitly invoke delete on a shared_ptr.  
- // delete( shared_ptr ) is called when the stl container is deleted.
+boost::variate_generator<base_generator_type&, boost::uniform_real<> > srnd(generator1,   boost::uniform_real<>(0.0, 1.0 )   );
+boost::variate_generator<base_generator_type&, boost::uniform_real<> > trnd(generator2,   boost::uniform_real<>(0.0, 2*M_PI) );
 
 }
 
-
-ParticleBunch::iterator   ParticleBunch::begin()
-{
-  return bunch_.begin();
-}
- 
-ParticleBunch:: iterator  ParticleBunch::end()
-{
-  return bunch_.end();
-}
-
-ParticleBunch::const_iterator   ParticleBunch::begin() const
-{
-  return bunch_.begin();
-}
- 
-ParticleBunch:: const_iterator  ParticleBunch::end() const
-{
-  return bunch_.end();
-}
-
-
-void ParticleBunch::append( const Particle& x )
-{
-  bunch_.push_back( ParticlePtr( x.Clone()) ); 
-}
-
-
-void ParticleBunch::append( ParticlePtr p )
-{
-  bunch_.push_back( p ); 
-} 
-
-
-std::vector<ParticlePtr> ParticleBunch::remove( Discriminator& dsc )
-{
-#if  0
-  //list<Particle*>::iterator collector 
-  //    = remove_if ( bunch_.begin(), bunch_.end(), dsc );
-
-  // This is kludged, until I can figure out how
-  // to make std algorithms do what I want them to do.
-  // This really seems horribly inefficient
-  // compared to using a dlist_traversor
-
-  list<ParticlePtr> marked;
-  list<ParticlePtr>::iterator cur;
-  for( cur  = bunch_.begin();
-       cur != bunch_.end();
-       cur++                ) 
-  {
-    if( dsc( *cur ) ) { 
-      marked.push_back( *cur ); 
-    }
-  }
-
-  for( cur  = marked.begin();
-       cur != marked.end();
-       cur++                  )
-  {
-    list<ParticlePtr>::iterator new_end 
-      = bunch_.erase( std::remove( bunch_.begin(), bunch_.end(), *cur ), bunch_.end() );
-  }
-  
-  return marked;
-#endif
-}
-
-
-void ParticleBunch::clear()
-{
-  clear();
-}
-
-
-int ParticleBunch::size() const
-{
-  return bunch_.size();
-}
-
-
-bool ParticleBunch::empty() const
-{
-  return bunch_.empty();
-}
-
-
-// FAILS: list<Particle*> ParticleBunch::remove( Discriminator& dsc )
-// FAILS: {
-// FAILS:   list<Particle*>::iterator new_end
-// FAILS:     = remove_if( bunch_.begin(), bunch_.end(), dsc );
-// FAILS:   // Contrary to its name, remove_if actually removes nothing.
-// FAILS: 
-// FAILS:   list<Particle*> ret;
-// FAILS:   list<Particle*>::iterator cur;
-// FAILS:   for( cur  = new_end; cur != bunch_.end(); cur++ ) {
-// FAILS:     ret.push_back( *cur ); 
-// FAILS:   }
-// FAILS:   bunch_.erase( new_end, bunch_.end() );
-// FAILS: 
-// FAILS:   return ret;
-// FAILS: }
-
-
-
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 std::ostream& operator<<( std::ostream &os, ParticleBunch const& bunch) 
 {
@@ -207,6 +80,9 @@ std::ostream& operator<<( std::ostream &os, ParticleBunch const& bunch)
   }
   return os;
 }
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
 std::istream& operator >>( std::istream &is, ParticleBunch& bunch) 
@@ -228,220 +104,236 @@ std::istream& operator >>( std::istream &is, ParticleBunch& bunch)
   return is;
 }
 
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-//
-//  Class ProtonBunch
-//
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-
-ProtonBunch::ProtonBunch() : ParticleBunch() 
+ParticleBunch::ParticleBunch(Particle const& p, int nparticles)
+:
+  bunch_( nparticles, ParticlePtr() ), reference_( ParticlePtr( p.Clone()) )  
 {
+
+  //-------------------------------------------------------------------------------
+  // For performance, particles should be allocated *contiguously* in memory !!!!!
+  // FIX ME !
+  //-------------------------------------------------------------------------------
+  for ( iterator it  = bunch_.begin();  it != bunch_.end(); ++it )  {
+  
+   *it = ParticlePtr( p.Clone());
+
+ }
+
+}
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+ParticleBunch::~ParticleBunch() 
+{}
+
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+ParticleBunch::iterator   ParticleBunch::begin()
+{
+  return bunch_.begin();
 }
  
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-ProtonBunch::~ProtonBunch()
+ParticleBunch:: iterator  ParticleBunch::end()
 {
+  return bunch_.end();
+}
 
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+ParticleBunch::const_iterator   ParticleBunch::begin() const
+{
+  return bunch_.begin();
 }
  
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-void ProtonBunch::append( const Proton& p) 
+ParticleBunch:: const_iterator  ParticleBunch::end() const
 {
-  ParticleBunch::append(p);
+  return bunch_.end();
 }
 
 
-void ProtonBunch::append( ProtonPtr p) 
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void ParticleBunch::append( Particle const& x )
 {
-  ParticleBunch::append(p);
+  bunch_.push_back( ParticlePtr( x.Clone()) ); 
 }
 
 
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-Proton* ProtonBunch::makeParticle( double energy, Vector const& state )
+void ParticleBunch::append( ParticlePtr const&  p )
 {
-  return new Proton( energy, state );
+  bunch_.push_back( p ); 
+} 
+
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+//std::vector<ParticlePtr> ParticleBunch::remove( Discriminator& dsc )
+//{}
+
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void ParticleBunch::clear()
+{
+  clear();
+}
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+int ParticleBunch::size() const
+{
+  return bunch_.size();
 }
 
 
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-//
-//  Class PositronBunch
-//
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-PositronBunch::PositronBunch() : ParticleBunch() 
+bool ParticleBunch::empty() const
 {
+  return bunch_.empty();
 }
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void  ParticleBunch::populateGaussian( PhaseSpaceProjection psid, double sigma_x, double sigma_px, double r_12 )
+{
+
+  int M = 10; //  Strictly speaking, the limit M --> infty is the Gaussian, but 30 is more than good enough
+              //  Cutoff will be at sqrt(10.5) = 3.2 sigma
+    
+  double x_lim  = sqrt(M + 0.5) *  sigma_x;
+  double px_lim = sqrt(M + 0.5) *  sigma_px;
+ 
+  populateBinomial ( psid,  M,  x_lim,  px_lim, r_12 ); 
+
+
+}
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void  ParticleBunch::populateWaterBag ( PhaseSpaceProjection psid, double x_rms, double px_rms,  double r_12 )
+{
+
+  // Water Bag = uniform in phase space 
+
+ double M = 1.0;
+
+ double x_lim  = sqrt(M + 0.5) *  x_rms;
+ double px_lim = sqrt(M + 0.5) *  px_rms;
+
+ populateBinomial ( psid,  M,  x_lim,  px_lim, r_12 ); 
+
+}
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void  ParticleBunch::populateParabolic( PhaseSpaceProjection psid, double x_rms, double px_rms,  double r_12 )
+{
+
+  double M = 2.0; 
+
+  double x_lim  = sqrt(M + 0.5) *  x_rms;
+  double px_lim = sqrt(M + 0.5) *  px_rms;
+
+  populateBinomial ( psid,  M,  x_lim,  px_lim, r_12 ); 
+
+
+
+}
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void  ParticleBunch::populateKV       ( PhaseSpaceProjection psid, double x_rms, double px_rms,  double r_12 )
+{
+  // KV = uniform projection  (KV in 2N phase space ==> uniform projection in N (momentum or coordinates) subspace    
+  
+  double M = 1.0e-9; // Stricly speaking, KV is the limit  M-->0, but this is pretty good. 
+  populateBinomial ( psid,  M,  x_rms,  px_rms, r_12 ); 
+
+}
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void ParticleBunch::populateBinomial ( PhaseSpaceProjection psid, double M, double x_lim, double px_lim, double r_12)
+{
+
+ double chi    = asin(r_12);
+
+ double a     = 0.0;
+ double alpha = 0.0;
+ double u     = 0.0;
+ double v     = 0.0;
+ double x     = 0.0;
+ double px    = 0.0;
+  
+ std::vector<std::pair<double,double> > phase_space;
+
  
 
-PositronBunch::PositronBunch(int nm, double energy, double* widths,
-                             Distribution& distrib,
-                             ParticleBunch::Discriminator* pBunchPredicate )
-: ParticleBunch() 
-{
- #if 0                               
- Positron* p;
- double x[ BMLN_dynDim ];
+ for ( iterator it  = bunch_.begin(); it != bunch_.end(); ++it ) {
 
- while( iNOfTracked_ < nm ) {
-   
-   p = new Positron(energy);
-   x[0] = distrib.getValue()*widths[0];
-   x[1] = distrib.getValue()*widths[1];
-   x[2] = distrib.getValue()*widths[2];
-   x[3] = distrib.getValue()*widths[3];
-   x[4] = distrib.getValue()*widths[4];
-   x[5] = distrib.getValue()*widths[5];
-   p->setState( x );
-   
-   if (( pBunchPredicate != 0 ) && ( (*pBunchPredicate)( *p ) == 0 )) {
-     delete p;
-   } else {
-     append( p );
-     iNOfTracked_++;
-   }
-   iNOfTotal_++;   
+    a     = sqrt(1.0 - pow( srnd(), 1.0/M ));
+
+    alpha = trnd();
+
+    u     = a*cos(alpha);
+    v     = a*sin(alpha);
+
+    Vector& state = (*it)->getState(); 
+ 
+    state[0+psid]    += x_lim*u;
+    state[3+psid]    += px_lim*(u*sin(chi) + v*cos(chi)); 
+
  }
- #endif
+
+  return;
 }
 
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-PositronBunch::PositronBunch(int nm, double energy, double* widths,
-			     double* offsets,Distribution& distrib,
-                             ParticleBunch::Discriminator* pBunchPredicate ) 
-: ParticleBunch() 
+void ParticleBunch::setReferenceParticle ( Particle const& p)
 {
- #if 0
- Positron* p;
- double x[ BMLN_dynDim ];
 
- while( iNOfTracked_ < nm ) {
-   
-   p = new Positron(energy);
-   x[0] = distrib.getValue()*widths[0] + offsets[0];
-   x[1] = distrib.getValue()*widths[1] + offsets[1];
-   x[2] = distrib.getValue()*widths[2] + offsets[2];
-   x[3] = distrib.getValue()*widths[3] + offsets[3];
-   x[4] = distrib.getValue()*widths[4] + offsets[4];
-   x[5] = distrib.getValue()*widths[5] + offsets[5];
-   p->setState( x );
-   
-   if (( pBunchPredicate != 0 ) && ( (*pBunchPredicate)( *p ) == 0 )) {
-     delete p;
-   } else {
-     append( p );
-     iNOfTracked_++;
-   }
-   iNOfTotal_++;   
- }
- #endif
+  reference_ = ParticlePtr(new Particle(p));
+
 }
 
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-PositronBunch::~PositronBunch() 
+Particle const& ParticleBunch::getReferenceParticle () const
+
 {
+
+  return *reference_;
 }
 
-
-void PositronBunch::recreate(int nm, double energy, double* widths,
-			     Distribution& distrib, 
-                             ParticleBunch::Discriminator* pBunchPredicate ) 
-{
- #if 0
- Positron* p;
- double x[ BMLN_dynDim ];
-				// First get rid of the old particles
- slist_iterator getNext( *(slist*) this );
- while( (p = (Positron*) getNext()) ) delete p;
-
- clear();
- iNOfTracked_ = 0;
- iNOfTotal_   = 0;
-
- while( iNOfTracked_ < nm ) {
-   
-   p = new Positron(energy);
-   x[0] = distrib.getValue()*widths[0];
-   x[1] = distrib.getValue()*widths[1];
-   x[2] = distrib.getValue()*widths[2];
-   x[3] = distrib.getValue()*widths[3];
-   x[4] = distrib.getValue()*widths[4];
-   x[5] = distrib.getValue()*widths[5];   
-   p->setState( x );
-   
-   if (( pBunchPredicate != 0 ) && ( (*pBunchPredicate)( *p ) == 0 )) {
-     delete p;
-   } else {
-     append( p );
-     iNOfTracked_++;
-   }
-   iNOfTotal_++;   
- }
- #endif
-}
-
-
-void PositronBunch::recreate(int nm, double energy, double* widths, double* offsets,
-                             Distribution& distrib, 
-                             ParticleBunch::Discriminator* pBunchPredicate )
-{
- #if 0
- Positron* p;
- double x[ BMLN_dynDim ];
-
-				// First get rid of the old particles
- slist_iterator getNext( *(slist*) this );
- while( (p = (Positron*) getNext()) ) delete p;
- clear();
-
- iNOfTracked_ = 0;
- iNOfTotal_   = 0;
-
- while( iNOfTracked_ < nm ) {
-
-   p = new Positron(energy);
-   x[0] = distrib.getValue()*widths[0] + offsets[0];
-   x[1] = distrib.getValue()*widths[1] + offsets[1];
-   x[2] = distrib.getValue()*widths[2] + offsets[2];
-   x[3] = distrib.getValue()*widths[3] + offsets[3];
-   x[4] = distrib.getValue()*widths[4] + offsets[4];
-   x[5] = distrib.getValue()*widths[5] + offsets[5];
-   p->setState( x );
-   
-   if (( pBunchPredicate != 0 ) && ( (*pBunchPredicate)( *p ) == 0 )) {
-     delete p;
-   } else {
-     append( p );
-     iNOfTracked_++;
-   }
-   iNOfTotal_++;
- }
- #endif
-}
-
-
-void PositronBunch::append( const Positron& p) 
-{
-  ParticleBunch::append(p);
-}
-
-
-void PositronBunch::append( PositronPtr p) 
-{
-  ParticleBunch::append(p);
-}
-
-
-
-Positron* PositronBunch::makeParticle( double energy, Vector const& state )
-{
-  return new Positron( energy, state );
-}
-
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
