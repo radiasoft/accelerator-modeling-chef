7c5ddafc4a80a532169ec5548d45d99ec5a95f9c
ostiguy <ostiguy>
2007-05-08 19:08:44 +0000

    Order of members offset_ and weight_ has been modified. weight_ is now last, which makes the process  of eventually eliminating it a bit easier. -jfo

--- mxyzptlk/src/TJLterm.cc
+++ mxyzptlk/src/TJLterm.cc
@@ -65,8 +65,8 @@ using FNAL::pcerr;
 
 template<>
 template<>
-TJLterm<std::complex<double> >::TJLterm( TJLterm<double> const& x):
-  value_(x.value_,0.0), weight_(x.weight_), offset_(x.offset_) 
+TJLterm<std::complex<double> >::TJLterm( TJLterm<double> const& x)
+  : value_(x.value_,0.0), offset_(x.offset_), weight_(x.weight_) 
 {}
 
 // |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
