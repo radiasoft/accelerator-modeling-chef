c8bd3610435a2c0f31577a099a91f244a7a500cb
ostiguy <ostiguy>
2007-05-31 22:10:40 +0000

    Changed header info. -jfo

--- beamline/src/gkick.cc
+++ beamline/src/gkick.cc
@@ -5,26 +5,25 @@
 ******  BEAMLINE:  C++ objects for design and analysis
 ******             of beamlines, storage rings, and   
 ******             synchrotrons.                      
-******  Version:   2.0                    
 ******                                    
 ******  File:      gkick.cc
 ******                                                                
-******  Copyright Universities Research Association, Inc./ Fermilab    
-******            All Rights Reserved                             
-*****
+******  Copyright (c) Fermi Research Alliance LLC 
+******                All Rights Reserved
+******
 ******  Usage, modification, and redistribution are subject to terms          
 ******  of the License supplied with this software.
 ******  
 ******  Software and documentation created under 
-******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
+******  U.S. Department of Energy Contract No. DE-AC02-07CH11359 
 ******  The U.S. Government retains a world-wide non-exclusive, 
 ******  royalty-free license to publish or reproduce documentation 
 ******  and software for U.S. Government purposes. This software 
-******  is protected under the U.S. and Foreign Copyright Laws.
+******  is protected under the U.S. and Foreign Copyright Laws. 
+******
 ******                                                                
 ******  Author:    Jean-Francois Ostiguy                                     
-******             ostiguy@fnal.gov                         
-******
+******             ostiguy@fnal.gov  
 ****** 
 **************************************************************************
 *************************************************************************/
