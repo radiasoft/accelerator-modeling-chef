7d9bc4345448e9fa05f1c562df333b56d4013de1
michelot <michelot>
2007-07-27 13:08:11 +0000

    Files rewritten so that they will compile and link within the MAIN trunk.  The executables have not yet been run to see if they still produce correct answers.  Testing continues ...              -lpjm

--- integrator/demos/Test_sym.cc
+++ integrator/demos/Test_sym.cc
@@ -1,14 +1,98 @@
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 
+#include <integrator/ODE.h>
 #include <basic_toolkit/VectorD.h>
 #include <integrator/Symplectic.h>
-#include <integrator/DriftMap.h>
-#include <integrator/KickMap.h>
 
 using namespace std;
+
+
+// ******************************************
+// class DriftMap
+// ******************************************
+class DriftMap : public ODE 
+{
+ private:
+ public:
+   DriftMap();
+   virtual ~DriftMap();
+   Vector operator() (const Vector&, double);
+};
+
+
+DriftMap::DriftMap() 
+:   ODE() 
+{
+  dimen = 2;
+}
+
+DriftMap::~DriftMap() 
+{
+}
+
+Vector DriftMap::operator() (const Vector& state, double t )
+{
+  Vector ret(dimen);
+  if (dimen == 2){
+    ret(0) = state(0)+t*state(1);
+    ret(1) = state(1);
+    return ret;
+  }
+  else{
+    (*pcerr) << "DriftMap::operator(): Wrong dimension." << std::endl;
+    return 0;
+  }
+}
+
+
+// ******************************************
+// class KickMap
+// ******************************************
+class KickMap : public ODE {
+
+ private:
+ public:
+   KickMap();
+   virtual ~KickMap();
+   Vector operator() (const Vector&, double);
+};
+
+
+KickMap::KickMap() 
+:   ODE() 
+{
+  dimen = 2;
+}
+
+
+KickMap::~KickMap() {
+}
+
+
+Vector KickMap::operator() (const Vector& state, double t ){
+  Vector ret(dimen);
+  if (dimen == 2){
+    ret(0) =  state(0);
+    ret(1) = -t*state(0)+state(1);
+    return ret;
+  }
+  else{
+    (*pcerr) << "KickMap::operator(): Wrong dimension." << std::endl;
+    return 0;
+  }
+}
+
+
+// ******************************************
+// Main program
+// ******************************************
 
 main()
 {
