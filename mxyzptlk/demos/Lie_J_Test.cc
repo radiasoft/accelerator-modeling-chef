a04a1068f707c7782eaf382bac3fa5586362277d
michelot <michelot>
2007-07-31 14:10:20 +0000

    Continuing changes while testing.  These are mostly aesthetic.  - lpjm

--- mxyzptlk/demos/Lie_J_Test.cc
+++ mxyzptlk/demos/Lie_J_Test.cc
@@ -14,8 +14,7 @@ using namespace std;
 int main( int argc, char** argv ) 
 {
   if( argc != 3 ) {
-    cout << "Usage: " << argv[0]
-         << "  <d>  <t>"
+    cout <<   "Usage: " << argv[0] << "  d  t"
             "\nwhere  d  = highest order derivative"
             "\n       t  = upper limit of integration"
          << endl;
@@ -34,25 +33,25 @@ int main( int argc, char** argv )
   CLieOperator u, v;
   JetC r1, r2;
 
-  u.SetComponent(  0, x*x );
-  u.SetComponent(  1, y*y );
-  u.SetComponent(  2, z*z );
+  u.SetComponent( 0, x*x );
+  u.SetComponent( 1, y*y );
+  u.SetComponent( 2, z*z );
 
-  v.SetComponent(  0, y*z );
-  v.SetComponent(  1, z*x );
-  v.SetComponent(  2, x*y );
+  v.SetComponent( 0, y*z );
+  v.SetComponent( 1, z*x );
+  v.SetComponent( 2, x*y );
 
-  cout << "\n-*-*-*-  x  coefficients  -*-*-*-*-*\n";
+  cout << "\n-*-*-*-  x  coefficients  -*-*-*-*-*" << endl;
   r1 = u.expMap( t, v.expMap( t, x ) );
   r2 = v.expMap( t, u.expMap( t, x ) );
   (r1 - r2).printCoeffs();
 
-  cout << "\n-*-*-*-  y  coefficients  -*-*-*-*-*\n";
+  cout << "\n-*-*-*-  y  coefficients  -*-*-*-*-*" << endl;
   r1 = u.expMap( t, v.expMap( t, y ) );
   r2 = v.expMap( t, u.expMap( t, y ) );
   (r1 - r2).printCoeffs();
 
-  cout << "\n-*-*-*-  z  coefficients  -*-*-*-*-*\n";
+  cout << "\n-*-*-*-  z  coefficients  -*-*-*-*-*" << endl;
   r1 = u.expMap( t, v.expMap( t, z ) );
   r2 = v.expMap( t, u.expMap( t, z ) );
   (r1 - r2).printCoeffs();
