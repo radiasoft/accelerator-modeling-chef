ace4e3663e4b5f08bf3d202b83c6986f6b8f560b
ostiguy <ostiguy>
2007-08-28 21:57:35 +0000

    - initial import jfo

---
+++ chef-mdi/src/CommandPropagateBunch.cpp
@@ -0,0 +1,66 @@
+/**************************************************************************
+***************************************************************************
+***************************************************************************
+******                                                               ******   
+******  CHEF:      An application layered on the Beamline/mxyzptlk   ****** 
+******             class libraries.                                  ****** 
+******                                                               ****** 
+******  File:      CommandPropagateBunch.cpp                         ****** 
+******                                                               ******
+******  Copyright (c) Universities Research Association, Inc.        ****** 
+******                All Rights Reserved                            ****** 
+******                                                               ****** 
+******  Author:                                                      ******
+******              Jean-Francois Ostiguy                            ******
+******              Fermilab                                         ****** 
+******              ostiguy@fnal.gov                                 ****** 
+******                                                               ******  
+******  Usage, modification, and redistribution are subject to terms ******
+******  of the License supplied with this software.                  ****** 
+******                                                               ******
+******  Software and documentation created under                     ****** 
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000.    ****** 
+******  The U.S. Government retains a world-wide non-exclusive,      ****** 
+******  royalty-free license to publish or reproduce documentation   ****** 
+******  and software for U.S. Government purposes. This software     ****** 
+******  is protected under the U.S. and Foreign Copyright Laws.      ****** 
+******  URA/FNAL reserves all rights.                                ****** 
+******                                                               ******
+***************************************************************************
+***************************************************************************
+**************************************************************************/
+#include <CommandDispersion.h>
+
+#include<string>
+#include<vector>
+#include<physics_toolkit/BeamlineContext.h>
+#include<physics_toolkit/DispersionSage.h>
+#include<CHEFPlotMain.h>
+#include<BunchData.h>
+#include<qwidget.h>
+
+using namespace std;
+
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+QWidget* CommandPropagateBunch::operator()( QWidget* parent, BmlContextPtr const& bml ) 
+{ 
+
+   CHEFPlotMain* plot =  new CHEFPlotMain(  parent, "plotWidget", Qt::WDestructiveClose );
+
+   string caption = "CHEF:  PropagateBunch";
+
+   plot->setCaption( caption.c_str() );
+   plot->setGeometry(0,0, parent->width(), parent->height() );
+   plot->setAutoClear(true);
+
+   BunchData data (  );
+   plot->addData( data );
+
+   return plot;
+}
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
