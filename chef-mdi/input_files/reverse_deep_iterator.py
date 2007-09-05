1af0d535c26fae9a3616f6c9140e91a8ac995674
ostiguy <ostiguy>
2007-09-05 16:09:43 +0000

    o fix reflecting support for multiple parsers -jfo

--- chef-mdi/input_files/reverse_deep_iterator.py
+++ chef-mdi/input_files/reverse_deep_iterator.py
@@ -25,7 +25,7 @@ pr     = beamline.Proton( energy )
 # read a lattice file and instantiate a beamline
 # 
 
-bfact    = bmlfactory.bmlfactory("Tevatron-E.lat")
+bfact    = bmlfactory.MAD8Factory("Tevatron-E.lat")
 tevatron = bfact.create_beamline("normal_tevatron", pr.ReferenceBRho() )
 
 # reverse_deep_iterator
@@ -37,6 +37,7 @@ tevatron = bfact.create_beamline("normal_tevatron", pr.ReferenceBRho() )
 
 for element in tevatron.reverse_iterator():
 	print element.Name(), element.Type()
+
 
 
 
