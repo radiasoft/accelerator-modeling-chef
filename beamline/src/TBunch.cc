5b478952dd2d19440abb70ad9020539e34a8a817
ostiguy <ostiguy>
2007-10-22 22:13:43 +0000

    o fixed emittance calculation: do not assume the bunch centroid is at the origin -jfo

--- beamline/src/TBunch.cc
+++ beamline/src/TBunch.cc
@@ -50,6 +50,14 @@ base_generator_type generator2(static_cast<boost::int32_t>(137u) );
 boost::variate_generator<base_generator_type&, boost::uniform_real<> > srnd(generator1,   boost::uniform_real<>(0.0, 1.0 )   );
 boost::variate_generator<base_generator_type&, boost::uniform_real<> > trnd(generator2,   boost::uniform_real<>(0.0, 2*M_PI) );
 
+
+Particle::PhaseSpaceIndex const&   i_x     = Particle::xIndex; 
+Particle::PhaseSpaceIndex const&   i_y     = Particle::yIndex; 
+Particle::PhaseSpaceIndex const&   i_cdt   = Particle::cdtIndex; 
+Particle::PhaseSpaceIndex const&   i_npx   = Particle::npxIndex; 
+Particle::PhaseSpaceIndex const&   i_npy   = Particle::npyIndex; 
+Particle::PhaseSpaceIndex const&   i_ndp   = Particle::ndpIndex;
+
 }
 
 
@@ -71,12 +79,9 @@ std::vector<double>  TBunch<Particle>::emittances() const
   //  
   //-----------------------------------------------------------------------------------
 
-  
-
-  std::vector<double> u_2 (3, 0.0);
-  std::vector<double> up_2(3, 0.0);
-  std::vector<double> u_up(3, 0.0);
-
+  std::vector<double>  u_2 (6, 0.0);
+  std::vector<double>  u_up(3, 0.0);
+  std::vector<double>  ubar(6, 0.0);
 
   for (TBunch<Particle >::const_iterator it = begin(); it != end(); ++it ) { 
 
@@ -84,33 +89,42 @@ std::vector<double>  TBunch<Particle>::emittances() const
 
     double const npz = it->get_npz();
      
-     u_2[0] +=  (state[0] * state[0]);  
-    up_2[0] +=  (state[3] * state[3])/( npz*npz);
-    u_up[0] +=  (state[0] * state[3])/ npz;  
+    for (int i=0; i<6; ++i) { 
+         u_2[i]  +=  (state[i] * state[i]);  
+         ubar[i] +=   state[i]; 
+    }
 
-     u_2[1] +=  (state[1] * state[1]);  
-    up_2[1] +=  (state[4] * state[4])/( npz*npz);
-    u_up[1] +=  (state[1] * state[4])/ npz;  
+    u_2[i_npx] /= ( npz*npz);
+    u_2[i_npy] /= ( npz*npz);
 
-     u_2[2] +=  (state[2] * state[2]);  
-    up_2[2] +=  (state[5] * state[5]);
-    u_up[2] +=  (state[2] * state[5]);
+    ubar[i_npx] /=  npz; 
+    ubar[i_npy] /=  npz; 
+
+    u_up[0] +=  (state[i_x]   * state[i_npx])/ npz;  
+    u_up[1] +=  (state[i_y]   * state[i_npy])/ npz;  
+    u_up[2] +=  (state[i_cdt] * state[i_ndp]);
 
   }
 
   int const n = size();
 
+  // subtract centroid 
+ 
+  for ( int i=0; i<6; ++i ) {
+    u_2[i]  -= (  ubar[i]*ubar[i] ) / n; 
+    
+  }
+
   for ( int i=0; i<3; ++i ) {
-     u_2[i] /= n; 
-    up_2[i] /= n; 
-    u_up[i] /= n;
+    u_up[i] -= (  ubar[i]*ubar[i]) / n; 
   }
 
   std::vector<double> eps(3);
 
-  for (int i=0; i<3; ++i ) {
-    eps[i] = sqrt( u_2[i]*up_2[i] - u_up[i]*u_up[i] ); 
-  }
+  eps[0] = sqrt( u_2[i_x]  *u_2[i_npx] - u_up[0]*u_up[0] ) / n ;
+  eps[1] = sqrt( u_2[i_y]  *u_2[i_npy] - u_up[1]*u_up[1] ) / n ;
+  eps[2] = sqrt( u_2[i_cdt]*u_2[i_ndp] - u_up[2]*u_up[2] ) / n ;
+  
 
   return eps;
 
