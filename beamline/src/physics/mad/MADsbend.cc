e0096ee56e88392e24b15ea47e757b41ebb00eec
ostiguy <ostiguy>
2008-01-17 16:23:10 +0000

    Refactored templated implementation. Now includes edge focusing.
    -jfo

--- beamline/src/physics/mad/MADsbend.cc
+++ beamline/src/physics/mad/MADsbend.cc
@@ -5,7 +5,6 @@
 ******  BEAMLINE:  C++ objects for design and analysis
 ******             of beamlines, storage rings, and   
 ******             synchrotrons.                      
-******  Version:   2.0                    
 ******                                    
 ******  File:      MADsbend.cc
 ******                                                                
@@ -32,8 +31,19 @@
 ******             Phone: (630) 840 4956                              
 ******             Email: michelotti@fnal.gov                         
 ******                                                                
-******                                                                
-**************************************************************************
+******  Jan 2008 ostiguy@fnal.gov
+******  
+******  - refactored version                        
+******  - templated implementation
+******  - Propagator is now appropriate for a generic wedge 
+******    bend magnet and optinally includes vertical focusing, arbitrary edge 
+******    angles and finite body gradient. 
+******  - added comments to document the impact of the choice of cdt
+******    rather than ds (arc length) as a long phase space coordinate.  
+******  - combined support for sbend CF_sbend 
+******                                      
+*************************************************************************
+*************************************************************************
 *************************************************************************/
 
 #if HAVE_CONFIG_H
@@ -42,150 +52,154 @@
 
 #include <basic_toolkit/GenericException.h>
 #include <beamline/sbend.h>
+#include <beamline/CF_sbend.h>
 #include <beamline/Particle.h>
 #include <beamline/JetParticle.h>
+#include <algorithm>
 
-sbend::MAD_Prop   sbend::LikeMAD;
+sbend::MAD_Prop sbend::LikeMAD;
 
-const double changeMADConvention = - 1.0;
+namespace { 
+ int const i_x   = Particle::xIndex();     
+ int const i_y   = Particle::yIndex();     
+ int const i_cdt = Particle::cdtIndex();   
+ int const i_npx = Particle::npxIndex();   
+ int const i_npy = Particle::npyIndex();   
+ int const i_ndp = Particle::ndpIndex();   
 
-int sbend::MAD_Prop::operator()( bmlnElmnt* p_be, Particle& p ) {
-  sbend* pbe = (sbend*) p_be;
-  static  int     i, j;
-  static  double  matrix [ BMLN_dynDim ][BMLN_dynDim];
-  static  double  inState[ BMLN_dynDim ], outState[ BMLN_dynDim ];
-  double rho;
-  
-  for   ( i = 0; i < BMLN_dynDim; i++ ) {
-    for ( j = 0; j < BMLN_dynDim; j++ )
-      matrix[i][j] = 0.0;
-    matrix[i][i] = 1.0;
-  }
-  
-  if(pbe->Strength() != 0.0) {
-    rho = p.BRho()/pbe->Strength();
 
-    matrix[0][0] = cos(pbe->getAngle());
-    matrix[0][3] = sin(pbe->getAngle())*rho;
-    matrix[0][5] = rho*(1.0 - cos(pbe->getAngle()))/p.Beta();
-    matrix[1][4] = pbe->Length();
-    matrix[2][0] = changeMADConvention*(-sin(pbe->getAngle())/p.Beta());
-    matrix[2][3] = changeMADConvention*(-matrix[0][5]);
-    matrix[2][5] = changeMADConvention *(pbe->Length()/p.Beta()/
-                                         p.Gamma()/
-					 p.Gamma() - 
-					 (pbe->Length() - sin(pbe->getAngle())*rho)
-					 /p.Beta());
-    matrix[3][0] = -sin(pbe->getAngle())/rho;
-    matrix[3][3] = matrix[0][0];
-    matrix[3][5] = changeMADConvention*(-matrix[2][0]);
-    // Finally .. the mapping ...............................
+template <typename T>
+inline double standardPart( T const& );
 
-    if( BMLN_dynDim != 6 ) {
+template<>
+inline double standardPart( Jet const& value)    { return value.standardPart(); }
+
+template<>
+inline double standardPart( double const& value) { return value; }
+ 
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+template < typename Element_t, typename Particle_t>
+int propagate( Element_t& elm, Particle_t& p, double const& rho, double const& n )
+{
+
+  if( BMLN_dynDim != 6 ) {
       throw(  GenericException( __FILE__, __LINE__,
              "int sbend::MAD_Prop::operator()( bmlnElmnt* p_be, Particle& p ) {", 
              "This was written for a phase space dimension of 6." ) );
-    }
+  }
 
-    inState[0] = p.get_x();
-    inState[1] = p.get_y();
-    inState[2] = p.get_cdt();
-    inState[3] = p.get_npx();
-    inState[4] = p.get_npy();
-    inState[5] = p.get_ndp();
+  typedef typename PropagatorTraits<Particle_t>::State_t     State_t;
+  typedef typename PropagatorTraits<Particle_t>::Component_t Component_t;
 
-    for( i = 0; i < BMLN_dynDim; i++  ) {
-      outState[i] = 0.0;
-      for( j = 0; j < BMLN_dynDim; j++  ) 
-	outState[i] += matrix[i][j]*inState[j];
-    }
+  State_t& state = p.State();
+  
+  double const h      = 1/rho;                    // curvature
+  double const length = elm.Length();         
+  double const gamma  = standardPart( p.Gamma() );
+  double const beta   = standardPart( p.Beta()  );
 
-    p.set_x  ( outState[0] );
-    p.set_y  ( outState[1] );
-    p.set_cdt( outState[2] );
-    p.set_npx( outState[3] );
-    p.set_npy( outState[4] );
-    p.set_ndp( outState[5] );
+  double const angle   = elm.getAngle();
 
-  } else {
-    p.set_x  ( p.get_x() + pbe->Length() * p.get_npx() );
-    p.set_y  ( p.get_y() + pbe->Length() * p.get_npy() );
-    p.set_cdt( p.get_cdt() - pbe->Length()/p.Beta()/p.Gamma()/p.Gamma() * p.get_ndp() );
+  if(angle == 0.0) {
+    state[i_x  ] +=   length * state[i_npx];
+    state[i_y  ] +=   length * state[i_npy];
+    state[i_cdt] +=  -length /(beta*gamma*gamma) * state[i_ndp];
+    return 0;
   }  
 
-  return 0;
-}
-
-int sbend::MAD_Prop::operator()( bmlnElmnt* p_be, JetParticle& p ) {
-  sbend* pbe = (sbend*) p_be;
-  Jet      inState  [BMLN_dynDim];
-  Jet      outState [BMLN_dynDim];
-  Jet      zero;
-  Jet dummy;
-  static  int     i, j;
-  static  double  matrix [ BMLN_dynDim ][ BMLN_dynDim ];
-  double rho;
-
-
-  for   ( i = 0; i < BMLN_dynDim; i++ ) {
-    for ( j = 0; j < BMLN_dynDim; j++ )
-      matrix[i][j] = 0.0;
-    matrix[i][i] = 1.0;
+  double  matrix [ BMLN_dynDim ][BMLN_dynDim];
+  
+  for   ( int i=0; i < BMLN_dynDim; ++i) {
+    for ( int j=0; j < BMLN_dynDim; ++j){
+      matrix[i][j] = ( i==j) ? 1.0 : 0.0;
+    }
   }
   
-  if(pbe->Strength() != 0.0) {
-    rho = p.BRho().standardPart()/pbe->Strength();
+  double const kx     = h * sqrt(1.0 - n ); 
+  double const ky     = h * sqrt( n ); 
+  
+  
+  // entrance focusing 
+  
+  state[i_npx] +=  tan(elm.getEntryEdgeAngle())/rho *  state[i_x]; 
+  state[i_npy] -=  tan(elm.getEntryEdgeAngle())/rho *  state[i_y]; 
 
-    matrix[0][0] = cos(pbe->getAngle());
-    matrix[0][3] = sin(pbe->getAngle())*rho;
-    matrix[0][5] = rho*(1.0 - cos(pbe->getAngle()))/p.Beta().standardPart();
-    matrix[1][4] = pbe->Length();
-    matrix[2][0] = changeMADConvention*(-sin(pbe->getAngle())/p.Beta().standardPart());
-    matrix[2][3] = changeMADConvention*(-matrix[0][5]);
-    matrix[2][5] = changeMADConvention*(pbe->Length()/p.Beta().standardPart()/
-                                        p.Gamma().standardPart()/
-                                        p.Gamma().standardPart() - 
-                                       (pbe->Length() - sin(pbe->getAngle())*rho)
-                                       /p.Beta().standardPart());
-    matrix[3][0] = -sin(pbe->getAngle())/rho;
-    matrix[3][3] = matrix[0][0];
-    matrix[3][5] = changeMADConvention*(-matrix[2][0]);
-    
-    // Finally .. the mapping ...............................
+  matrix[i_x  ][i_x  ] =           cos(kx*length);
+  matrix[i_x  ][i_npx] =  (1.0/kx)*sin(kx*length);
+  matrix[i_x  ][i_ndp] =  h/(kx*kx)*(1.0 - cos(kx*length));
 
-    if( BMLN_dynDim != 6 ) {
-      throw( GenericException( __FILE__, __LINE__,
-             "int sbend::MAD_Prop::operator()( bmlnElmnt* p_be, JetParticle& p ) {", 
-             "This was written for a phase space dimension of 6." ) );
+  matrix[i_npx][i_x   ] = -kx*sin(kx*length);
+  matrix[i_npx][i_npx ] =  cos(kx*length);
+  matrix[i_npx][i_ndp ] =  h/kx * sin(kx*length); 
+
+  matrix[i_y]  [i_y   ] =  cos(ky*length);
+  matrix[i_y]  [i_npy ] = ( ky == 0.0 ) ? length : (1.0/ky)*sin(ky*length);
+
+  matrix[i_npy][i_y   ] = -ky*sin(ky*length);
+  matrix[i_npy][i_npy ] =  cos(ky*length);
+
+  const double cnv =  - 1.0/beta;  
+
+  //----------------------------------------------------------------------------------
+  // Note: the std optical matrix formalism assumes the arc length s as the independent 
+  //       coordinate. CHEF uses cdt with t > 0 when the particle is *late* w/r to 
+  //       a reference particle. 
+  //       A conversion factor cnv from ds to cdt is introduced to convert from cdt   
+  //       to ds. A subtle difference is that cdt depends on dp/p not only 
+  //       through the change in path length, but also through the change in velocity
+  //       associated with a change in momentum. This latter effect is the origin
+  //       of the extra term   - length/(beta*gamma*gamma);   
+  // ----------------------------------------------------------------------------------
+
+  matrix[i_cdt][i_x]    =  cnv * (h/kx)*sin(kx*length);
+  matrix[i_cdt][i_npx]  =  cnv * (h/kx*kx)*(1.0-cos(kx*length));
+  matrix[i_cdt][i_ndp]  =  cnv * (h*h)/(kx*kx*kx)*(kx*length - sin(kx*length) ) - length/(beta*gamma*gamma); 
+
+  // Finally .. the mapping ...............................
+
+  State_t const& inState  = p.State();
+  State_t        outState( p.State().Dim() ); 
+
+  for(  int i=0; i<BMLN_dynDim; ++i  ) {
+    outState[i] = 0.0;
+    for( int j=0; j<BMLN_dynDim; ++j  ) {
+      outState[i] += matrix[i][j]*inState[j];
     }
-
-    inState[0] = p.get_x();
-    inState[1] = p.get_y();
-    inState[2] = p.get_cdt();
-    inState[3] = p.get_npx();
-    inState[4] = p.get_npy();
-    inState[5] = p.get_ndp();
-
-    zero = 0.0;
-    
-    for( i = 0; i < BMLN_dynDim; i++  ) {
-      outState[i] = zero;
-      for( j = 0; j < BMLN_dynDim; j++  ) 
-	outState[i] = outState[i] + matrix[i][j]*inState[j];
-    }
-
-    p.set_x  ( outState[0] );
-    p.set_y  ( outState[1] );
-    p.set_cdt( outState[2] );
-    p.set_npx( outState[3] );
-    p.set_npy( outState[4] );
-    p.set_ndp( outState[5] );
-
-  } else {
-    p.set_x  ( p.get_x() + pbe->Length() * p.get_npx() );
-    p.set_y  ( p.get_y() + pbe->Length() * p.get_npy() );
-    p.set_cdt( p.get_cdt() - pbe->Length()/p.Beta()/p.Gamma()/p.Gamma() * p.get_ndp() );
   }
+
+  std::copy( outState.begin(), outState.end(), state.begin() ); 
+
+  // exit focusing ......................... 
+
+  state[i_npx] +=  tan( elm.getExitEdgeAngle())/rho *  state[i_x]; 
+  state[i_npy] -=  tan( elm.getExitEdgeAngle())/rho *  state[i_y]; 
 
   return 0;
 }
+
+} // anonymous namespace
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+int sbend::MAD_Prop::operator()( bmlnElmnt* p_be, Particle& p ) 
+{  
+
+  sbend* pbe       = dynamic_cast<sbend*>(p_be);
+  double const rho = p.BRho()/pbe->Strength();
+  return ::propagate( *pbe, p, rho, 0.0);
+}
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+int sbend::MAD_Prop::operator()( bmlnElmnt* p_be, JetParticle& p ) 
+{  
+  sbend* pbe       = dynamic_cast<sbend*>(p_be);
+  double const rho = standardPart(p.BRho())/pbe->Strength();
+  return ::propagate( *pbe,p, rho, 0.0);
+}
+
