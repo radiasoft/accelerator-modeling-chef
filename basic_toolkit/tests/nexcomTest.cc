a6ada45ced3f620e363fd36dcfb144fa2878d35d
michelot <michelot>
2008-02-12 15:33:23 +0000

    First installment on some new tests.  Continuing ...    - lpjm

---
+++ basic_toolkit/tests/nexcomTest.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+extern char nexcom( int, int, int* );
+
+using namespace std;
+
+int main( int argc, char** argv )
+{
+  if( argc != 3 ) {
+    cout << "Usage: " << argv[0]
+         << " <order> <number of variables>"
+         << endl;
+    return -1;
+  }
+  int order = atoi( argv[1] );
+  int n     = atoi( argv[2] );
+
+  int e[n];
+  int w, i;
+  int k = 0;
+  for( w = 1; w <= order; w++ ) {
+    int m = 0;
+    while( nexcom( w, n, e  ) ) {
+      cout << ++k << ": " << ++m << ": ( " << e[0];
+      for( i = 1; i < n; i++ ) {
+        cout << ", " << e[i];
+      }
+      cout << " )" << endl;
+    }  
+    cout << "-----------------------------" << endl;
+  }
+  return 0;
+}
