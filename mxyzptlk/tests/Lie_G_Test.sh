4cdd610bebdd657228d2583b66afcf97f255928b
michelot <michelot>
2008-02-14 15:26:27 +0000

    *** empty log message ***

---
+++ mxyzptlk/tests/Lie_G_Test.sh
@@ -0,0 +1,6 @@
+#!/bin/csh
+   
+Lie_G_Test 5 0.1  >& Lie_G_Test.out
+Lie_G_Test 5 0.2 >>& Lie_G_Test.out
+Lie_G_Test 5 0.5 >>& Lie_G_Test.out
+Lie_G_Test 5 1.0 >>& Lie_G_Test.out
---
+++ mxyzptlk/tests/Lie_I_Test.sh
@@ -0,0 +1,4 @@
+#!/bin/csh
+   
+Lie_I_Test 5 0.1  >& Lie_I_Test.out
+Lie_I_Test 3 0.5 >>& Lie_I_Test.out
