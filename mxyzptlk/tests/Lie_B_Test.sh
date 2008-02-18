9bf94b5cdf5ff37f18a3ba81715080a3a6862f7c
cvs2svn <admin@example.com>
2008-02-18 13:02:31 +0000

    This commit was manufactured by cvs2svn to create tag 'Mar_13_2008'.

--- mxyzptlk/tests/Lie_B_Test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/csh
-   
-Lie_B_Test >& Lie_B_Test.out
--- mxyzptlk/tests/Lie_D_Test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/csh
-   
-Lie_D_Test 5 0.1  >& Lie_D_Test.out
-Lie_D_Test 5 0.2 >>& Lie_D_Test.out
-Lie_D_Test 5 0.5 >>& Lie_D_Test.out
-Lie_D_Test 5 1.0 >>& Lie_D_Test.out
