dcf3239c10ac087f3057a0679bf741ca5d8f3a9c
ostiguy <ostiguy>
2008-02-22 17:03:54 +0000

    o fix for g++ < 4.2 workaround -jfo

--- beamline/src/ThinPolesPropagators.cc
+++ beamline/src/ThinPolesPropagators.cc
@@ -113,8 +113,8 @@ void propagate( int n, Element_t& elm, Particle_t& p )
 
 #if (__GNUC__ == 3) ||  ((__GNUC__ == 4) && (__GNUC_MINOR__ < 2 ))
 
-template void propagate(          ThinPole& elm,    Particle& p );
-template void propagate(          ThinPole& elm, JetParticle& p );
+// *** NOT IMPLEMENTED template void propagate(          ThinPole& elm,    Particle& p );
+// *** NOT IMPLEMENTED template void propagate(          ThinPole& elm, JetParticle& p );
 
 #endif
 
