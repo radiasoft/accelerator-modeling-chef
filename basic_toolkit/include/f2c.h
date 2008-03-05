56acf91ec2ecf74bdf7d6d1371796349b58d1386
amundson <amundson>
2008-03-05 21:05:13 +0000

    f2c.h now correct on both 32-bit and 64-bit architectures.

--- basic_toolkit/include/f2c.h
+++ basic_toolkit/include/f2c.h
@@ -39,14 +39,14 @@
 #ifndef F2C_INCLUDE
 #define F2C_INCLUDE
 
-typedef long int integer;
+typedef int integer;
 typedef char *address;
 typedef short int shortint;
 typedef float real;
 typedef double doublereal;
 typedef struct { real r, i; } complex;
 typedef struct { doublereal r, i; } doublecomplex;
-typedef long int logical;
+typedef int logical;
 typedef short int shortlogical;
 typedef char logical1;
 typedef char integer1;
