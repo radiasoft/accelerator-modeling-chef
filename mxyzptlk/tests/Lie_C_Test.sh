bf600d725c97a1974abcc79e7e501f58194b9824
cvs2svn <admin@example.com>
2008-03-26 19:50:47 +0000

    This commit was manufactured by cvs2svn to create tag 'CREATED_20080326'.

--- mxyzptlk/tests/Lie_C_Test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh
-   
-Lie_C_Test 3 0.1  >& Lie_C_Test.out
-Lie_C_Test 3 0.2 >>& Lie_C_Test.out
-Lie_C_Test 3 0.5 >>& Lie_C_Test.out
-Lie_C_Test 3 1.0 >>& Lie_C_Test.out
-Lie_C_Test 5 1.0 >>& Lie_C_Test.out
-Lie_C_Test 7 1.0 >>& Lie_C_Test.out
