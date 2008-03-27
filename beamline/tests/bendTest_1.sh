630df01508640c11f6317841530ea1b2f73d8e62
michelot <michelot>
2008-03-27 14:49:00 +0000

    Extended to include non-zero dp/p.    - lpjm

--- beamline/tests/bendTest_1.sh
+++ beamline/tests/bendTest_1.sh
@@ -6,4 +6,22 @@ if( 0 != $return_status ) then
   exit $return_status
   endif
 
+bendTest_1 -dpp -0.01
+set return_status = $status
+if( 0 != $return_status ) then
+  exit $return_status
+  endif
+
+bendTest_1 -dpp 0.01
+set return_status = $status
+if( 0 != $return_status ) then
+  exit $return_status
+  endif
+
+bendTest_1 -N 32 -dpp 0.01
+set return_status = $status
+if( 0 != $return_status ) then
+  exit $return_status
+  endif
+
 exit 0
