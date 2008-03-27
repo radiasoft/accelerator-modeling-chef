835aa31f8e7c5a124c4875c6920cf129086a5566
ostiguy <ostiguy>
2008-03-27 16:48:00 +0000

    o improved MAD-style propagators -jfo

--- beamline/src/RBendPropagators.cc
+++ beamline/src/RBendPropagators.cc
@@ -50,6 +50,19 @@ namespace {
   Particle::PhaseSpaceIndex const& i_ndp = Particle::ndpIndex;
 
 
+template <typename T>
+inline double standardPart( T const& );
+
+template<>
+inline double standardPart( Jet const& value)    { return value.standardPart(); }
+
+template<>
+inline double standardPart( double const& value) { return value; }
+ 
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
 template<typename Particle_t>
 void propagate( rbend& elm, Particle_t& p )
 {
@@ -69,73 +82,102 @@ void propagate( rbend& elm, Particle_t& p )
 
 }
 
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-template <typename T>
-double toDouble( T const& value);
-
-template <>
-double toDouble( double const& value)
-{ return value;}
-
-template <>
-double toDouble( Jet const& value)
-{ return value.standardPart();}
-
-#if 0
-template <typename Particle_t> 
-void mad_propagate( rbend& elm, Particle_t& p ) 
+template < typename Element_t, typename Particle_t>
+void mad_propagate( Element_t& elm, Particle_t& p, double const& rho, double const& n )
 {
 
-  typedef typename PropagatorTraits<Particle_t>::State_t       State_t;
-  typedef typename PropagatorTraits<Particle_t>::Component_t   Component_t;
+  int const BMLN_dynDim = 6;
 
-  State_t&     state    = p.State();
+  typedef typename PropagatorTraits<Particle_t>::State_t     State_t;
+  typedef typename PropagatorTraits<Particle_t>::Component_t Component_t;
 
-  Component_t rho   = p.BRho()/ elm.Strength();
-
-  double const length = elm.Length();
-  double const angle  = 2.0 * asin( length /( 2.0* p.ReferenceBrho() )); // ????? 
-
-  Component_t m00 =  cos(angle);
-  Component_t m03 =  sin(angle)*rho;
-  Component_t m05 =  rho*(1.0 - cos(angle))/p.Beta();
-  Component_t m14 =  length;
-  Component_t m20 = -sin(angle)/p.Beta();
-  Component_t m23 = -m05;
-  Component_t m25 =  length/p.Beta()/p.Gamma()/p.Gamma() - ( length - sin(angle)*rho )/p.Beta();
-  Component_t m30 = -sin(angle)/rho;
-  Component_t m33 =  m00;
-  Component_t m35 = -m20;
+  State_t& state = p.State();
   
-  Component_t edge30 =  tan( angle/2.0 )/(rho);
-  Component_t edge41 = -edge30;
- 
- // entrance edge
-   
-  State_t      instate  = state;
+  double const h      = 1/rho;                    // curvature
+  double angle        = 2.0 * asin(elm.Length()/( 2.0*standardPart(rho)));
+  double const length = angle*rho;         
+  double const gamma  = standardPart( p.Gamma() );
+  double const beta   = standardPart( p.Beta()  );
 
-  state[i_npx] = edge30*state[i_x];
-  state[i_npy] = edge41*state[i_y];
 
-  // bend  
+  if(angle == 0.0) {
+    state[i_x  ] +=   length * state[i_npx];
+    state[i_y  ] +=   length * state[i_npy];
+    state[i_cdt] +=  -length /(beta*gamma*gamma) * state[i_ndp];
+    return 0;
+  }  
 
-  instate  = state;
-   
-  state[i_x  ] = m00*instate[i_x  ] + m03*instate[i_npx] +  m05*instate[i_ndp];  
-  state[i_y  ] = m14*instate[i_npy];
-  state[i_cdt] = m20*instate[i_x  ] + m23*instate[i_npx] +  m25*instate[i_ndp];
-  state[i_npx] = m30*instate[i_x  ] + m33*instate[i_npx] +  m35*instate[i_ndp];
+  double  matrix [ BMLN_dynDim ][BMLN_dynDim];
+  
+  for   ( int i=0; i < BMLN_dynDim; ++i) {
+    for ( int j=0; j < BMLN_dynDim; ++j){
+      matrix[i][j] = ( i==j) ? 1.0 : 0.0;
+    }
+  }
+  
+  double const kx     = h * sqrt(1.0 - n ); 
+  double const ky     = h * sqrt( n ); 
+  
+  
+  // entrance focusing 
+  
+  state[i_npx] +=  tan(angle/2.0+elm.getEntryAngle())/rho *  state[i_x]; 
+  state[i_npy] -=  tan(angle/2.0-elm.getEntryAngle())/rho *  state[i_y]; 
 
-  instate  = state;
+  matrix[i_x  ][i_x  ] =           cos(kx*length);
+  matrix[i_x  ][i_npx] =  (1.0/kx)*sin(kx*length);
+  matrix[i_x  ][i_ndp] =  h/(kx*kx)*(1.0 - cos(kx*length));
 
- // exit edge
+  matrix[i_npx][i_x   ] = -kx*sin(kx*length);
+  matrix[i_npx][i_npx ] =  cos(kx*length);
+  matrix[i_npx][i_ndp ] =  h/kx * sin(kx*length); 
 
-  state[i_npx] = edge30*state[i_x];
-  state[i_npy] = edge41*state[i_y];
+  matrix[i_y]  [i_y   ] =  cos(ky*length);
+  matrix[i_y]  [i_npy ] = ( ky == 0.0 ) ? length : (1.0/ky)*sin(ky*length);
+
+  matrix[i_npy][i_y   ] = -ky*sin(ky*length);
+  matrix[i_npy][i_npy ] =  cos(ky*length);
+
+  const double cnv =  - 1.0/beta;  
+
+  //----------------------------------------------------------------------------------
+  // Note: the std optical matrix formalism assumes the arc length s as the independent 
+  //       coordinate. CHEF uses cdt with t > 0 when the particle is *late* w/r to 
+  //       a reference particle. 
+  //       A conversion factor cnv from ds to cdt is introduced to convert from cdt   
+  //       to ds. A subtle difference is that cdt depends on dp/p not only 
+  //       through the change in path length, but also through the change in velocity
+  //       associated with a change in momentum. This latter effect is the origin
+  //       of the extra term   - length/(beta*gamma*gamma);   
+  // ----------------------------------------------------------------------------------
+
+  matrix[i_cdt][i_x]    =  cnv * (h/kx)*sin(kx*length);
+  matrix[i_cdt][i_npx]  =  cnv * (h/kx*kx)*(1.0-cos(kx*length));
+  matrix[i_cdt][i_ndp]  =  cnv * (h*h)/(kx*kx*kx)*(kx*length - sin(kx*length) ) - length/(beta*gamma*gamma); 
+
+  // Finally .. the mapping ...............................
+
+  State_t const& inState  = p.State();
+  State_t        outState( p.State().Dim() ); 
+
+  for(  int i=0; i<BMLN_dynDim; ++i  ) {
+    outState[i] = 0.0;
+    for( int j=0; j<BMLN_dynDim; ++j  ) {
+      outState[i] += matrix[i][j]*inState[j];
+    }
+  }
+
+  std::copy( outState.begin(), outState.end(), state.begin() ); 
+
+  // exit focusing ......................... 
+
+  state[i_npx] +=  tan( angle/2.0+elm.getExitAngle())/rho *  state[i_x]; 
+  state[i_npy] -=  tan(-angle/2.0+elm.getExitAngle())/rho *  state[i_y]; 
 
 }
-#endif
-
 
 //----------------------------------------------------------------------------------
 // Workaround for gcc < 4.2 mishandling of templates defined in anonymous namespace
@@ -145,6 +187,12 @@ void mad_propagate( rbend& elm, Particle_t& p )
 
 template void propagate(     rbend& elm,    Particle& p );
 template void propagate(     rbend& elm, JetParticle& p );
+template void mad_propagate( rbend& elm,    Particle& p, double const& rho, double const& n );
+template void mad_propagate( rbend& elm, JetParticle& p, double const& rho, double const& n );
+template void mad_propagate( rbend& elm,  Particle_t& p )
+template void mad_propagate( rbend& elm,  Particle_t& p )
+template double standardPart( double const& );
+template double standardPart( Jet    const& );
 
 #endif
 
@@ -156,7 +204,7 @@ template void propagate(     rbend& elm, JetParticle& p );
 void rbend::Propagator::setup( rbend& arg) 
 {
   
-  BmlPtr bml = bmlnElmnt::core_access::get_BmlPtr( arg); 
+  BmlPtr& bml = bmlnElmnt::core_access::get_BmlPtr( arg); 
   bml = BmlPtr(new beamline("RBEND_PRIVATE") );
  
   double& usFaceAngle_ = rbend::rbend_core_access::get_usFaceAngle(arg); 
@@ -194,3 +242,33 @@ void rbend::Propagator::operator()( rbend& elm, JetParticle& p )
 {
   ::propagate(elm,p);
 }
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+#if 0
+void rbend::MADPropagator::operator()( rbend& elm, Particle& p ) 
+{
+
+  double const rho    = standardPart( p.BRho() )/elm.Strength();
+
+  // field index = -(dB/B)/ (dx/rho) 
+
+  double const n      = - ( 1.0/ elm.Length() ) * elm.getQuadrupole() / elm.Strength() * rho;                       
+  ::propagate(elm, p, n);
+
+}
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void rbend::MADPropagator::operator()( rbend& elm, JetParticle& p ) 
+{
+  double const rho    = standardPart( p.BRho() )/elm.Strength();
+
+  // field index = -(dB/B)/ (dx/rho) 
+
+  double const n      = - ( 1.0/ elm.Length() ) * elm.getQuadrupole() / elm.Strength() * rho;                       
+  ::propagate(elm, p, n);
+}
+#endif
