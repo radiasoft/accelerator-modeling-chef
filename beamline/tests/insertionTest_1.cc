a5ae5d42ae39320418fa0dcad6cbc2069e3ffa59
michelot <michelot>
2008-04-07 15:16:55 +0000

    Continuing development ...    - lpjm

--- beamline/tests/insertionTest_1.cc
+++ beamline/tests/insertionTest_1.cc
@@ -29,6 +29,7 @@
 ////////////////////////////////////////////////////////////
 
 #include <beamline/drift.h>
+#include <beamline/quadrupole.h>
 
 using namespace std;
 
@@ -37,6 +38,8 @@ struct Options
   bool   valid;
   double pct;
   double driftLength;
+  double quadLength;
+  double quadStrength;
 
   Options( int, char**, int=0 );
 };
@@ -46,6 +49,8 @@ Options::Options( int argc, char** argv, int lastargs )
 :   valid(true)
   , pct(0.5)
   , driftLength(1)
+  , quadLength(1)
+  , quadStrength(5)
 {
   valid = true;
   int limit = argc-lastargs;
@@ -83,6 +88,33 @@ Options::Options( int argc, char** argv, int lastargs )
 
 /////////////////////////////////////////////////////////////////////
 
+int testLengths(   ElmPtr const& a
+                 , ElmPtr const& b
+                 , ElmPtr const& c
+                 , double pct )
+{
+  int ret = 0;
+  if( 1.0e-14 < std::abs(a->Length() - b->Length() - c->Length()) ) {
+    cout <<   "*** FAILED *** "
+            "\n*** FAILED *** : " << __FILE__ << ", line " << __LINE__
+         << "\n*** FAILED *** : For " << a->Type() << "  " << a->Name()
+         << "\n*** FAILED *** : " 
+         << a->Length() << " != " << b->Length() << " + " << c->Length()
+         << "\n*** FAILED *** "
+         << endl;
+    ret = 1;
+  }
+  if( 1.0e-14 < std::abs(b->Length() - pct*a->Length()) ) {
+    ret = 2;
+  }
+  if( 1.0e-14 < std::abs(c->Length() - (1.0-pct)*a->Length()) ) {
+    ret = 3;
+  }
+  return ret;
+}
+
+/////////////////////////////////////////////////////////////////////
+
 int main( int argc, char** argv ) 
 {
   int ret = 0;
@@ -99,11 +131,23 @@ int main( int argc, char** argv )
     return 1;
   }
 
-  drift ooo( "", myOptions.driftLength );
-  ElmPtr oo1, oo2;
-  ooo.Split( myOptions.pct, oo1, oo2 );
-  cout << oo1->Type() << "  " << oo1->Length() << endl;
-  cout << oo2->Type() << "  " << oo2->Length() << endl;
+  
+  ElmPtr oo0, oo1, oo2;
+
+  
+  { // drift
+  oo0 = ElmPtr( new drift( "", myOptions.driftLength ) );
+  oo0->Split( myOptions.pct, oo1, oo2 );
+  ret = testLengths( oo0, oo1, oo2, myOptions.pct );
+  }
+
+  { // quadrupole
+  oo0 = ElmPtr( new quadrupole(   ""
+                                , myOptions.quadLength
+                                , myOptions.quadStrength ) );
+  oo0->Split( myOptions.pct, oo1, oo2 );
+  ret = testLengths( oo0, oo1, oo2, myOptions.pct );
+  }
 
   return ret;
 }
