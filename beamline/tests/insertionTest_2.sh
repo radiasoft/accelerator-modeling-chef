eb67fb8c94b637747da0b4b8f12d28d45e0fd249
michelot <michelot>
2008-05-19 15:19:30 +0000

    *** empty log message ***

--- beamline/tests/insertionTest_2.sh
+++ beamline/tests/insertionTest_2.sh
@@ -6,4 +6,10 @@ if( 0 != $return_status ) then
   exit $return_status
   endif
 
+./insertionTest_2 -l
+set return_status = $status
+if( 0 != $return_status ) then
+  exit $return_status
+  endif
+
 exit 0
