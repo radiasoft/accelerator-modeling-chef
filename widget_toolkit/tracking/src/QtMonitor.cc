5a8c0b8c9809f9411452b3229e5ee452d80e3bff
ostiguy <ostiguy>
2008-09-12 17:56:13 +0000

    o modified to work without virtual bmnlElmnt::localPropagate() method -jfo

--- widget_toolkit/tracking/src/QtMonitor.cc
+++ widget_toolkit/tracking/src/QtMonitor.cc
@@ -6,8 +6,6 @@
 ******             interfaces to exercise the functionality        
 ******             of BEAMLINE.                                    
 ******                                                                
-******  Version:   3.0                    
-******                                    
 ******  File:      QtMonitor.cc
 ******                                                                
 ******  Copyright (c) 2004  Universities Research Association, Inc.   
@@ -18,8 +16,8 @@
 ******  royalty-free license to publish or reproduce documentation 
 ******  and software for U.S. Government purposes. This software 
 ******  is protected under the U.S.and Foreign Copyright Laws. 
-                                                                
-                                                                
+******                                                                
+******                                                                
 ******  Author:    Leo Michelotti                                     
 ******                                                                
 ******             Fermilab                                           
@@ -34,7 +32,9 @@
 ****** Mar 2007 ostiguy@fnal.gov
 ****** - eliminated references to slist/dlist
 ****** - use new style STL-compatible beamline iterators 
-******                                                                
+****** Sep 2008 ostiguy@fnal.gov
+****** - modified to work without virtual localPropagate method. 
+******                                                               
 **************************************************************************
 *************************************************************************/
 
@@ -51,27 +51,102 @@
 #include <typeinfo>
 
 #include "QtMonitor.h"
-#include "bmlnElmnt.h"
-#include "Particle.h"
+#include <beamline/bmlnElmnt.h>
+#include <beamline/Particle.h>
+#include <beamline/JetParticle.h>
 #include <beamline/beamline.h>
+#include <beamline/BasePropagator.h>
+
+class QtMonitor::Propagator: public BasePropagator {
+
+ public:
+ 
+  Propagator* Clone() const;
+
+  void  setup( bmlnElmnt& );
+  void  operator()( bmlnElmnt const& elm,    Particle& p);
+  void  operator()( bmlnElmnt const& elm, JetParticle& p);
+};
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+QtMonitor::Propagator* QtMonitor::Propagator::Clone() const 
+{  
+   return new Propagator(*this);
+}
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void QtMonitor::Propagator::setup(  bmlnElmnt& arg ) 
+{  
+  ElmPtr& elm = bmlnElmnt::core_access::get_ElmPtr(arg);
+  
+  elm = ElmPtr( new monitor( arg.Name(), arg.Length() ));
+
+}
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void QtMonitor::Propagator::operator()( bmlnElmnt const& elm, Particle& p ) 
+{
+   ConstElmPtr mon = bmlnElmnt::core_access::get_ElmPtr(elm);
+   mon->propagate(p);
+
+   QtMonitor& qtmon = const_cast<QtMonitor&>( static_cast< QtMonitor const&>(elm));
+   qtmon.emit ping( qtmon.getAzimuth(), p.State() );
+}
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void QtMonitor::Propagator::operator()( bmlnElmnt const& elm, JetParticle& p ) 
+{
+   ConstElmPtr mon = bmlnElmnt::core_access::get_ElmPtr(elm);
+   mon->propagate(p);
+}
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 // -------------------------------
 // Implementation: class QtMonitor
 // -------------------------------
 
-QtMonitor::QtMonitor( const char* identifier )
-: monitor( (char*) identifier ), azimuth_(0.0)
+QtMonitor::QtMonitor( char const * identifier )
+: monitor( identifier ), azimuth_(0.0)
 {
+  propagator_ = PropagatorPtr(new Propagator() ); 
+  propagator_->setup(*this);
 }
 
-QtMonitor::QtMonitor( const QtMonitor& x )
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+QtMonitor::QtMonitor( QtMonitor const& x )
 : QObject(this), monitor( x ), azimuth_( x.azimuth_ )
-{
-}
+{}
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
 
 QtMonitor::~QtMonitor()
+{}
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+char const* QtMonitor::Type() const
 {
+  return "QtMonitor";
 }
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
 
 int QtMonitor::setAzimuth( beamline  const& x )
 {
@@ -92,9 +167,20 @@ int QtMonitor::setAzimuth( beamline  const& x )
   return n;
 }
 
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-void QtMonitor::localPropagate( Particle& p )
+
+double const&  QtMonitor::getAzimuth() const
 {
-  this->monitor::localPropagate( p );
-  emit ping( azimuth_, p.State() );
+  return azimuth_;
+}  
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+QtMonitor* QtMonitor::Clone() const 
+{
+ return new QtMonitor( *this ); 
 }
