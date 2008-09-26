b20e0362d881cb6eef4cea1b86b8269f82a803f3
ostiguy <ostiguy>
2008-09-26 21:05:17 +0000

    o allow "-" in file names -jfo

--- updateheaders.sh
+++ updateheaders.sh
@@ -12,9 +12,9 @@ if [ -f header_files ]; then  mv header_files  header_files.old; else touch head
 headername=`basename \`pwd\``
 echo  includedir = \$\{prefix\}/include/$headername > header_files.new
 echo "include_HEADERS = " | tr -d "\n"  >> header_files.new 
-ls include/ | grep '.*\.h$'    | sed -e '/[a-zA-Z\.0-9_]*/i\ include/' | tr -d "\n" >> header_files.new  
-ls include/ | grep '.*\.hpp$'  | sed -e '/[a-zA-Z\.0-9_]*/i\ include/' | tr -d "\n" >> header_files.new   
-ls include/ | grep '.*\.tcc$'  | sed -e '/[a-zA-Z\.0-9_]*/i\ include/' | tr -d "\n" >> header_files.new   
+ls include/ | grep '.*\.h$'    | sed -e '/[a-zA-Z\.0-9_\-]*/i\ include/' | tr -d "\n" >> header_files.new  
+ls include/ | grep '.*\.hpp$'  | sed -e '/[a-zA-Z\.0-9_\-]*/i\ include/' | tr -d "\n" >> header_files.new   
+ls include/ | grep '.*\.tcc$'  | sed -e '/[a-zA-Z\.0-9_\-]*/i\ include/' | tr -d "\n" >> header_files.new   
 echo >>  header_files.new
 diff header_files.old header_files.new | sed "s/</Removed/g" | sed "s/>/Added/g"
 if [ -f header_files.new ]; then mv header_files.new header_files; fi
