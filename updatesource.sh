ca91e922c4a3268c5f946dbcb5f602ab0386a7da
ostiguy <ostiguy>
2008-09-29 14:48:33 +0000

    o wrong ordering .... yacc before lex ! -jfo

--- updatesource.sh
+++ updatesource.sh
@@ -5,7 +5,8 @@
 #
 # Author: ostiguy@fnal.gov
 #
-# Updates a list of source files (to be included in Makefile.am)  
+# Scans the contents of the (src) directory an generated a list of source 
+# files in a format suitable for inclusion in Makefile.am  
 #-----------------------------------------------------------------------
 
 more source_files | tr " " "\n" > test.source_files.old
@@ -22,9 +23,9 @@ export libname
 
 cd $current_dir
 
-#----------------------------------------------------------
-# note: yacc sources must be listed *before* lex sources !
-#----------------------------------------------------------
+#-----------------------------------------------------------------
+# note: yacc sources must always be listed *before* lex sources !
+#-----------------------------------------------------------------
 
 echo lib$libname"_la_SOURCES= " > source_files.new
 ls | grep '.*\.cpp$' >> source_files.new  
@@ -32,8 +33,8 @@ ls | grep '.*\.cxx$' >> source_files.new
 ls | grep '.*\.cc$'  >> source_files.new  
 ls | grep '.*\.c$'   >> source_files.new  
 ls | grep '.*\.C$'   >> source_files.new  
-ls | grep '.*\.l$'   >> source_files.new  
 ls | grep '.*\.y$'   >> source_files.new  
+ls | grep '.*\.l$'   >> source_files.new  
 
 cat source_files.new | tr "\n" " " > test.source_files.new
 echo >> test.source_files.new
