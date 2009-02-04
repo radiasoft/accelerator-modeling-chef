d1350e3797c1b902a6fc321646d2bf8bf999198b
ostiguy <ostiguy>
2009-02-04 19:07:06 +0000

    o improved const-correctness. -jfo

--- beamline/src/MonitorPropagators.cc
+++ beamline/src/MonitorPropagators.cc
@@ -75,40 +75,40 @@ template<>
 double toDouble(Jet    const& value) { return value.standardPart(); }
 
 template<typename Elm_t, typename Particle_t>
-double setMonitorState( Elm_t elm,  Particle_t& p );
+double setMonitorState( Elm_t elm,  Particle_t const& p );
 
 template<typename Particle_t>
-double setMonitorState( monitor const& elm,  Particle_t& p )
+double setMonitorState( monitor const& elm,  Particle_t const& p )
 {
   typedef typename PropagatorTraits<Particle_t>::State_t       State_t;
   typedef typename PropagatorTraits<Particle_t>::Component_t   Component_t;
  
-  State_t& state = p.State();
+  State_t const& state = p.State();
 
   elm.setHposition( toDouble(state[i_x]) );
-  elm.setNpx( toDouble(state[i_npx]) );
+        elm.setNpx( toDouble(state[i_npx]) );
+  elm.setVposition( toDouble(state[i_y]) );
+        elm.setNpy( toDouble(state[i_npy]) );
+}
+
+template<typename Particle_t>
+double setMonitorState( vmonitor const& elm,  Particle_t const& p )
+{
+  typedef typename PropagatorTraits<Particle_t>::State_t       State_t;
+  typedef typename PropagatorTraits<Particle_t>::Component_t   Component_t;
+ 
+  State_t const& state = p.State();
   elm.setVposition( toDouble(state[i_y]) );
   elm.setNpy( toDouble(state[i_npy]) );
 }
 
 template<typename Particle_t>
-double setMonitorState( vmonitor const& elm,  Particle_t& p )
+double setMonitorState( hmonitor const& elm,  Particle_t const& p )
 {
   typedef typename PropagatorTraits<Particle_t>::State_t       State_t;
   typedef typename PropagatorTraits<Particle_t>::Component_t   Component_t;
  
-  State_t& state = p.State();
-  elm.setVposition( toDouble(state[i_y]) );
-  elm.setNpy( toDouble(state[i_npy]) );
-}
-
-template<typename Particle_t>
-double setMonitorState( hmonitor const& elm,  Particle_t& p )
-{
-  typedef typename PropagatorTraits<Particle_t>::State_t       State_t;
-  typedef typename PropagatorTraits<Particle_t>::Component_t   Component_t;
- 
-  State_t& state = p.State();
+  State_t const& state = p.State();
   elm.setHposition( toDouble(state[i_x]) );
   elm.setNpx( toDouble(state[i_npx]) );
 }
@@ -123,9 +123,9 @@ void propagate( Element_t const& elm, Particle_t& p )
   State_t& state = p.State();
 
   if( elm.Length() > 0.0 ) {
-     ::driftpropagate( elm. Length()*elm.getDriftFraction(), elm, p ); 
+     ::driftpropagate( elm.Length() * elm.getDriftFraction(),       elm, p ); 
     ::setMonitorState( elm, p);    
-     ::driftpropagate( elm. Length()*(1.0-elm.getDriftFraction()), elm, p ); 
+     ::driftpropagate( elm.Length() * (1.0-elm.getDriftFraction()), elm, p ); 
   } 
   else {
    ::setMonitorState(elm,p);    
@@ -138,6 +138,7 @@ void propagate( Element_t const& elm, Particle_t& p )
 template<typename Element_t>
 void propagate( Element_t const& elm, ParticleBunch& b ) 
 { 
+
   // note: specialized versions for H and V monitors would be a tad more efficient
 
   double xpos = 0.0;
@@ -200,12 +201,12 @@ template void propagate( hmonitor const& elm, JetParticleBunch& b );
 template void propagate( vmonitor const& elm,    ParticleBunch& b );
 template void propagate( vmonitor const& elm, JetParticleBunch& b );
 
-template double setMonitorState( monitor& elm,  Particle& p );
-template double setMonitorState( monitor& elm,  JetParticle& p );
-template double setMonitorState( hmonitor& elm,  Particle& p );
-template double setMonitorState( hmonitor& elm,  JetParticle& p );
-template double setMonitorState( vmonitor& elm,  Particle& p );
-template double setMonitorState( vmonitor& elm,  JetParticle& p );
+template double setMonitorState( monitor& elm,  Particle     const& p );
+template double setMonitorState( monitor& elm,  JetParticle  const& p );
+template double setMonitorState( hmonitor& elm,  Particle    const& p );
+template double setMonitorState( hmonitor& elm,  JetParticle const& p );
+template double setMonitorState( vmonitor& elm,  Particle    const& p );
+template double setMonitorState( vmonitor& elm,  JetParticle const& p );
 
 #endif
 
