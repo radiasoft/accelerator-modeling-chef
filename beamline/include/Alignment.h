b35529c019183ec5b9b78a861c98361ec95d1f1d
ostiguy <ostiguy>
2009-02-04 18:45:18 +0000

    o eliminated  class alignmentData o added support for [small] pitch angle -jfo

--- beamline/include/Alignment.h
+++ beamline/include/Alignment.h
@@ -12,6 +12,9 @@
 ******  Copyright (c) Universities Research Association, Inc. / Fermilab     
 ******                All Rights Reserved                             
 ******
+******  Author: ostiguy@fnal.gov
+******          michelotti@fnal.gov
+******
 ******  Usage, modification, and redistribution are subject to terms          
 ******  of the License supplied with this software.
 ******  
@@ -22,8 +25,9 @@
 ******  and software for U.S. Government purposes. This software 
 ******  is protected under the U.S. and Foreign Copyright Laws. 
 ******                                                                 
-******                                                                
-******                                                           
+******  REVISION HISTORY                                                              
+******                     
+******                                        
 **************************************************************************
 *************************************************************************/
 #ifndef ALIGNMENT_H
@@ -31,70 +35,50 @@
 
 #include <basic_toolkit/globaldefs.h>
 #include <basic_toolkit/VectorD.h>
-#include <beamline/ParticleFwd.h>
+#include <mxyzptlk/JetFwd.h>
+#include <mxyzptlk/MappingFwd.h>
+#include <mxyzptlk/JetVectorFwd.h>
 
 
-template <typename T>
-class  TJet;
-
-template <typename T>
-class  TJetVector;
-
-template <typename T>
-class  TMapping;
-
-typedef TJet<double>       Jet;
-typedef TJetVector<double> JetVector; 
-typedef TMapping<double>   Mapping; 
-
-
-struct DLLEXPORT alignmentData {
-  double       xOffset; // x offset in m
-  double       yOffset; // y offset in m
-  double       roll;    // roll angle in radians
-
-  alignmentData();
-  alignmentData(double const&, double const&, double const& );
-  alignmentData( alignmentData const&);
-  ~alignmentData();
-
-  alignmentData&  operator=( alignmentData const&);
-  bool            operator==( alignmentData const&) const;
-};
-
-//---------------------------------------------------------------------------------
-
-class DLLEXPORT alignment {
+class DLLEXPORT Alignment {
 
 public:
 
-  alignment();
-  alignment( double const& xOffset,
+  Alignment();
+  Alignment( double const& xOffset,
              double const& yOffset,
-             double const& roll ); // roll = roll
-  alignment(alignment     const&);
-  alignment(alignmentData const&);
- ~alignment();
+             double const& roll,
+             double const& pitch ); 
+
+  Alignment(Alignment  const&);
+ ~Alignment();
 
   bool isNull() const;
-  bool operator==( alignment const& ) const;
+  bool operator==( Alignment const& ) const;
 
-  alignment& operator=(alignment const&);
+  Alignment& operator=(Alignment const&);
 
-  void                 setAlignment(alignmentData const&);
-  alignmentData const& getAlignment() const;
+  double const& xOffset()  const  { return xOffset_; }
+  double const& yOffset()  const  { return yOffset_; }
+  double const& roll()     const  { return roll_;    }
+  double const& pitch()    const  { return pitch_;   }
 
-  double const& x_offset() { return adata_.xOffset; }
-  double const& y_offset() { return adata_.yOffset; }
-  double const& roll()     { return adata_.roll;    }
-  double const& cos_roll() { return cosRoll_; }
-  double const& sin_roll() { return sinRoll_; }
+  double setXOffset(double const& val) {  double old = xOffset_; xOffset_ = val; return old; }  
+  double setYOffset(double const& val) {  double old = yOffset_; yOffset_ = val; return old; }   
+  double    setRoll(double const& val) {  double old = roll_;      roll_  = val;  
+                                          cosRoll_= cos(roll_); sinRoll_= sin(roll_); return old;}
+ 
+  double const& cos_roll() const { return cosRoll_; }
+  double const& sin_roll() const { return sinRoll_; } 
 
-  friend std::ostream& operator<<(std::ostream&, alignment&);
+  friend std::ostream& operator<<(std::ostream&, Alignment&);
 
 private:
 
-  alignmentData adata_;    
+  double        xOffset_;  // x offset in m
+  double        yOffset_;  // y offset in m
+  double           roll_;  // roll  angle in radians, [ arbitary                    ]
+  double          pitch_;  // pitch angle in radians  [ assumed small i.e. << 1 rad ]
 
   double        cosRoll_;         
   double        sinRoll_;         
