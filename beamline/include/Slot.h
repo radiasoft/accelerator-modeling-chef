c492047d964d4a091d85e584e2e971abd9066956
michelot <michelot>
2010-01-26 18:21:54 +0000

    Modified void Slot::split(...) to forbid splitting "small" Slots, smallness being specified by new public static variable.  Splitting previously worked correctly but led to confusion in certain applications (viz. Synergia) which created Mappings between inserted markers that did not map 0->0.  This change was made in conjunction with introduction of class DriftConverter in directory physics_toolkit.                                                           - lpjm

--- beamline/include/Slot.h
+++ beamline/include/Slot.h
@@ -1,50 +1,65 @@
 /*************************************************************************
 **************************************************************************
 **************************************************************************
-******                                                                
+******
 ******  BEAMLINE:  C++ objects for design and analysis
-******             of beamlines, storage rings, and   
-******             synchrotrons.                      
-******                                    
+******             of beamlines, storage rings, and
+******             synchrotrons.
+******
 ******  File:      Slot.h
-******                                                                
-******  Copyright (c) 1991 Universities Research Association, Inc.    
-******                All Rights Reserved                             
-******                                                                
-******  Usage, modification, and redistribution are subject to terms          
+******
+******             Description for class Slot
+******             - representing a region of free space connecting
+******               two completely general faces (Frames).
+******             - generalization of "drift," which connects parallel
+******               faces whose origins are incident on a normally
+******               intersecting line.
+******
+******  Author:    Leo Michelotti
+******             Phone: (630) 840 4956
+******             Email: michelotti@fnal.gov
+******
+******  Copyright (c) 1998 Universities Research Association, Inc.
+******                All Rights Reserved
+******
+******  Usage, modification, and redistribution are subject to terms
 ******  of the License supplied with this software.
-******  
-******  Software and documentation created under 
-******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
-******  The U.S. Government retains a world-wide non-exclusive, 
-******  royalty-free license to publish or reproduce documentation 
-******  and software for U.S. Government purposes. This software 
-******  is protected under the U.S. and Foreign Copyright Laws. 
-****** 
-******  Author:    Leo Michelotti                                     
-******                                                                
-******             Fermilab                                           
-******             P.O.Box 500                                        
-******             Mail Stop 220                                      
-******             Batavia, IL   60510                                
-******                                                                
-******             Phone: (630) 840 4956                              
-******             Email: michelotti@fnal.gov                         
-******                                                                
-******                                                                
-******    REVISION HISTORY
+******
+******  Software and documentation created under
+******  U.S. Department of Energy Contract No. DE-AC02-76CH03000.
+******  The U.S. Government retains a world-wide non-exclusive,
+******  royalty-free license to publish or reproduce documentation
+******  and software for U.S. Government purposes. This software
+******  is protected under the U.S. and Foreign Copyright Laws.
+******
+******
+****** ----------------
+****** REVISION HISTORY
+****** ----------------
+****** Jun 1998 (?)        michelotti@fnal.gov
+****** - original (working) version
 ******
 ****** Mar 2007            ostiguy@fnal.gov
 ****** - use covariant return types
 ****** - support for reference counted elements
+******
 ****** Dec 2007            ostiguy@fnal.gov
 ****** - new typesafe propagators
+******
 ****** Apr 2008            michelotti@fnal.gov
 ****** - added placeholder Slot::setLength method
+******
 ****** May 2008 ostiguy@fnal
 ****** - proper, explicit assignment operator
 ****** - propagator moved (back) to base class
 ****** - no assumption about internal structure
+******
+****** Jan 2010            michelotti@fnal.gov
+****** - added public static variables  Slot::minSplitLength
+******   and  (const) Slot::minSplitLengthDefault  to specify
+******   the size of a "small" Slot.
+****** - done in conjunction with changes to Slot::Split(...).
+******   See Slot.cc for additional information.
 ******
 **************************************************************************
 *************************************************************************/
@@ -63,17 +78,16 @@ class Slot;
 
 typedef boost::shared_ptr<Slot> SlotPtr;
  
-class DLLEXPORT Slot : public BmlnElmnt {
+class DLLEXPORT Slot : public BmlnElmnt
+{
 
   class Propagator;
 
  public:
 
    Slot();
-   Slot( std::string const&  name);
-
-   Slot(  std::string const& name, Frame const& out );
-
+   Slot( std::string const& name);
+   Slot( std::string const& name, Frame const& out );
    Slot( Slot const& );
 
    ~Slot();
@@ -111,6 +125,12 @@ class DLLEXPORT Slot : public BmlnElmnt {
      { return out_; }
 
    double OrbitLength( Particle const& );
+
+
+ public:
+   static double       minSplitLength;
+   static double const minSplitLengthDefault;
+
 
  private:
 
