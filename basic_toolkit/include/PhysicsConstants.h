02a628598de8916eb1bc7a46b87bab81d8909e39
Eric G. Stern <egstern@fnal.gov>
2013-08-14 15:04:29 -0500

    preprocessor directory PDG selects which set of constants to use.  Default to PDG 2010 values.

--- basic_toolkit/include/PhysicsConstants.h
+++ basic_toolkit/include/PhysicsConstants.h
@@ -47,29 +47,82 @@
  *  April 10, 1995
  */
 
+/*
+ * I need M_PI from MathConstants.h
+ */
+
+#include "MathConstants.h"
+
 #ifndef PHYSC_H
 #define PHYSC_H
+
+// use 2010 values for now
+#define PDG 2010
 
 #define PH_NORM_h          4.13566733e-24     /* Planck's constant [ GeV-sec ]       */
 #define PH_NORM_hbar       6.58211899e-25     /* Planck's constant [ twopi GeV-sec ] */
 
+#if PDG == 2012
+#define PH_NORM_me         0.510998928e-3      /* Mass of electron  [GeV/c^2]         */
+#define PH_NORM_mmu        0.1056583715        /* mass of muon      [GeV/c^2]         */
+#define PH_MKS_e           1.602176565e-19    /* Proton charge     [Coulombs]        */
+#define PH_NORM_mp         0.938272046         /* Mass of proton    [GeV/c^2]        */
+
+#else
+#if PDG == 2010 // use PDG 2010 values
+
+#define PH_NORM_mp         0.938272013         /* Mass of proton    [GeV/c^2]        */
+#define PH_NORM_me         0.510998910e-3     /* Mass of electron  [GeV/c^2]         */
+#define PH_NORM_mmu        0.105658367        /* mass of muon      [GeV/c^2]         */
+#define PH_MKS_e           1.602176487e-19    /* Proton charge     [Coulombs]        */
+#else
+#if PDG == 2008 // use PDG 2008 value
+
+#define PH_NORM_mp         0.938272013         /* Mass of proton    [GeV/c^2]        */
+#define PH_NORM_me         0.510998910e-3     /* Mass of electron  [GeV/c^2]         */
+#define PH_NORM_mmu        0.105658367        /* mass of muon      [GeV/c^2]         */
+#define PH_MKS_e           1.602176487e-19    /* Proton charge     [Coulombs]        */
+
+#else
+#if PDG == -1 // legacy
+
 #define PH_NORM_mp         0.93827203         /* Mass of proton    [GeV/c^2]         */
 #define PH_NORM_me         0.51099892e-3      /* Mass of electron  [GeV/c^2]         */
 #define PH_NORM_mmu        0.105658369        /* mass of muon      [GeV/c^2]         */
-
 #define PH_MKS_e           1.602176487e-19    /* Proton charge     [Coulombs]        */
-#define PH_MKS_c           2.99792458e8       /* Speed of light    [m/sec]           */
-#define PH_MKS_h           6.62606896e-34     /* Planck's constant [Joule-sec]       */
 
-#define PH_MKS_omega_p     9.578833753e7      /* Proton cyclotron frequence          */
-                                              /* = e / m_p [ rad/sec/Tesla ]         */
-#define PH_MKS_e_div_h     2.417989454e14     /* e/h [Amps/Joule]                    */
-#define PH_MKS_eps0        8.854187817e-12    /* Epsilon nought:                     */
-                                              /* permittivity of free space          */
+#else // legacy
+#error "No selection for physicsl_constants PDG set"
+
+#endif // legacy
+#endif // 2008
+#endif // 2010
+#endif // 2012
+
+#define PH_MKS_c           2.99792458e8       /* Speed of light    [m/sec] (exact)    */
+#define PH_MKS_h           6.62606957e-34     /* Planck's constant [Joule-sec]       */
+
+#define PH_MKS_omega_p     ((PH_MKS_c*PH_MKS_c)/(PH_MKS_mp*1.0e9))      /* Proton cyclotron frequence          */
+                                                      /* = e / m_p [ rad/sec/Tesla ]         */
+#define PH_MKS_e_div_h    (PH_MKS_e/PH_MKS_h)     /* e/h [Amps/Joule]                    */
+#define PH_MKS_mu0       (4.0*M_PI*1.0e-7) /* permittivity of free space */ 
+#define PH_MKS_eps0      (1.0/(PH_MKS_c*PH_MKS_C*PH_MKS_mu0))      /* Epsilon nought */
+                                              /* permeability of free space          */
 					      /* Units:[F/m] or  [Amp-sec/V-m]       */
-#define PH_MKS_re          2.8179402894e-15   /* Classical radius                    */
+/*
+ * classical radius of particle with mass m
+ *  e^2/(4 pi epsilon_0 m c**2)
+ * if m is in GeV/c**2 becomes
+ * e / (4 pi epsilon_0 m 1.0e9)
+ * but
+ * 1/(4 pi epsilon0) is 1.0e-7 * c**2
+ * so this is
+ * e * 1.0e-16 * c**2 / m
+ */
+
+#define PH_MKS_re    (1.0e-16 * PH_MKS_e*PH_MKS_c*PH_MKS_c/PH_NORM_me)  /* Classical radius                    */
                                               /* of electron       [m]               */
-#define PH_MKS_rp          1.5346982522e-18   /* Classical radius                    */
+#define PH_MKS_rp    (1.0e-16 * PH_MKS_e*PH_MKS_c*PH_MKS_c/PH_NORM_mp)   /* Classical radius                    */
                                               /* of proton         [m]               */
 #define PH_MKS_cwe         2.4263102175e-12   /* Compton wavelength                  */
                                               /* of electron       [m]               */
@@ -85,9 +138,9 @@
 /*                                                       */
 /*    Q [ y units ] = PH_CNV_x_to_y * Q [ x units ]      */
 
-#define PH_CNV_eV_to_cgs   1.602176487e-12 /* Energy [erg] = -- * Energy [eV]          */
-#define PH_CNV_eV_to_mks   1.602176487e-19 /* Energy [N-m] = -- * Energy [eV]          */
-#define PH_CNV_brho_to_p   0.299792458     /* p [Gev/c] = -- * B*rho [ Tesla meters ]  */
+#define PH_CNV_eV_to_cgs   (PH_MKS_E * 1.0e7) /* Energy [erg] = -- * Energy [eV]          */
+#define PH_CNV_eV_to_mks   PH_MKS_e /* Energy [N-m] = -- * Energy [eV]          */
+#define PH_CNV_brho_to_p   (1.0e-9 * PH_MKS_c)     /* p [Gev/c] = -- * B*rho [ Tesla meters ]  */
 
 /* Connections:                                                               */
 /*                                                                            */
