4f988a6227f1b863ff8909307ebb4961f9349d98
Eric G. Stern <egstern@fnal.gov>
2014-08-18 15:08:51 -0500

    python bindings to support rfcavities

--- python-bindings/src/py-rfcavity.cpp
+++ python-bindings/src/py-rfcavity.cpp
@@ -31,31 +31,47 @@
 
 using namespace boost::python;
 
+// wrappers for overloaded definitions
+void (rfcavity::*rfcavity_setHarmonicNumber_int)(int n) = &rfcavity::setHarmonicNumber;
+void (rfcavity::*rfcavity_setHarmonicNumber_double)(double const &x) = &rfcavity::setHarmonicNumber;
+
+void (thinrfcavity::*thinrfcavity_setHarmonicNumber_int)(int n) = &thinrfcavity::setHarmonicNumber;
+void (thinrfcavity::*thinrfcavity_setHarmonicNumber_double)(double const &) = &thinrfcavity::setHarmonicNumber;
 void wrap_rfcavity () {
   
 class_<rfcavity, bases<bmlnElmnt>, RFCavityPtr >("rfcavity")
   .def( init<const char*>() )
   .def( init<char*, double,double,double,double,double,double>() )    
+  .def("getHarmonicNumber", &rfcavity::getHarmonicNumber, return_value_policy<copy_const_reference>())
   .def("getPhi",             &rfcavity::getPhi,             return_value_policy<copy_const_reference>())
+  .def("setHarmonicNumber", rfcavity_setHarmonicNumber_int)
+  .def("setHarmonicNumber", rfcavity_setHarmonicNumber_double)
   .def("setPhi",             &rfcavity::setPhi)
   .def("setRadialFrequency",             &rfcavity::setRadialFrequency)
+    .def("setDisplacedFrequency", &rfcavity::setDisplacedFrequency)
   .def("setFrequency",             &rfcavity::setFrequency)
   .def("getRadialFrequency", &rfcavity::getRadialFrequency, return_value_policy<copy_const_reference>())
   .def("getQ",               &rfcavity::getQ,               return_value_policy<copy_const_reference>())
   .def("getR",               &rfcavity::getR,               return_value_policy<copy_const_reference>())
+    .def("turnUpdate", &rfcavity::turnUpdate)
   .def("setStrength",             &rfcavity::setStrength)
   .def("Type",               &rfcavity::Type);
 
 
 class_<thinrfcavity, bases<bmlnElmnt>, ThinRFCavityPtr >("thinrfcavity", init<char *>() )
   .def(init< char*,    double,   double, double, double, double> () )    
+  .def("getHarmonicNumber", &thinrfcavity::getHarmonicNumber, return_value_policy<copy_const_reference>())
   .def("getPhi",             &thinrfcavity::getPhi,             return_value_policy<copy_const_reference>())
+  .def("setHarmonicNumber", thinrfcavity_setHarmonicNumber_int)
+  .def("setHarmonicNumber", thinrfcavity_setHarmonicNumber_double)
   .def("setPhi",             &thinrfcavity::setPhi)
   .def("setRadialFrequency",             &thinrfcavity::setRadialFrequency)
+    .def("setDisplacedFrequency", &thinrfcavity::setDisplacedFrequency)
   .def("setFrequency",             &thinrfcavity::setFrequency)
   .def("getRadialFrequency", &thinrfcavity::getRadialFrequency, return_value_policy<copy_const_reference>())
   .def("getQ",               &thinrfcavity::getQ,               return_value_policy<copy_const_reference>())
   .def("getR",               &thinrfcavity::getR,               return_value_policy<copy_const_reference>())
+    .def("turnUpdate", &thinrfcavity::turnUpdate)
   .def("Type",               &thinrfcavity::Type);
 
 }
