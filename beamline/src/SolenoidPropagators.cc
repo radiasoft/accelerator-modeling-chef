867b28f23a1cde869838b974c428b9e8feb22c3b
Leo Michelotti <michelotti@fnal.gov>
2014-09-17 15:21:55 -0500

    Removed 0/0 singularity for (effectively) zero strength solenoids.
    When | B | < 10^{-13} Tesla, propagation is handled by an
    internal drift instantiated by the propagator's constructor.
    (cherry picked from commit e18ee07bebec01ffb8b27c021d8780c83f171ddd)

--- beamline/src/SolenoidPropagators.cc
+++ beamline/src/SolenoidPropagators.cc
@@ -1,58 +1,63 @@
 /*************************************************************************
 **************************************************************************
-******                                                                
-******  BEAMLINE:  C++ objects for design and analysis
-******             of beamlines, storage rings, and   
-******             synchrotrons.                      
-******                                    
-******  File:      SolenoidPropgators.cc
-******                                                                
-******  Copyright Universities Research Association, Inc./ Fermilab    
-******            All Rights Reserved                             
 ******
-******  Usage, modification, and redistribution are subject to terms          
+******  BEAMLINE:  C++ objects for design and analysis
+******             of beamlines, storage rings, and
+******             synchrotrons.
+******
+******  File:      SolenoidPropgators.cc
+******
+******  Copyright Universities Research Association, Inc./ Fermilab
+******            All Rights Reserved
+******
+******  Usage, modification, and redistribution are subject to terms
 ******  of the License supplied with this software.
-******  
-******  Software and documentation created under 
-******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
-******  The U.S. Government retains a world-wide non-exclusive, 
-******  royalty-free license to publish or reproduce documentation 
-******  and software for U.S. Government purposes. This software 
+******
+******  Software and documentation created under U.S. Department of Energy
+******  Contract No. DE-AC02-76CH03000 and
+******  Contract No. DE-AC02-07CH11359.
+******  The U.S. Government retains a world-wide non-exclusive,
+******  royalty-free license to publish or reproduce documentation
+******  and software for U.S. Government purposes. This software
 ******  is protected under the U.S. and Foreign Copyright Laws.
-******                                                                
-******  Author:    Leo Michelotti                                     
-******                                                                
-******             Fermilab                                           
-******             P.O.Box 500                                        
-******             Mail Stop 220                                      
-******             Batavia, IL   60510                                
-******                                                                
-******             Phone: (630) 840 4956                              
-******             Email: michelotti@fnal.gov                         
-******                                          
-******  Revision Date: 
-******  Original version: April 10, 2007
-****** 
-******  November 2007 ostiguy@fnal.gov 
+******
+******  Author:    Leo Michelotti
+******             Email: michelotti@fnal.gov
+******
+******
+******  REVISION HISTORY:
+******  ----------------
+******  April 10, 2007  michelotti@fnal.gov
+******  Original version.
+******
+******  November 2007   ostiguy@fnal.gov
 ******  - adapted to new propagator architecture
 ******  - templated version
 ******  - various optimizations
-******  
-******  April 2008    michelotti@fnal.gov
-******  - bug fixes: errors had been made in transcribing 
+******
+******  April 2008      michelotti@fnal.gov
+******  - bug fixes: errors had been made in transcribing
 ******    from original algorithm to current one
-******  
+******
+******  September 2014  michelotti@fnal.gov
+******  - removed 0/0 singularity for (effectively) zero strength
+******    solenoids with propagation through a drift.
+******  - "effectively" means | B | < 10^{-13} Tesla.
+******    This bound can be modified by changing the value
+******    of parameter "smallest_nonzero_field" below.
+******
 **************************************************************************
 *************************************************************************/
 
-
-
 #include <basic_toolkit/GenericException.h>
+#include <beamline/drift.h>
 #include <beamline/Particle.h>
 #include <beamline/JetParticle.h>
 #include <beamline/SolenoidPropagators.h>
 
 namespace {
+
+ double const smallest_nonzero_field = 1.0e-13; // Tesla
 
  Particle::PhaseSpaceIndex const& i_x   = Particle::xIndex;
  Particle::PhaseSpaceIndex const& i_y   = Particle::yIndex;
@@ -69,11 +74,10 @@ void propagate( Solenoid& elm, Particle_t & p )
   // ***  NOTE: This implementation assumes the magnetic axis of
   // **** NOTE: the solenoid AND the beam current are in the +z direction.
 
-
   typedef typename PropagatorTraits<Particle_t>::State_t       State_t;
   typedef typename PropagatorTraits<Particle_t>::Component_t   Component_t;
- 
-  
+
+
   State_t& state = p.State();
 
   Component_t& x   = state[i_x  ];
@@ -82,85 +86,82 @@ void propagate( Solenoid& elm, Particle_t & p )
   Component_t& npy = state[i_npy];
   Component_t& cdt = state[i_cdt];
 
-  
-  //------------------
-  // Entry edge effect
-  // -----------------
 
-  double const edgeKick = elm.Strength() / ( 2.0* p.ReferenceBRho() );
+  // If the solenoid's strength is (effectively) zero,
+  // propagate through the drift that was
+  // instantiated by the setup function ...
 
-  if( elm.hasInEdge() ) {
-    npx += edgeKick*y;
-    npy -= edgeKick*x;
+  if( std::abs( elm.Strength() ) < smallest_nonzero_field )
+  {
+    bmlnElmnt::core_access::get_ElmPtr(elm)->propagate( p );
+
+    // NOTE: RefRegVisitor does not touch this element,
+    // so this propagation should advance cdt, which now
+    // must be handled.
+
+    cdt -= elm.getReferenceTime();
   }
 
-  //------------
-  // Body field
-  // -----------
+  // Otherwise, ordinary solenoid propagation ...
 
-  Component_t dtheta =   ( elm.Strength() * elm.Length() / p.ReferenceBRho() ) 
-                 / p.get_npz();
-  
-  Component_t sn = sin(dtheta);
-  Component_t cs = cos(dtheta);
+  else
+  {
+    //------------------
+    // Entry edge effect
+    // -----------------
 
-  #if 0
-  //----------------------------------------------------------------------------
-  // ... In what follows, I assume that dtheta is smaller than pi/4.
-  const double threshold = 1.0e-14;
-  // ??? const double threshold = 4.0*numeric_limits<double>::epsilon(); ???
-  int counter = 0;
-  double eps = ( 1 - sn*sn - cs*cs ) / (2.0*cs);
-  while( threshold < fabs(eps) ) {
-    if( counter < 8 ) {
-      cs += eps;
-      eps = ( 1 - sn*sn - cs*cs ) / (2.0*cs);
-      ++counter;
+    double const edgeKick = elm.Strength() / ( 2.0* p.ReferenceBRho() );
+
+    if( elm.hasInEdge() ) {
+      npx += edgeKick*y;
+      npy -= edgeKick*x;
     }
-    else {
-      throw GenericException( __FILE__, __LINE__, 
-            "void Solenoid::localPropagate( Particle& p )",
-            "Too many iterations to normalize cosine." );
+
+    //------------
+    // Body field
+    // -----------
+
+    Component_t dtheta =   ( elm.Strength() * elm.Length() / p.ReferenceBRho() )
+                   / p.get_npz();
+
+    Component_t sn = sin(dtheta);
+    Component_t cs = cos(dtheta);
+
+    Component_t x_i   = x;
+    Component_t y_i   = y;
+    Component_t npx_i = npx;
+    Component_t npy_i = npy;
+
+    npx =     cs*npx_i + sn*npy_i;
+    npy =  (-sn)*npx_i + cs*npy_i;
+
+    cs -= 1.0;
+
+    x += (    cs*(-npy_i) + sn*npx_i ) / (2.0*edgeKick);
+    y += ( (-sn)*(-npy_i) + cs*npx_i ) / (2.0*edgeKick);
+
+
+    //-----------------
+    // Exit edge effect
+    // ----------------
+
+    if( elm.hasOutEdge() ) {
+      npx -= edgeKick*y;
+      npy += edgeKick*x;
     }
+
+    // It's about time
+    // Assumes pre-registration via RefRegVisitor
+    //
+    // NOTE: p.get_npz() is non-negative by definition.
+    // NOTE: I may want to change this some day.
+    // -----------------------------------------------
+
+    Component_t duration =   elm.Length()
+                     / ( p.get_npz() * p.ReferenceMomentum() / p.Energy() );
+
+    cdt += ( duration - elm.getReferenceTime() );
   }
-  //----------------------------------------------------------------------------
-  #endif
-  
-  Component_t x_i   = x;
-  Component_t y_i   = y;
-  Component_t npx_i = npx;
-  Component_t npy_i = npy;
-
-  npx =     cs*npx_i + sn*npy_i;
-  npy =  (-sn)*npx_i + cs*npy_i;
-  
-  cs -= 1.0;
-
-  x += (    cs*(-npy_i) + sn*npx_i ) / (2.0*edgeKick);
-  y += ( (-sn)*(-npy_i) + cs*npx_i ) / (2.0*edgeKick);
-
-
-  //-----------------
-  // Exit edge effect
-  // ----------------
-
-  if( elm.hasOutEdge() ) {
-    npx -= edgeKick*y;
-    npy += edgeKick*x;
-  }
-
-
-  // It's about time
-  // Assumes pre-registration via RefRegVisitor
-  // 
-  // NOTE: p.get_npz() is non-negative by definition.
-  // NOTE: I may want to change this some day.
-  // -----------------------------------------------
-
-  Component_t duration =   elm.Length() 
-                   / ( p.get_npz() * p.ReferenceMomentum() / p.Energy() );
-
-  cdt += ( duration - elm.getReferenceTime() );
 }
 
 //----------------------------------------------------------------------------------
@@ -180,9 +181,20 @@ template void propagate(          Solenoid& elm, JetParticle& p );
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-void Solenoid::Propagator::setup( Solenoid& arg) 
+void Solenoid::Propagator::setup( Solenoid& arg)
 {
-  // empty for the moment ...
+  // Create a drift to be used when strength is zero.
+  // The propagator equations have a 0/0 singularity
+  // at that point.
+
+  ElmPtr& elm_ptr = bmlnElmnt::core_access::get_ElmPtr(arg);
+  elm_ptr = ElmPtr( new drift( "", arg.Length() ) );
+  
+  // This step should not be necessary, as the drift
+  // constructor (currently) handles this. Nonetheless,
+  // it is included for paranoia's sake.
+
+  elm_ptr->setReferenceTime( 0. );
 }
 
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -200,6 +212,7 @@ void Solenoid::Propagator::operator()( Solenoid& elm, JetParticle& p )
 {
   ::propagate( elm, p);
 }
+
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
