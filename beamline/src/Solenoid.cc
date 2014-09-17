9958f07b0dfc86803d1cd9b25b69b744e12a6530
Leo Michelotti <michelotti@fnal.gov>
2014-09-17 15:21:55 -0500

    Removed 0/0 singularity for (effectively) zero strength solenoids.
    When | B | < 10^{-13} Tesla, propagation is handled by an
    internal drift instantiated by the propagator's constructor.
    (cherry picked from commit e18ee07bebec01ffb8b27c021d8780c83f171ddd)

--- beamline/src/Solenoid.cc
+++ beamline/src/Solenoid.cc
@@ -1,59 +1,50 @@
 /*************************************************************************
 **************************************************************************
-******                                                                
+******
 ******  BEAMLINE:  C++ objects for design and analysis
-******             of beamlines, storage rings, and   
-******             synchrotrons.                      
-******                                    
+******             of beamlines, storage rings, and
+******             synchrotrons.
+******
 ******  File:      Solenoid.cc
-******  Author:    Leo Michelotti
-******                                                                
-******  Copyright Universities Research Association, Inc./ Fermilab    
+******
+******  Copyright Universities Research Association, Inc./ Fermilab
 ******            All Rights Reserved
 ******
-******  Usage, modification, and redistribution are subject to terms          
+******  Usage, modification, and redistribution are subject to terms
 ******  of the License supplied with this software.
-******  
-******  Software and documentation created under 
-******  U.S. Department of Energy Contract No. DE-AC02-76CH03000. 
-******  The U.S. Government retains a world-wide non-exclusive, 
-******  royalty-free license to publish or reproduce documentation 
-******  and software for U.S. Government purposes. This software 
+******
+******  Software and documentation created under U.S. Department of Energy
+******  Contract No. DE-AC02-76CH03000 and
+******  Contract No. DE-AC02-07CH11359.
+******  The U.S. Government retains a world-wide non-exclusive,
+******  royalty-free license to publish or reproduce documentation
+******  and software for U.S. Government purposes. This software
 ******  is protected under the U.S. and Foreign Copyright Laws.
-******                                                                
-******             Fermilab                                           
-******             P.O.Box 500                                        
-******             Mail Stop 220                                      
-******             Batavia, IL   60510                                
-******                                                                
-******             Phone: (630) 840 4956                              
-******             Email: michelotti@fnal.gov                         
-******                                                                
-******                                                                
+******
+******  Author:    Leo Michelotti
+******             Email: michelotti@fnal.gov
+******
+******
 ******  REVISION HISTORY:
 ******  ----------------
-******  Date   : April 10, 2007
-******  Author : Leo Michelotti
-******  Changes: Original version.
-******                                                                
-******  Apr 2007 ostiguy@fnal.gov
+******  April 10, 2007  michelotti@fnal.gov
+******  Original version.
 ******
+******  Apr 2007        ostiguy@fnal.gov
 ******  - support for reference counted elements
 ******  - eliminated unnecessary casts
 ******  - use std::string for renaming
-******  - changes to header file to reduce file coupling 
+******  - changes to header file to reduce file coupling
 ******
-******  Dec 2007 ostiguy@fnal.gov
+******  Dec 2007        ostiguy@fnal.gov
 ******  - new typesafe propagator architecture
-****** 
-******  February 20, 2008  michelotti@fnal.gov
+******
+******  Feb 2008        michelotti@fnal.gov
 ******  - bug fix: instantiated attribute propagator_
 ******    in the Solenoid constructors
-****** 
+******
 **************************************************************************
 *************************************************************************/
-
-
 
 #include <iomanip>
 
@@ -70,13 +61,13 @@ using FNAL::pcout;
 
 
 ///////////////////////////////////////////////////
-// 
+//
 // Constructors (and the destructor)
-// 
+//
 ///////////////////////////////////////////////////
 
 Solenoid::Solenoid()
-:  bmlnElmnt("", 1.0, 0.0), 
+:  bmlnElmnt("", 1.0, 0.0),
    inEdge_(true), outEdge_(true)
 {
   propagator_ = PropagatorPtr( new Propagator() );
@@ -106,9 +97,9 @@ Solenoid::Solenoid( Solenoid const& x )
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-Solenoid* Solenoid::Clone() const 
-{ 
-  return new Solenoid( *this ); 
+Solenoid* Solenoid::Clone() const
+{
+  return new Solenoid( *this );
 
 }
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -122,9 +113,9 @@ Solenoid::~Solenoid()
 
 
 ///////////////////////////////////////////////////
-// 
+//
 // Other member function(s) (except localPropagate)
-// 
+//
 ///////////////////////////////////////////////////
 
 void Solenoid::Split( double const& pc, ElmPtr& a, ElmPtr& b ) const
@@ -132,28 +123,28 @@ void Solenoid::Split( double const& pc, ElmPtr& a, ElmPtr& b ) const
   if( ( pc <= 0.0 ) || ( pc >= 1.0 ) ) {
     ostringstream uic;
     uic  << "pc = " << pc << ": this should be within [0,1].";
-    throw( GenericException( __FILE__, __LINE__, 
-           "void Solenoid::Split( double const&, bmlnElmnt**, bmlnElmnt** ) const", 
+    throw( GenericException( __FILE__, __LINE__,
+           "void Solenoid::Split( double const&, bmlnElmnt**, bmlnElmnt** ) const",
            uic.str().c_str() ) );
   }
 
   // We assume "strength" means field, not field*length_.
   // --------------------------------------------------
- 
+
    SolenoidPtr s_a( (Clone()) );
    s_a->setLength( pc  *length_ );
 
-   s_a->inEdge_   = inEdge_;    
-   s_a->outEdge_  = false;      
+   s_a->inEdge_   = inEdge_;
+   s_a->outEdge_  = false;
 
    s_a->rename(  Name() + std::string("_1") );
-  
-   SolenoidPtr s_b( (Clone()) ); 
+
+   SolenoidPtr s_b( (Clone()) );
 
    s_b->setLength( ( 1.0 - pc )*length_ );
 
-   s_b->inEdge_   = false;      
-   s_b->outEdge_  = outEdge_;   
+   s_b->inEdge_   = false;
+   s_b->outEdge_  = outEdge_;
    s_b->rename( Name() + std::string( "_2" ) );
 
    a = s_a;
@@ -184,9 +175,9 @@ const char* Solenoid::Type() const
 
 bool Solenoid::hasInEdge()   const
 {
-  
+
   return inEdge_;
-} 
+}
 
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -200,17 +191,17 @@ bool Solenoid::hasOutEdge()  const
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-void Solenoid::accept( BmlVisitor& v )            
-{  
-   v.visit( *this ); 
+void Solenoid::accept( BmlVisitor& v )
+{
+   v.visit( *this );
 }
 
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-void Solenoid::accept( ConstBmlVisitor& v ) const 
-{ 
-  v.visit( *this ); 
+void Solenoid::accept( ConstBmlVisitor& v ) const
+{
+  v.visit( *this );
 }
 
 //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -227,32 +218,32 @@ void Solenoid::usePropagator( PropagatorPtr& x )
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-void Solenoid::localPropagate( Particle &p ) 
-{ 
+void Solenoid::localPropagate( Particle &p )
+{
   (*propagator_)(*this,p);
 }
 
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-void Solenoid::localPropagate( JetParticle &p ) 
-{ 
+void Solenoid::localPropagate( JetParticle &p )
+{
   (*propagator_)(*this,p);
 }
 
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-void Solenoid::localPropagate( ParticleBunch &b ) 
-{ 
+void Solenoid::localPropagate( ParticleBunch &b )
+{
   (*propagator_)(*this,b);
 }
 
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-void Solenoid::localPropagate( JetParticleBunch &b ) 
-{ 
+void Solenoid::localPropagate( JetParticleBunch &b )
+{
   (*propagator_)(*this,b);
 }
 
