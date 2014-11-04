91ee84632c1af6ad7cbc6266e70fe1ad79f8066b
Leo Michelotti <michelotti@fnal.gov>
2014-11-04 14:54:32 -0600

    Removed Mapping argument from signature of constructor (devel-synergia version)
    This version of files physics_toolkit/include/normalFormSage.h and
    physics_toolkit/src/normalFormSage.cc are (more or less) equivalent to
    those given to Alex Macridin on June 12, 2014 but without the
    additional code needed to handle offset closed orbits.
    (cherry picked from commit 852c2e0ea9f625394c212d110c9638cf6c51cf18)

--- physics_toolkit/src/normalFormSage.cc
+++ physics_toolkit/src/normalFormSage.cc
@@ -1,11 +1,101 @@
+/*************************************************************************
+**************************************************************************
+**************************************************************************
+******
+******  PHYSICS TOOLKIT: Library of utilites and Sage classes
+******             which facilitate calculations with the
+******             BEAMLINE class library.
+******
+******  File:      normalFormSage.cc
+******
+******  Copyright (c) Fermi Research Alliance
+******                Universities Research Association, Inc.
+******                Fermilab
+******                All Rights Reserved
+******
+******  Usage, modification, and redistribution are subject to terms
+******  of the License supplied with this software.
+******
+******  Software and documentation created under
+******  U.S. Department of Energy Contracts No. DE-AC02-76CH03000
+******  and No. DE-AC02-07CH11359.
+******
+******  The U.S. Government retains a world-wide non-exclusive,
+******  royalty-free license to publish or reproduce documentation
+******  and software for U.S. Government purposes. This software
+******  is protected under the U.S. and Foreign Copyright Laws.
+******
+******
+******  Authors:   Leo Michelotti     michelotti@fnal.gov
+******             Eric Stern         egstern@fnal.gov
+******
+******
+******  ----------------
+******  REVISION HISTORY
+******  ----------------
+******
+******  Sep 2001        michelotti@fnal.gov
+******  - original version of this code was written as a graphical
+******    demonstration of normal form coordinates at any order
+******    in two-dimensional longitudinal phase space.
+******  - It used the already existing procedures in
+******    physics_toolkit/src/normalForm.cc to generate the
+******    operators, which were then used to create the normal
+******    form coordinates.
+******  - the original demo was called "macdemo.cc," because it was
+******    written for Jim Maclachlan. A modified version still survives
+******    as widget_toolkit/tests/lpsDemo.cc.
+******
+******  Sep 2010        michelotti@fnal.gov
+******  - procedures in macdemo are encapsulated into a new class 
+******    called EquilibriumSage, because its intent was to
+******    generate equilibrium distributions using normal form coordinates.
+******  - only prototype version written.
+******
+******  Jan - Feb 2011  michelotti@fnal.gov
+******  - prototype EquilbriumSage is generalized to higher dimensions.
+******  - name change to normalFormSage and passed on for testing.
+******
+******  Feb - Apr 2011  egstern@fnal.gov
+******  - changed signature of constructor to accept JetProton
+******  - transformations to canonical coordinates (x, p_x, y, p_y, -t, E )
+******  - tests added to the code. 
+******  - utility functions added, such as checkLinearNormalForm
+******  - changed normalFormSage class so it does not create jet environment
+******  
+******  Jan - Feb 2012  egstern@fnal.gov
+******  - added signatures to checkLinearNormalForm
+******  - repressed warnings about imaginary coordinates
+******
+******  Jun 2014        michelotti@fnal.gov
+******  - changed signature of constructor to eliminate usage of
+******    both a JetProton and a Mapping.
+******  - modified code in anticipation of need to extend to
+******    usage with offset closed orbits (for Alex Macridin).
+******
+******
+**************************************************************************
+*************************************************************************/
+
 #include <physics_toolkit/normalFormSage.h>
 
+#define DIM  6  // ??? BAD FORM ???
+
+#define I_X  0  // ??? BAD FORM ???
+#define I_Y  1
+#define I_T  2
+#define I_PX 3
+#define I_PY 4
+#define I_DE 5
+
+#define THROTTLE 10
+#define NAG      1000
 
 extern void normalForm( const Mapping& theMapping, /* input */
-                 int            maxOrder,   /* input */
-                 MatrixC*       Bptr,
-                 CLieOperator*  N,
-			CLieOperator*  T ) ;
+                        int            maxOrder,   /* input */
+                        MatrixC*       Bptr,
+                        CLieOperator*  N,
+                        CLieOperator*  T );
 
 
 
@@ -13,62 +103,43 @@ extern void normalForm( const Mapping& theMapping, /* input */
 // Implementation: class normalFormSage
 // -----------------------------
 
-normalFormSage::normalFormSage( Mapping & Map, const JetParticle &jpr, int theOrder)
-    :   _E(DIM,DIM)
-    , _invE(DIM,DIM)
-    , _XG(0)
-    , _T(0)
-    , _f(0)
-    , _g(0)
-    , _state(DIM)
-    , _p_je(0)
-    , _order(theOrder)
-    , _p_rf( &Map )
-    , _canonMap("id")
-    , _initialized(false)
+normalFormSage::normalFormSage( JetParticle const& jpr, int theOrder )
+    : E_(DIM,DIM)
+    , invE_(DIM,DIM)
+    , XG_(0)
+    , T_(0)
+    , f_(0)
+    , g_(0)
     , CanonToChef("id")
     , ChefToCanon("id")
+    , order_(theOrder)
+    , canonMap_("id")
 {
-
-  _order = theOrder;
-
-  // initialize _state to 0
-  for (int ii=0; ii<DIM; ++ii) {
-    _state(ii) = 0.0;
-  }
-  Jet x, y, t, px, py, de;
-
-  x = jpr.get_x();
-  y = jpr.get_y();
-  t = jpr.get_cdt();
-  px = jpr.get_npx();
-  py = jpr.get_npy();
-  de = jpr.get_ndp();
-
-  Mapping M;
-  
-  M.SetComponent( I_X, x );
-  M.SetComponent( I_Y, y);
-  M.SetComponent( I_T, t);
-  M.SetComponent( I_PX, px);
-  M.SetComponent( I_PY, py);
-  M.SetComponent( I_DE, de);
-  
-  MatrixD jac(DIM,DIM);
-  M = *_p_rf;
-  
-  jac = M.Jacobian();
-
-  _E = jac.eigenVectors();
-  _invE = _E.inverse();
+  Mapping M( jpr.State() );
+  MatrixD jac( M.Jacobian() );
+  E_ = jac.eigenVectors();
+  invE_ = E_.inverse();      // ??? Not necessary here; see below  ???
 
   // initialize the mappings that turn chef coordinates into a
   // symplectic and canonical coordinate basis
 
-  double e0 = jpr.ReferenceEnergy();
-  double pc0 = jpr.ReferenceMomentum();
+  double e0    = jpr.ReferenceEnergy();
+  double pc0   = jpr.ReferenceMomentum();
   double pmass = jpr.Mass();
-  
+
+
+  // canonMap_ was set to the identity
+  // before entering the constructor's body
+  // and unchanged since.
+  // Thus, these are coordinates.
+  // -------------------------
+  Jet  x( canonMap_[0] );
+  Jet  y( canonMap_[1] );
+  Jet  t( canonMap_[2] );
+  Jet px( canonMap_[3] );
+  Jet py( canonMap_[4] );
+  Jet de( canonMap_[5] );  // ??? de ???  WHAT ???
+
   Jet realP, deltaE;
   // first map chef variables to canonical variables
   // change transverse momenta
@@ -80,16 +151,16 @@ normalFormSage::normalFormSage( Mapping & Map, const JetParticle &jpr, int theOr
   realP = (1.0+de)*pc0 ;
   deltaE = sqrt( realP*realP + pmass*pmass ) - e0;
   ChefToCanon.SetComponent(I_DE, deltaE);
-  
+
   // map from canonical variables to chef variables
   CanonToChef.SetComponent(I_T, -t*PH_MKS_c);
   CanonToChef.SetComponent(I_PX, px*PH_MKS_c/pc0);
   CanonToChef.SetComponent(I_PY, py*PH_MKS_c/pc0);
-  
+
   // convert deltaE to dp/p.  For the canonical particle, pz is
   //    really deltaE.
   Jet dp, realE;
-  realE = de+e0;
+  realE = de+e0;     // ??? ???
   dp = sqrt(realE*realE - pmass*pmass) -  pc0;
   CanonToChef.SetComponent(I_DE, dp/pc0);
 
@@ -98,46 +169,44 @@ normalFormSage::normalFormSage( Mapping & Map, const JetParticle &jpr, int theOr
   // apply the maps that turns a canonical particle to a chef particle,
   // one turn map of a chef particle, chef particle to canonical particle.
 
-  _canonMap = ChefToCanon(M(CanonToChef));
+  canonMap_ = ChefToCanon(M(CanonToChef));
 
-  if( _XG ) { delete [] _XG;}
-  _XG = new CLieOperator [_order];
-  if( _T  ) { delete [] _T;}
-  _T  = new CLieOperator [_order];
+  if( XG_ ) { delete [] XG_;}
+  XG_ = new CLieOperator [order_];
+  if( T_  ) { delete [] T_;}
+  T_  = new CLieOperator [order_];
 
-  normalForm( _canonMap, _order, &_E, _XG, _T );
+  normalForm( canonMap_, order_, &E_, XG_, T_ );
 
-  _invE = _E.inverse();
+  invE_ = E_.inverse();
 
   // Construct the transformations
   MappingC id( "ident" );
-  if( _f  ) { delete [] _f; }
-  _f = new MappingC [ _order - 1 ];
-  if( _g  ) { delete [] _g; }
-  _g = new MappingC [ _order - 1 ];
-  for( int i = 0; i < _order - 1; i++ ) {
-    _f[i] = _T[i].expMap(-1.0,id);
-    _f[i] = _f[i].filter(0,i+2);
-    _g[i] = _T[i].expMap(1.0,id);
-    _g[i] = _g[i].filter(0,i+2);
+  if( f_  ) { delete [] f_; }
+  f_ = new MappingC [ order_ - 1 ];
+  if( g_  ) { delete [] g_; }
+  g_ = new MappingC [ order_ - 1 ];
+  for( int i = 0; i < order_ - 1; i++ ) {
+    f_[i] = T_[i].expMap(-1.0,id);
+    f_[i] = f_[i].filter(0,i+2);
+    g_[i] = T_[i].expMap(1.0,id);
+    g_[i] = g_[i].filter(0,i+2);
   }
-
-  _initialized = true;
 }
 
 
 normalFormSage::~normalFormSage()
 {
-  if( _XG ) { delete [] _XG;}
-  if( _T  ) { delete [] _T;}
-  if( _f  ) { delete [] _f; }
-  if( _g  ) { delete [] _g; }
+  if( XG_ ) { delete [] XG_;}
+  if( T_  ) { delete [] T_;}
+  if( f_  ) { delete [] f_; }
+  if( g_  ) { delete [] g_; }
 }
 
 // coordinates are assumed to be "sensibly ordered" in the Michelotti sense
 // hform are the coordinates in human form input
 // nform are the (complex) normal form coordinates (output)
-void normalFormSage::cnvDataToNormalForm( const Vector &hform, VectorC &nform )
+void normalFormSage::cnvDataToNormalForm( Vector const& hform, VectorC &nform )
 {
   static MatrixC u(DIM,1);
   std::vector<std::complex<double> > a(DIM);
@@ -158,15 +227,15 @@ void normalFormSage::cnvDataToNormalForm( const Vector &hform, VectorC &nform )
     u(i) = hsymp(i);
   }
 
-  u = _invE*u;
+  u = invE_*u;
 
-  for( i = 0; i < _order - 1; i++ ) {
+  for( i = 0; i < order_ - 1; i++ ) {
     for( j = 0; j < DIM; j++ ) {
       a[j] = u(j);
     }
     // cout << "    before: " << u << endl;
     for( j = 0; j < DIM; j++ ) {
-      u(j) = (_f[i](j))(a);
+      u(j) = (f_[i](j))(a);
       // u(j) = a[j];
     }
     // cout << "    after: " << u << endl;
@@ -178,14 +247,14 @@ void normalFormSage::cnvDataToNormalForm( const Vector &hform, VectorC &nform )
 }
 
 // vec are normal form coordinates.  nform(i+3) is the conjugate of nform(i)
-void normalFormSage::cnvDataFromNormalForm( const VectorC &nform, Vector& hform )
+void normalFormSage::cnvDataFromNormalForm( VectorC const& nform, Vector& hform )
 {
-  static MatrixC u(DIM,1);
-  static int nwarnings=0;
-  static int nagthreshold=NAG;
-  static int warningsdecade=0;
+  static int  nwarnings=0;
+  static int  nagthreshold=NAG;
+  static int  warningsdecade=0;
   static bool throttled=false;
   
+  static MatrixC u(DIM,1);
   for (int i=0; i<DIM; ++i) {
     u(i) = nform(i);
   }
@@ -193,73 +262,93 @@ void normalFormSage::cnvDataFromNormalForm( const VectorC &nform, Vector& hform 
   std::vector<std::complex<double> > a(DIM);
   static int i, j;
 
-  for( i = _order - 2; i >= 0; --i ) {
-    // cout << "_cnvDataFromNormalForm: applying _g order " << i << endl;
+  for( i = order_ - 2; i >= 0; --i ) {
     for( j = 0; j < DIM; j++ ) {
       a[j] = u(j);
     }
     //cout << "    before: " << u << endl;
     for( j = 0; j < DIM; j++ ) {
-      u(j) = (_g[i](j))(a);
+      u(j) = (g_[i](j))(a);
     }
     // cout << "    after: " << u << endl;
   }
 
-  u = _E*u;
+  u = E_*u;
 
   Vector hsymp(DIM);
   const double small_thresh = 5.0e-11;
-  //  const double small_thresh = 1.0e-12;
-  for (i=0; i<DIM; ++i) {
+
+  #if 1
+  // This is the test as modified by Eric Stern.
+  // ------------------------------------------
+  for (i = 0; i < DIM; ++i )
+  {
     // imaginary part of u(i) should be small.
-    if (std::abs(real(u(i))) > small_thresh) {
+    if (std::abs(real(u(i))) > small_thresh) 
+    {
       // if the real part is non-zero, the imaginary part should
       // be a small fraction of it.
       if (std::abs(imag(u(i))/real(u(i))) > small_thresh) {
-	++nwarnings;
-	if (!throttled) {
-	  cout << "error, imaginary part of human form coordinate relatively large" << endl;
-	  cout << u << endl;
-	} else if (nwarnings%nagthreshold == 0) { // throttled, but maybe we'll nag
-	  cout << "error, imaginary part of human form coordinate, " << nwarnings << " times." << endl;
-	  ++warningsdecade;
-	  if (warningsdecade == 9) {
-	    nagthreshold *= 10;
-	    warningsdecade = 0;
-	  }
-	}
-	if (nwarnings >= THROTTLE) {
-	  throttled = true;
-	}
+        ++nwarnings;
+        if (!throttled) {
+          cout << "error, imaginary part of human form coordinate relatively large" << endl;
+          cout << u << endl;
+        } else if (nwarnings%nagthreshold == 0) { // throttled, but maybe we'll nag
+          cout << "error, imaginary part of human form coordinate, " << nwarnings << " times." << endl;
+          ++warningsdecade;
+          if (warningsdecade == 9) {
+            nagthreshold *= 10;
+            warningsdecade = 0;
+          }
+        }
+        if (nwarnings >= THROTTLE) {
+          throttled = true;
+        }
       }
     } else {
       // the absolute value of the real part is small, the imaginary part
       // should be similarly small
       if (std::abs(imag(u(i))) > small_thresh) {
-	++nwarnings;
-	if (!throttled) {
-	  cout << "error, real and imaginary parts of human form coordinate both real and similarly small" << endl;
-	  cout << u << endl;
-	} else if (nwarnings%nagthreshold == 0) { // throttled, but maybe we'll nag
-	  cout << "error, imaginary part of human form coordinate, " << nwarnings << " times." << endl;
-	  ++warningsdecade;
-	  if (warningsdecade == 9) {
-	    nagthreshold *= 10;
-	    warningsdecade = 0;
-	  }
-	}
-	if (nwarnings >= THROTTLE) {
-	  throttled = true;
-	}
+        ++nwarnings;
+        if (!throttled) {
+          cout << "error, real and imaginary parts of human form coordinate both real and similarly small" << endl;
+          cout << u << endl;
+        } else if (nwarnings%nagthreshold == 0) { // throttled, but maybe we'll nag
+          cout << "error, imaginary part of human form coordinate, " << nwarnings << " times." << endl;
+          ++warningsdecade;
+          if (warningsdecade == 9) {
+            nagthreshold *= 10;
+            warningsdecade = 0;
+          }
+        }
+        if (nwarnings >= THROTTLE) {
+          throttled = true;
+        }
       }
     }
       
     hsymp(i) = real(u(i));
   }
+  // -------------------------------------
+  #endif
+  #if 0
+  // Here is the original form of the test
+  // -------------------------------------
+  for (i=0; i<DIM; ++i) {
+    if (std::abs(imag(u(i))) > 1.0e-8) {
+      cout << "error, imaginary part of human form coordinate not 0" << endl;
+      cout << u << endl;
+    }
+    hsymp(i) = real(u(i));
+  }
+  // -------------------------------------
+  #endif
+
 
   // transform canonical to chef coordinates
   hform = CanonToChef(hsymp);
 }
+
 
 bool normalFormSage::checkLinearNormalForm()
 {
@@ -267,46 +356,55 @@ bool normalFormSage::checkLinearNormalForm()
   return normalFormSage::checkLinearNormalForm(default_toler);
 }
 
-bool normalFormSage::checkLinearNormalForm(double toler)
+
+bool normalFormSage::checkLinearNormalForm( double toler )
 {
-  // _E is the linear normal form matrix.  According to the Theory and
-  // Praxis of Linear Normal Forms, when _E is properly constructed,
-  // _E' * J * _E = i J, or i * _E' * J * _E * J is the identity
+  // E_ is the linear normal form matrix.  According to the Theory and
+  // Praxis of Linear Normal Forms, when E_ is properly constructed,
+  // E_' * J * E_ = i J, or i * E_' * J * E_ * J is the identity
   // matrix.  This routine checks this condition.  Returns true
   // if check works.
+
   std::complex<double> complex_i(0.0,1.0); // i
   MatrixD J("J", DIM);
-  MatrixC NE = (_E.transpose() * J * _E * J) * complex_i;
+  MatrixC NE = (E_.transpose() * J * E_ * J) * complex_i;
   bool checkOK = true;
+
+  cout << "DGN: " << __FILE__ << "  " << __LINE__
+       << ": printing NE" << '\n'
+       << NE
+       << '\n' << endl;
 
   for (int i=0; i<DIM; ++i) {
     for (int j = 0; j<DIM; ++j) {
       if (i==j) {
-	if (std::abs(NE(i,j)-1.0) > toler) {
-	  checkOK = false;
-	  // std::cout << "Error, element "<< i << "," << j << " of iB'*J*B is not unity: " << NE(i,j) << std::endl;
-	}
+        if (std::abs(NE(i,j)-1.0) > toler) {
+          checkOK = false;
+        }
       } else {
-	if (std::abs(NE(i,j)) > toler) {
-	  checkOK = false;
-	  // std::cout << "Error, element " << i << "," << j << " iB'*J*B is not 0: " << NE(i,j) << std::endl;
-	}
+        if (std::abs(NE(i,j)) > toler) {
+          checkOK = false;
+        }
       }
     }
   }
+
   return checkOK;
 }
+
 
 // return the mapping that was used by the sage to contruct the normal form
 Mapping  normalFormSage::getCanonMap()
 {
-  return (_canonMap);
+  return (canonMap_);
 }
+
 
 const MatrixC& normalFormSage::getNormalEigenvectors()
 {
-  return _E;
+  return E_;
 }
+
 
 // use the linear part of the mapping to find the mean actions that result in
 //   a distribution of given second moments.  
@@ -317,7 +415,7 @@ std::vector<double> normalFormSage::stationaryActions(const double stdx, const d
   // matrix bmom(i,j) = 2*|B(i,j)|**2
   for (i=0; i<3; ++i) {
     for (j=0; j<3; ++j) {
-      bmom(i,j) = 2.0 * real(_E(i,j) * conj(_E(i,j)));
+      bmom(i,j) = 2.0 * real(E_(i,j) * conj(E_(i,j)));
     }
   }
 
@@ -339,5 +437,3 @@ std::vector<double> normalFormSage::stationaryActions(const double stdx, const d
 
   return meanActions;
 }
-
-		    
