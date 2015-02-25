47093e0dbbcc45f4e5908d284e6d04d072712bf7
Chong Shik Park <cspark@fnal.gov>
2015-02-25 16:32:15 -0600

    bug fix: NonlinearLensPropagators.cc
    avoid singularities

--- beamline/src/NonLinearLensPropagators.cc
+++ beamline/src/NonLinearLensPropagators.cc
@@ -43,6 +43,10 @@
 #include <beamline/ParticleBunch.h>
 #include <beamline/nonLinearLens.h>
 
+using namespace std;
+using FNAL::pcerr;
+using FNAL::pcout;
+
 namespace {
 
   Particle::PhaseSpaceIndex const& i_x   = Particle::xIndex;
@@ -53,11 +57,14 @@ namespace {
   Particle::PhaseSpaceIndex const& i_ndp = Particle::ndpIndex;
 
 template<typename Particle_t>
-void propagate( nonLinearLens& elm, Particle_t& p )
+void propagate( nonLinearLens& elm, Particle_t& p );
+
+template<>
+void propagate( nonLinearLens& elm, Particle& p )
 {
-   
-  typedef typename PropagatorTraits<Particle_t>::State_t       State_t;
-  typedef typename PropagatorTraits<Particle_t>::Component_t   Component_t;
+
+  typedef PropagatorTraits<Particle>::State_t       State_t;
+  typedef PropagatorTraits<Particle>::Component_t   Component_t;
 
   State_t& state = p.State();
 
@@ -69,33 +76,131 @@ void propagate( nonLinearLens& elm, Particle_t& p )
   Component_t xbar = state[i_x] / cnll_;
   Component_t ybar = state[i_y] / cnll_;
 
-  Component_t sq_xpy = sqrt((xbar + 1.0)*(xbar + 1.0) + ybar * ybar);
-  Component_t sq_xmy = sqrt((xbar - 1.0)*(xbar - 1.0) + ybar * ybar);
+  Component_t sq_xpy = sqrt((xbar + 1.)*(xbar + 1.) + ybar * ybar);
+  Component_t sq_xmy = sqrt((xbar - 1.)*(xbar - 1.) + ybar * ybar);
 
   Component_t xi  = 0.5 * (sq_xpy + sq_xmy);
   Component_t eta = 0.5 * (sq_xpy - sq_xmy);
 
-  Component_t dxi_dxbar  = 0.5 * ((xbar+1.0) / sq_xpy + (xbar-1.0) / sq_xmy);
-  Component_t dxi_dybar  = 0.5 * ybar * (1.0 / sq_xpy + 1.0 / sq_xmy);
-  Component_t deta_dxbar = 0.5 * ((xbar+1.0) / sq_xpy - (xbar-1.0) / sq_xmy);
-  Component_t deta_dybar = 0.5 * ybar * (1.0 / sq_xpy - 1.0 / sq_xmy);
+  Component_t dU_dxbar;
+  Component_t dU_dybar;
 
-  Component_t xixi_m_etaeta = (xi * xi - eta * eta);
-  // csp: avoid a zero argument on sqrt
-  Component_t sq_xixi_m_1 = (xi == 1.0) ? 0.0 : sqrt(xi * xi - 1.0);
-  Component_t sq_1_m_etaeta = sqrt(1.0 - eta * eta);
-  // csp: avoid dividing by a zero
-  Component_t singluar_term = (xi == 1.0) ? 0.0 : xi * xi / sq_xixi_m_1;
-  Component_t d_hacn_xi = dcoeff_ + knll_ * log(xi + sq_xixi_m_1);
-  Component_t b_acn_eta = bcoeff_ + knll_ * acos(eta);
+  if ( ( ybar == 0 ) && ( abs(xbar) == 1. ) ) {
+    (*pcerr) << "*** ERROR *** "
+             << "\n*** ERROR *** " << __FILE__ << ", " << __LINE__
+             << "\nNonLinearLensPropagator propagates with singular points: "
+             << xbar
+             << endl;
+  } else if ( ( ybar == 0 ) && ( std::abs(xbar) != 1. ) ) {
+    dU_dxbar = - eta;
+    dU_dxbar += sqrt( 1. - eta * eta ) * ( bcoeff_ / knll_ + acos(eta) );
+    dU_dxbar += - eta * eta * ( bcoeff_ / knll_ + acos(eta) ) / sqrt( 1. - eta * eta );
+    dU_dxbar += 2. * eta * eta * sqrt( 1. - eta * eta ) * ( bcoeff_ / knll_ + acos(eta) ) / ( 1. - eta * eta );
 
-  Component_t f2_g2 = (xi * sq_xixi_m_1 * d_hacn_xi + eta * sq_1_m_etaeta * b_acn_eta);
+    dU_dxbar /= ( 1. - eta * eta );
+    dU_dxbar *=  ( knll_ / cnll_ );
 
-  Component_t dU_dxi = ( knll_ * xi + sq_xixi_m_1 * d_hacn_xi + singluar_term * d_hacn_xi) / xixi_m_etaeta - 2.0 * xi * f2_g2 / (xixi_m_etaeta * xixi_m_etaeta);
-  Component_t dU_deta = - (knll_ * eta - sq_1_m_etaeta * b_acn_eta + eta * eta / sq_1_m_etaeta * b_acn_eta) / xixi_m_etaeta + 2.0 * eta * f2_g2 / (xixi_m_etaeta * xixi_m_etaeta);
+    dU_dybar = 0.0;
+  } else {
+    Component_t dxi_dxbar  = 0.5 * ((xbar+1.) / sq_xpy + (xbar-1.) / sq_xmy);
+    Component_t dxi_dybar  = 0.5 * ybar * (1. / sq_xpy + 1. / sq_xmy);
+    Component_t deta_dxbar = 0.5 * ((xbar+1.) / sq_xpy - (xbar-1.) / sq_xmy);
+    Component_t deta_dybar = 0.5 * ybar * (1. / sq_xpy - 1. / sq_xmy);
 
-  state[i_npx] += (dU_dxi * dxi_dxbar + dU_deta * deta_dxbar) / cnll_;
-  state[i_npy] += (dU_dxi * dxi_dybar + dU_deta * deta_dybar) / cnll_;
+    Component_t dU_dxi = xi;
+    dU_dxi += sqrt( xi * xi - 1. ) * ( dcoeff_ / knll_ + log( xi + sqrt(xi * xi - 1. ) ) );
+    dU_dxi += xi * xi * ( dcoeff_ / knll_ + log( xi + sqrt(xi * xi - 1. ) ) ) / sqrt( xi * xi - 1. );
+    dU_dxi += - 2. * xi * xi * sqrt( xi * xi - 1. ) * ( dcoeff_ / knll_ + log( xi + sqrt(xi * xi - 1. ) ) ) / ( xi * xi - eta * eta );
+    dU_dxi += - 2. * xi * eta * sqrt( 1. - eta * eta ) * ( bcoeff_ / knll_ + acos(eta) ) / ( xi * xi - eta * eta );
+
+    dU_dxi /= ( xi * xi - eta * eta );
+
+    Component_t dU_deta = - eta;
+    dU_deta += sqrt( 1. - eta * eta ) * ( bcoeff_ / knll_ + acos(eta) );
+    dU_deta += - eta * eta * ( bcoeff_ / knll_ + acos(eta) ) / sqrt( 1. - eta * eta);
+    dU_deta += 2. * eta * xi * sqrt( xi * xi - 1. ) * ( dcoeff_ / knll_ + log( xi + sqrt(xi * xi - 1. ) ) ) / ( xi * xi - eta * eta );
+    dU_deta += 2. * eta * eta * sqrt( 1. - eta * eta ) * ( bcoeff_ / knll_ + acos(eta) ) / ( xi * xi - eta * eta );
+
+    dU_deta /= ( xi * xi - eta * eta );
+
+    dU_dxbar = (dU_dxi * dxi_dxbar + dU_deta * deta_dxbar) * ( knll_ / cnll_ );
+    dU_dybar = (dU_dxi * dxi_dybar + dU_deta * deta_dybar) * ( knll_ / cnll_ );
+  }
+
+  state[i_npx] += dU_dxbar;
+  state[i_npy] += dU_dybar;
+}
+
+template<>
+void propagate( nonLinearLens& elm, JetParticle& p )
+{
+   
+  typedef PropagatorTraits<JetParticle>::State_t       State_t;
+  typedef PropagatorTraits<JetParticle>::Component_t   Component_t;
+
+  State_t& state = p.State();
+
+  double const knll_ = elm.get_knll();
+  double const cnll_ = elm.get_cnll();
+  double const bcoeff_ = elm.get_bcoeff();
+  double const dcoeff_ = elm.get_dcoeff();
+
+  Component_t xbar = state[i_x] / cnll_;
+  Component_t ybar = state[i_y] / cnll_;
+
+  Component_t sq_xpy = sqrt((xbar + 1.)*(xbar + 1.) + ybar * ybar);
+  Component_t sq_xmy = sqrt((xbar - 1.)*(xbar - 1.) + ybar * ybar);
+
+  Component_t xi  = 0.5 * (sq_xpy + sq_xmy);
+  Component_t eta = 0.5 * (sq_xpy - sq_xmy);
+
+  Component_t dU_dxbar;
+  Component_t dU_dybar;
+
+  if ( ( ybar.standardPart() == 0 ) && ( abs(xbar.standardPart()) == 1. ) ) {
+    (*pcerr) << "*** ERROR *** "
+             << "\n*** ERROR *** " << __FILE__ << ", " << __LINE__
+             << "\nNonLinearLensPropagator propagates with singular points: "
+             << xbar
+             << endl;
+  } else if ( ( ybar.standardPart() == 0 ) && ( std::abs(xbar.standardPart()) != 1. ) ) {
+    dU_dxbar = - eta;
+    dU_dxbar += sqrt( 1. - eta * eta ) * ( bcoeff_ / knll_ + acos(eta) );
+    dU_dxbar += - eta * eta * ( bcoeff_ / knll_ + acos(eta) ) / sqrt( 1. - eta * eta );
+    dU_dxbar += 2. * eta * eta * sqrt( 1. - eta * eta ) * ( bcoeff_ / knll_ + acos(eta) ) / ( 1. - eta * eta );
+
+    dU_dxbar /= ( 1. - eta * eta );
+    dU_dxbar *=  ( knll_ / cnll_ );
+
+    dU_dybar = 0.0;
+  } else {
+    Component_t dxi_dxbar  = 0.5 * ((xbar+1.) / sq_xpy + (xbar-1.) / sq_xmy);
+    Component_t dxi_dybar  = 0.5 * ybar * (1. / sq_xpy + 1. / sq_xmy);
+    Component_t deta_dxbar = 0.5 * ((xbar+1.) / sq_xpy - (xbar-1.) / sq_xmy);
+    Component_t deta_dybar = 0.5 * ybar * (1. / sq_xpy - 1. / sq_xmy);
+
+    Component_t dU_dxi = xi;
+    dU_dxi += sqrt( xi * xi - 1. ) * ( dcoeff_ / knll_ + log( xi + sqrt(xi * xi - 1. ) ) );
+    dU_dxi += xi * xi * ( dcoeff_ / knll_ + log( xi + sqrt(xi * xi - 1. ) ) ) / sqrt( xi * xi - 1. );
+    dU_dxi += - 2. * xi * xi * sqrt( xi * xi - 1. ) * ( dcoeff_ / knll_ + log( xi + sqrt(xi * xi - 1. ) ) ) / ( xi * xi - eta * eta );
+    dU_dxi += - 2. * xi * eta * sqrt( 1. - eta * eta ) * ( bcoeff_ / knll_ + acos(eta) ) / ( xi * xi - eta * eta );
+
+    dU_dxi /= ( xi * xi - eta * eta );
+
+    Component_t dU_deta = - eta;
+    dU_deta += sqrt( 1. - eta * eta ) * ( bcoeff_ / knll_ + acos(eta) );
+    dU_deta += - eta * eta * ( bcoeff_ / knll_ + acos(eta) ) / sqrt( 1. - eta * eta);
+    dU_deta += 2. * eta * xi * sqrt( xi * xi - 1. ) * ( dcoeff_ / knll_ + log( xi + sqrt(xi * xi - 1. ) ) ) / ( xi * xi - eta * eta );
+    dU_deta += 2. * eta * eta * sqrt( 1. - eta * eta ) * ( bcoeff_ / knll_ + acos(eta) ) / ( xi * xi - eta * eta );
+
+    dU_deta /= ( xi * xi - eta * eta );
+
+    dU_dxbar = (dU_dxi * dxi_dxbar + dU_deta * deta_dxbar) * ( knll_ / cnll_ );
+    dU_dybar = (dU_dxi * dxi_dybar + dU_deta * deta_dybar) * ( knll_ / cnll_ );
+  }
+
+  state[i_npx] += dU_dxbar;
+  state[i_npy] += dU_dybar;
 }
 
 //----------------------------------------------------------------------------------
